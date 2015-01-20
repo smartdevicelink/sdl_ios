73ce318604a918e35df36c9eebaf72cadbd8ba3c
Joel Fischer <joeljfischer@gmail.com>
2015-01-20 12:20:11 -0500

    SDLRPCPayload use enum instead of raw byte

--- sdl_ios/SmartDeviceLink/SDLRPCPayload.h
+++ sdl_ios/SmartDeviceLink/SDLRPCPayload.h
@@ -3,10 +3,11 @@
 //  Copyright (c) 2014 Ford Motor Company. All rights reserved.
 
 #import <Foundation/Foundation.h>
+#import <SmartDeviceLink/SDLRPCMessageType.h>
 
 @interface SDLRPCPayload : NSObject
 
-@property (assign) Byte rpcType;
+@property (assign) SDLRPCMessageType rpcType;
 @property (assign) UInt32 functionID;
 @property (assign) UInt32 correlationID;
 @property (strong) NSData *jsonData;
