5acb505082658719603f8035522ac19060301366
Joel Fischer <joel@livioradio.com>
2015-02-16 17:17:10 -0500

    More stuff in example app

--- SmartDeviceLink-iOS/SmartDeviceLink_Example/Classes/ConnectionTCPTableViewController.m
+++ SmartDeviceLink-iOS/SmartDeviceLink_Example/Classes/ConnectionTCPTableViewController.m
@@ -8,12 +8,18 @@
 
 #import "ConnectionTCPTableViewController.h"
 
+#import "Preferences.h"
+#import "ProxyManager.h"
+
 
 
 @interface ConnectionTCPTableViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *ipAddressTextField;
 @property (weak, nonatomic) IBOutlet UITextField *portTextField;
+
+@property (weak, nonatomic) IBOutlet UITableViewCell *connectTableViewCell;
+@property (weak, nonatomic) IBOutlet UIButton *connectButton;
 
 @end
 
@@ -23,12 +29,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Observe Proxy Manager state
+    [[ProxyManager sharedManager] addObserver:self forKeyPath:NSStringFromSelector(@selector(state)) options:(NSKeyValueObservingOptionInitial|NSKeyValueObservingOptionNew) context:nil];
+    
+    // Tableview setup
+    self.tableView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
+    self.ipAddressTextField.text = [Preferences sharedPreferences].ipAddress;
+    self.portTextField.text = [Preferences sharedPreferences].port;
+    
+    // Connect Button setup
+    self.connectButton.tintColor = [UIColor whiteColor];
+}
+
+- (void)dealloc {
+    @try {
+        [[ProxyManager sharedManager] removeObserver:self forKeyPath:NSStringFromSelector(@selector(state))];
+    } @catch (NSException __unused *exception) {}
+}
+
+
+#pragma mark - IBActions
+
+- (IBAction)connectButtonWasPressed:(UIButton *)sender {
+    [Preferences sharedPreferences].ipAddress = self.ipAddressTextField.text;
+    [Preferences sharedPreferences].port = self.portTextField.text;
+    
+    ProxyState state = [ProxyManager sharedManager].state;
+    switch (state) {
+        case ProxyStateStopped: {
+            [[ProxyManager sharedManager] startProxyWithTransportType:ProxyTransportTypeTCP];
+        } break;
+        case ProxyStateSearchingForConnection: {
+            [[ProxyManager sharedManager] stopProxy];
+        } break;
+        case ProxyStateConnected: {
+            [[ProxyManager sharedManager] stopProxy];
+        } break;
+        default: break;
+    }
 }
 
 
 #pragma mark - Table view delegate
 
-// In a xib-based application, navigation from a table can be handled in -tableView:didSelectRowAtIndexPath:
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     switch (indexPath.row) {
         case 0: {
@@ -41,5 +85,35 @@
     }
 }
 
+
+#pragma mark - KVO
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([keyPath isEqualToString:NSStringFromSelector(@selector(state))]) {
+        ProxyState newState = [change[NSKeyValueChangeNewKey] unsignedIntegerValue];
+        [self proxyManagerDidChangeState:newState];
+    }
+}
+
+
+#pragma mark - Private Methods
+
+- (void)proxyManagerDidChangeState:(ProxyState)newState {
+    switch (newState) {
+        case ProxyStateStopped: {
+            self.connectTableViewCell.backgroundColor = [UIColor redColor];
+            self.connectButton.titleLabel.text = @"Connect";
+        } break;
+        case ProxyStateSearchingForConnection: {
+            self.connectTableViewCell.backgroundColor = [UIColor blueColor];
+            self.connectButton.titleLabel.text = @"Stop Searching";
+        } break;
+        case ProxyStateConnected: {
+            self.connectTableViewCell.backgroundColor = [UIColor greenColor];
+            self.connectButton.titleLabel.text = @"Disconnect";
+        } break;
+        default: break;
+    }
+}
 
 @end
