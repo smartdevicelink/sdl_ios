22435c27d3db0252332a0e277ee61b8d5c7dd234
Joel Fischer <joel@livioradio.com>
2015-03-27 10:45:14 -0400

    Update PrioritizedObjectCollection Spec
    Add an ObjectWithPriority spec

--- SmartDeviceLink-iOS/SmartDeviceLinkTests/UtilitiesSpecs/Prioritized Objects/SDLPrioritizedObjectCollectionSpec.m
+++ SmartDeviceLink-iOS/SmartDeviceLinkTests/UtilitiesSpecs/Prioritized Objects/SDLPrioritizedObjectCollectionSpec.m
@@ -21,32 +21,62 @@ describe(@"a prioritized object collection", ^{
         collection = [[SDLPrioritizedObjectCollection alloc] init];
     });
     
+    it(@"should be empty when first created", ^{
+        id returnObj = [collection nextObject];
+        
+        expect(returnObj).to(beNil());
+    });
+    
+    it(@"should ignore adding nil objects", ^{
+        id obj = nil;
+        [collection addObject:obj withPriority:0];
+        
+        id returnObj = [collection nextObject];
+        expect(returnObj).to(beNil());
+    });
+    
+    it(@"should ignore adding NSNull objects", ^{
+        [collection addObject:[NSNull null] withPriority:0];
+        
+        id returnObj = [collection nextObject];
+        expect(returnObj).to(beNil());
+    });
+    
     it(@"should be able to add and retrieve a single item", ^{
         NSString *string = @"testString";
         [collection addObject:string withPriority:0];
         
-        id object = [collection nextObject];
+        id returnObj = [collection nextObject];
         
-        expect(object).to(equal(string));
+        expect(returnObj).to(equal(string));
     });
     
-    it(@"should retrieve higher priority objects first", ^{
+    describe(@"should retrieve higher priority objects first", ^{
         NSString *highPriorityString = @"highPriority";
         NSString *mediumPriorityString = @"mediumPriority";
         NSString *lowPriorityString = @"lowPriority";
         
-        // Add them in "incorrect" order to make sure sorting works correctly
+        // Add them in "incorrect" order to make sure sorting works correctly.
+        // Lower numbers indicate higher priority
         [collection addObject:mediumPriorityString withPriority:100];
-        [collection addObject:highPriorityString withPriority:200];
-        [collection addObject:lowPriorityString withPriority:0];
+        [collection addObject:lowPriorityString withPriority:200];
+        [collection addObject:highPriorityString withPriority:0];
         
         id firstObjectOut = [collection nextObject];
         id secondObjectOut = [collection nextObject];
         id thirdObjectOut = [collection nextObject];
         
-        expect(firstObjectOut).to(equal(highPriorityString));
-        expect(secondObjectOut).to(equal(mediumPriorityString));
-        expect(thirdObjectOut).to(equal(lowPriorityString));
+        it(@"should retrieve the highest priority first", ^{
+            expect(firstObjectOut).to(equal(highPriorityString));
+        });
+        
+        it(@"should retrieve the medium priority second", ^{
+            expect(secondObjectOut).to(equal(mediumPriorityString));
+        });
+        
+        it(@"should retrieve the lowest priority last", ^{
+            expect(thirdObjectOut).to(equal(lowPriorityString));
+        });
     });
 });
 
