78f00d293967fb8f8ce045137a5980a2e55b7de5
Joel Fischer <joel@livioradio.com>
2015-03-30 13:13:18 -0400

    Remove copyright from new file

--- SmartDeviceLink-iOS/SmartDeviceLink/SDLDebugToolConsole.h
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLDebugToolConsole.h
@@ -1,10 +1,6 @@
 //
 //  SDLDebugToolConsole.h
 //  SmartDeviceLink-iOS
-//
-//  Created by Joel Fischer on 3/12/15.
-//  Copyright (c) 2015 smartdevicelink. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 
