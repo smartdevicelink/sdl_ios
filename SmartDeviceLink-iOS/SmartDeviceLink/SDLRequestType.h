b6c9665b174b8b5973b8a77476e9a66c9d48d883
Joel Fischer <joel@livioradio.com>
2015-04-02 10:07:14 -0400

    Add new RequestType enum values
    Add new RequestType enum value specs

--- SmartDeviceLink-iOS/SmartDeviceLink/SDLRequestType.h
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLRequestType.h
@@ -16,5 +16,19 @@
 +(SDLRequestType*) AUTH_CHALLENGE;
 +(SDLRequestType*) AUTH_ACK;
 +(SDLRequestType*) PROPRIETARY;
++(SDLRequestType*) QUERY_APPS;
++(SDLRequestType*) LAUNCH_APP;
++(SDLRequestType*) LOCK_SCREEN_ICON_URL;
++(SDLRequestType*) TRAFFIC_MESSAGE_CHANNEL;
++(SDLRequestType*) DRIVER_PROFILE;
++(SDLRequestType*) VOICE_SEARCH;
++(SDLRequestType*) NAVIGATION;
++(SDLRequestType*) PHONE;
++(SDLRequestType*) CLIMATE;
++(SDLRequestType*) SETTINGS;
++(SDLRequestType*) VEHICLE_DIAGNOSTICS;
++(SDLRequestType*) EMERGENCY;
++(SDLRequestType*) MEDIA;
++(SDLRequestType*) FOTA;
 
 @end
