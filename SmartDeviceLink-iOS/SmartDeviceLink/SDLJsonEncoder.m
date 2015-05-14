96035da75079512f1a3543756d70ba979f0fdc6d
Joel Fischer <joel@livio.io>
2015-05-14 10:35:03 -0400

    Add logging to SDLJsonEncoder
    Log if a nil dictionary is passed
    
    Log if the encoding fails with an error

--- SmartDeviceLink-iOS/SmartDeviceLink/SDLJsonEncoder.m
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLJsonEncoder.m
@@ -4,7 +4,9 @@
 
 #import "SDLJsonEncoder.h"
 
+#import "SDLDebugTool.h"
 #import "SDLNames.h"
+
 
 @implementation SDLJsonEncoder
 
@@ -18,8 +20,18 @@ static NSObject<SDLEncoder>* jsonEncoderInstance;
 }
 
 -(NSData*) encodeDictionary:(NSDictionary*) dict {
-    NSError* error;
+    if (dict == nil) {
+        [SDLDebugTool logInfo:@"Warning: Encoding dictionary to JSON, no dictionary passed" withType:SDLDebugType_Protocol];
+        return nil;
+    }
+    
+    NSError* error = nil;
     NSData* jsonData = [NSJSONSerialization dataWithJSONObject:dict options:kNilOptions error:&error];
+    
+    if (error != nil) {
+        [SDLDebugTool logInfo:[NSString stringWithFormat:@"Error encoding JSON data: %@", error] withType:SDLDebugType_Protocol];
+        return nil;
+    }
     
     return jsonData;
 }
