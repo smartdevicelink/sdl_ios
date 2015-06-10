27b484493bca1a789508e9f4f5d77c0dee3da37b
kmilitel <kmilitel@MGC000000427>
2015-06-10 07:43:57 -0400

    Removed created by.

--- SmartDeviceLink-iOS/SmartDeviceLink/SDLOnError.h
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLOnError.h
@@ -2,7 +2,6 @@
 //  SDLOnError.h
 //  SmartDeviceLink
 //
-//  Created by Militello, Kevin (K.) on 12/17/14.
 //  Copyright (c) 2014 Ford Motor Company. All rights reserved.
 //
 
--- SmartDeviceLink-iOS/SmartDeviceLink/SDLOnProxyClosed.m
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLOnProxyClosed.m
@@ -2,7 +2,6 @@
 //  SDLOnProxyClosed.m
 //  SmartDeviceLink
 //
-//  Created by Militello, Kevin (K.) on 3/26/15.
 //  Copyright (c) 2015 FMC. All rights reserved.
 //
 
