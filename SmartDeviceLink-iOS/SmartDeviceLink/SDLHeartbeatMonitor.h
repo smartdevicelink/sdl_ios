195e0ccd774802c749c7e3bd01fbf7751787f248
kmilitel <kmilitel@MGC000000427>
2015-06-10 07:40:34 -0400

    Class stub.

---
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLHeartbeatMonitor.h
@@ -0,0 +1,14 @@
+//
+//  SDLHeartbeatMonitor.h
+//  SmartDeviceLink
+//
+//  Copyright (c) 2015 FMC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDLHeartbeatMonitor : NSObject
+
+@property (nonatomic) NSTimeInterval interval;
+
+@end
