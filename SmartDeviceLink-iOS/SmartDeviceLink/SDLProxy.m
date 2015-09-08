a453a1f1676d56dd1bec830b421b2fa2a67fe9aa
Joel Fischer <joel@livio.io>
2015-09-08 12:54:14 -0400

    Update to v4.0.0-alpha.4

--- SmartDeviceLink-iOS/SmartDeviceLink/SDLProxy.m
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLProxy.m
@@ -43,7 +43,7 @@
 
 typedef void (^URLSessionTaskCompletionHandler)(NSData *data, NSURLResponse *response, NSError *error);
 
-NSString *const SDLProxyVersion = @"4.0.0-alpha.3";
+NSString *const SDLProxyVersion = @"4.0.0-alpha.4";
 const float startSessionTime = 10.0;
 const float notifyProxyClosedDelay = 0.1;
 const int POLICIES_CORRELATION_ID = 65535;
