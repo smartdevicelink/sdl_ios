90bfb282e51d5a59f0ce4be152135c5a8b93c24e
Justin Dickow <jjdickow@gmail.com>
2015-09-30 13:28:49 -0400

    Added extern strings for new notification enhancement

---
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLNotificationConstants.h
@@ -0,0 +1,34 @@
+//
+//  SDLNotificationConstants.h
+//  SmartDeviceLink-iOS
+//
+//  Created by Justin Dickow on 9/30/15.
+//  Copyright © 2015 smartdevicelink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const SDLDidReceiveFirstFullHMIStatusNotification;
+extern NSString *const SDLDidReceiveFirstNonNoneHMIStatusNotification;
+extern NSString *const SDLDidChangeDriverDistractionStateNotification;
+extern NSString *const SDLDidChangeHMIStatusNotification;
+extern NSString *const SDLDidDisconnectNotification;
+extern NSString *const SDLDidConnectNotification;
+extern NSString *const SDLDidRegisterNotification;
+extern NSString *const SDLDidFailToRegisterNotification;
+extern NSString *const SDLDidReceiveErrorNotification;
+extern NSString *const SDLDidUnregisterNotification;
+extern NSString *const SDLDidReceiveAudioPassThruNotification;
+extern NSString *const SDLDidReceiveButtonEventNotification;
+extern NSString *const SDLDidReceiveButtonPressNotification;
+extern NSString *const SDLDidReceiveCommandNotification;
+extern NSString *const SDLDidReceiveEncodedDataNotification;
+extern NSString *const SDLDidReceiveNewHashNotification;
+extern NSString *const SDLDidChangeLanguageNotification;
+extern NSString *const SDLDidChangeLockScreenStatusNotification;
+extern NSString *const SDLDidChangePermissionsNotification;
+extern NSString *const SDLDidReceiveDataNotification;
+extern NSString *const SDLDidReceiveSystemRequestNotification;
+extern NSString *const SDLDidChangeTurnByTurnStateNotification;
+extern NSString *const SDLDidReceiveTouchEventNotification;
+extern NSString *const SDLDidReceiveVehicleDataNotification;
---
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLNotificationConstants.m
@@ -0,0 +1,34 @@
+//
+//  SDLNotificationConstants.m
+//  SmartDeviceLink-iOS
+//
+//  Created by Justin Dickow on 9/30/15.
+//  Copyright © 2015 smartdevicelink. All rights reserved.
+//
+
+#import "SDLNotificationConstants.h"
+
+NSString *const SDLDidReceiveFirstFullHMIStatusNotification = @"com.sdl.notification.firstFullHMIStatus";
+NSString *const SDLDidReceiveFirstNonNoneHMIStatusNotification = @"com.sdl.notification.firstNonNoneHMIStatus";
+NSString *const SDLDidChangeDriverDistractionStateNotification = @"com.sdl.notification.changeDriverDistractionState";
+NSString *const SDLDidChangeHMIStatusNotification = @"com.sdl.notification.changeHMIStatus";
+NSString *const SDLDidDisconnectNotification = @"com.sdl.notification.disconnect";
+NSString *const SDLDidConnectNotification = @"com.sdl.notification.connect";
+NSString *const SDLDidRegisterNotification = @"com.sdl.notification.register";
+NSString *const SDLDidFailToRegisterNotification = @"com.sdl.notification.failToRegister";
+NSString *const SDLDidReceiveErrorNotification = @"com.sdl.notification.receiveError";
+NSString *const SDLDidUnregisterNotification = @"com.sdl.notification.unregister";
+NSString *const SDLDidReceiveAudioPassThruNotification = @"com.sdl.notification.receiveAudioPassThru";
+NSString *const SDLDidReceiveButtonEventNotification = @"com.sdl.notification.receiveButtonEvent";
+NSString *const SDLDidReceiveButtonPressNotification = @"com.sdl.notification.receiveButtonPress";
+NSString *const SDLDidReceiveCommandNotification = @"com.sdl.notification.receiveCommand";
+NSString *const SDLDidReceiveEncodedDataNotification = @"com.sdl.notification.receiveEncodedData";
+NSString *const SDLDidReceiveNewHashNotification = @"com.sdl.notification.receiveNewHash";
+NSString *const SDLDidChangeLanguageNotification = @"com.sdl.notification.changeLanguage";
+NSString *const SDLDidChangeLockScreenStatusNotification = @"com.sdl.notification.changeLockScreenStatus";
+NSString *const SDLDidChangePermissionsNotification = @"com.sdl.notification.changePermission";
+NSString *const SDLDidReceiveDataNotification = @"com.sdl.notification.receiveData";
+NSString *const SDLDidReceiveSystemRequestNotification = @"com.sdl.notification.receiveSystemRequest";
+NSString *const SDLDidChangeTurnByTurnStateNotification = @"com.sdl.notification.changeTurnByTurnState";
+NSString *const SDLDidReceiveTouchEventNotification = @"com.sdl.notification.receiveTouchEvent";
+NSString *const SDLDidReceiveVehicleDataNotification = @"com.sdl.notification.receiveVehicleData";
