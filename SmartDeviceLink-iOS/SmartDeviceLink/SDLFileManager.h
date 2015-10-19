1e30d5cfdd2b32bf7fb5b3fc19ecf32295823e05
Joel Fischer <joeljfischer@gmail.com>
2015-10-19 15:29:55 -0400

    SDLFileManager delete and upload updates
    * Added delete and upload methods with completion handlers
    * Add a file wrapper "struct" to contain a file and it's completion handler
    * Remote files array is now mutable
    * Add a currentFileUploadOffset property for large file uploads
    * Upload queue now has a constraint
    * State is now handled a little differently and affects a few parts
    * Fix not doing weak/strongself where necessary

--- SmartDeviceLink-iOS/SmartDeviceLink/SDLFileManager.h
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLFileManager.h
@@ -8,16 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+@class SDLFile;
+
+
 NS_ASSUME_NONNULL_BEGIN
 
 typedef NSString SDLFileName;
+
+typedef void (^SDLFileManagerDeleteCompletion)(BOOL success, NSUInteger bytesAvailable, NSError *error);
+typedef void (^SDLFileManagerUploadCompletion)(BOOL success, NSUInteger bytesAvailable, NSError *error);
 
 @interface SDLFileManager : NSObject
 
 @property (copy, nonatomic, readonly) NSArray<SDLFileName *> *remoteFiles;
 @property (assign, nonatomic, readonly) NSUInteger bytesAvailable;
 
-- (void)deleteRemoteFileWithName:(SDLFileName *)name;
+- (void)deleteRemoteFileWithName:(SDLFileName *)name completionHandler:(SDLFileManagerDeleteCompletion)completion;
+- (void)uploadFile:(SDLFile *)file completionHandler:(SDLFileManagerUploadCompletion)completion;
 
 @end
 
--- SmartDeviceLink-iOS/SmartDeviceLink/SDLFileManager.m
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLFileManager.m
@@ -10,6 +10,7 @@
 
 #import "SDLDeleteFile.h"
 #import "SDLDeleteFileResponse.h"
+#import "SDLFile.h"
 #import "SDLListFiles.h"
 #import "SDLListFilesResponse.h"
 #import "SDLManager.h"
@@ -25,13 +26,29 @@ typedef NS_ENUM(NSUInteger, SDLFileManagerState) {
     SDLFileManagerStateWaiting
 };
 
+
+@interface SDLFileWrapper : NSObject
+
+@property (strong, nonatomic, readonly) SDLFile *file;
+@property (copy, nonatomic, readonly) SDLFileManagerUploadCompletion completionHandler;
+
+- (instancetype)initWithFile:(SDLFile *)file completionHandler:(SDLFileManagerUploadCompletion)completionHandler;
+
++ (instancetype)wrapperWithFile:(SDLFile *)file completionHandler:(SDLFileManagerUploadCompletion)completionHandler;
+
+@end
+
+
 @interface SDLFileManager ()
 
-@property (copy, nonatomic, readwrite) NSArray<SDLFileName *> *remoteFiles;
+// Remote state
+@property (copy, nonatomic, readwrite) NSMutableArray<SDLFileName *> *mutableRemoteFiles;
 @property (assign, nonatomic, readwrite) NSUInteger bytesAvailable;
 
-@property (copy, nonatomic) NSMutableArray *uploadQueue;
+// Local state
+@property (copy, nonatomic) NSMutableArray<SDLFileWrapper *> *uploadQueue;
 @property (assign, nonatomic) SDLFileManagerState state;
+@property (assign, nonatomic) NSUInteger currentFileUploadOffset;
 
 @end
 
@@ -44,10 +61,11 @@ typedef NS_ENUM(NSUInteger, SDLFileManagerState) {
         return nil;
     }
     
-    _remoteFiles = @[];
     _bytesAvailable = 0;
+    _mutableRemoteFiles = [NSMutableArray array];
     _uploadQueue = [NSMutableArray array];
     _state = SDLFileManagerStateNotConnected;
+    _currentFileUploadOffset = 0;
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sdl_didConnect:) name:SDLDidConnectNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sdl_didDisconnect:) name:SDLDidDisconnectNotification object:nil];
@@ -56,27 +74,75 @@ typedef NS_ENUM(NSUInteger, SDLFileManagerState) {
 }
 
 
+#pragma mark - Getters
+
+- (NSArray<SDLFileName *> *)remoteFiles {
+    return [self.mutableRemoteFiles copy];
+}
+
+
+#pragma mark - Setters
+
+- (void)setState:(SDLFileManagerState)state {
+    _state = state;
+    
+    switch (state) {
+        case SDLFileManagerStateReady: {
+            if (self.uploadQueue.count != 0) {
+                SDLFileWrapper *wrapper = [self.uploadQueue firstObject];
+                [self.uploadQueue removeObjectAtIndex:0];
+                [self uploadFile:wrapper.file completionHandler:wrapper.completionHandler];
+            }
+        } break;
+        default: {
+            return;
+        } break;
+    }
+}
+
+
 #pragma mark - Remote File Manipulation
 
-- (void)deleteRemoteFileWithName:(SDLFileName *)name {
+- (void)deleteRemoteFileWithName:(SDLFileName *)name completionHandler:(SDLFileManagerDeleteCompletion)completion {
     SDLDeleteFile *deleteFile = [SDLRPCRequestFactory buildDeleteFileWithName:name correlationID:@0];
     
+    __weak typeof(self) weakSelf = self;
     [[SDLManager sharedManager] sendRequest:deleteFile withCompletionHandler:^(__kindof SDLRPCRequest *request, __kindof SDLRPCResponse *response, NSError *error) {
-        if (error != nil) {
-            // TODO:
-            return;
+        __strong typeof(self) strongSelf = weakSelf;
+        
+        // Pull out the parameters
+        SDLDeleteFileResponse *deleteFileResponse = (SDLDeleteFileResponse *)response;
+        BOOL success = [deleteFileResponse.success boolValue];
+        NSInteger bytesAvailable = [deleteFileResponse.spaceAvailable unsignedIntegerValue];
+        
+        // Mutate self based on the changes
+        strongSelf.bytesAvailable = bytesAvailable;
+        if (success) {
+            [self.mutableRemoteFiles removeObject:name];
         }
         
-//        SDLDeleteFileResponse *deleteFileResponse = (SDLDeleteFileResponse *)response;
-//        BOOL success = deleteFileResponse.success.boolValue; TODO: Do anything with this? Optional completion block?
+        // Callback
+        completion(success, bytesAvailable, error);
     }];
+}
+
+- (void)uploadFile:(SDLFile *)file completionHandler:(SDLFileManagerUploadCompletion)completion {
+    switch (self.state) {
+        case SDLFileManagerStateReady: {
+            self.state = SDLFileManagerStateWaiting;
+            
+            SDLPutFile *putFile = [SDLRPCRequestFactory buildPutFileWithFileName:file.name fileType:<#(SDLFileType *)#> persistentFile:file.isPersistent correlationId:@0];
+        } break;
+        case SDLFileManagerStateWaiting: {
+            [self.uploadQueue addObject:[SDLFileWrapper wrapperWithFile:file completionHandler:completion]];
+        } break;
+    }
 }
 
 
 #pragma mark - SDL Notification Observers
 
 - (void)sdl_didConnect:(NSNotification *)notification {
-    // TODO: List files
     self.state = SDLFileManagerStateWaiting;
     SDLListFiles *listFiles = [SDLRPCRequestFactory buildListFilesWithCorrelationID:@0];
     
@@ -90,8 +156,8 @@ typedef NS_ENUM(NSUInteger, SDLFileManagerState) {
         }
         
         SDLListFilesResponse *listFilesResponse = (SDLListFilesResponse *)response;
-        strongSelf.remoteFiles = [listFilesResponse.filenames copy];
-        strongSelf.bytesAvailable = listFilesResponse.spaceAvailable.unsignedIntegerValue;
+        strongSelf.mutableRemoteFiles = [listFilesResponse.filenames copy];
+        strongSelf.bytesAvailable = [listFilesResponse.spaceAvailable unsignedIntegerValue];
         
         strongSelf.state = SDLFileManagerStateReady;
     }];
@@ -99,10 +165,33 @@ typedef NS_ENUM(NSUInteger, SDLFileManagerState) {
 
 - (void)sdl_didDisconnect:(NSNotification *)notification {
     // TODO: Reset properties
-    self.remoteFiles = @[];
+    self.mutableRemoteFiles = [NSMutableArray array];
     self.bytesAvailable = 0;
     self.uploadQueue = [NSMutableArray array];
     self.state = SDLFileManagerStateNotConnected;
+}
+
+@end
+
+
+#pragma mark - SDLFileWrapper Helper
+
+@implementation SDLFileWrapper
+
+- (instancetype)initWithFile:(SDLFile *)file completionHandler:(SDLFileManagerUploadCompletion)completionHandler {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    
+    _file = file;
+    _completionHandler = completionHandler;
+    
+    return self;
+}
+             
++ (instancetype)wrapperWithFile:(SDLFile *)file completionHandler:(SDLFileManagerUploadCompletion)completionHandler {
+    return [[self alloc] initWithFile:file completionHandler:completionHandler];
 }
 
 @end
