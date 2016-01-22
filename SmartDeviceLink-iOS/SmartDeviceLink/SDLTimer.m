fe2fe5ac2431ab297655acf16f38bc0bf4797f0f
Joel Fischer <joeljfischer@gmail.com>
2016-01-22 11:19:27 -0500

    Replace SDLTimer's NSTimers with dispatch_source's running in the background

--- SmartDeviceLink-iOS/SmartDeviceLink/SDLTimer.m
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLTimer.m
@@ -7,20 +7,16 @@
 
 @interface SDLTimer ()
 
-@property (strong) NSTimer *timer;
-@property (assign) BOOL timerRunning;
-@property (nonatomic) BOOL repeat;
+@property (strong, atomic) dispatch_source_t timer;
+@property (assign, atomic) BOOL timerRunning;
+@property (assign, nonatomic) BOOL repeat;
 @end
 
 
 @implementation SDLTimer
 
 - (instancetype)init {
-    if (self = [super init]) {
-        _duration = 0;
-        _timerRunning = NO;
-    }
-    return self;
+    return [self initWithDuration:0 repeat:NO];
 }
 
 - (instancetype)initWithDuration:(float)duration {
@@ -37,43 +33,70 @@
     return self;
 }
 
+- (void)dealloc {
+    [self cancel];
+}
+
 - (void)start {
     if (self.duration > 0) {
-        [self stopAndDestroyTimer];
-        self.timer = [NSTimer timerWithTimeInterval:self.duration target:self selector:@selector(timerElapsed) userInfo:nil repeats:self.repeat];
-        [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+        [self sdl_stopAndDestroyTimer];
+        
+        self.timer = sdl_createDispatchTimer(_duration, 0.05, [self.class sdl_timerQueue], ^{
+            [self sdl_timerElapsed];
+        });
+        
         self.timerRunning = YES;
     }
 }
 
 - (void)cancel {
-    [self stopAndDestroyTimer];
+    [self sdl_stopAndDestroyTimer];
+    
     if (self.timerRunning && self.canceledBlock != nil) {
         self.timerRunning = NO;
         self.canceledBlock();
     }
+    
     self.timerRunning = NO;
 }
 
-- (void)timerElapsed {
+- (void)sdl_timerElapsed {
     if (self.repeat == NO) {
-        [self stopAndDestroyTimer];
+        [self sdl_stopAndDestroyTimer];
         self.timerRunning = NO;
     }
+    
     if (self.elapsedBlock != nil) {
         self.elapsedBlock();
     }
 }
 
-- (void)stopAndDestroyTimer {
+- (void)sdl_stopAndDestroyTimer {
     if (self.timer != nil) {
-        [self.timer invalidate];
-        self.timer = nil;
+        dispatch_source_cancel(self.timer);
     }
 }
 
-- (void)dealloc {
-    [self cancel];
+// http://stackoverflow.com/a/8403743/1221798
+dispatch_source_t sdl_createDispatchTimer(uint64_t interval, uint64_t leeway, dispatch_queue_t queue, dispatch_block_t block) {
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    if (timer != nil) {
+        dispatch_source_set_timer(timer, dispatch_walltime(NULL, 0), interval * NSEC_PER_SEC, leeway * NSEC_PER_SEC);
+        dispatch_source_set_event_handler(timer, block);
+        dispatch_resume(timer);
+    }
+    
+    return timer;
+}
+
++ (dispatch_queue_t)sdl_timerQueue {
+    static dispatch_queue_t timerQueue = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        timerQueue = dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0);
+    });
+    
+    return timerQueue;
 }
 
 @end
