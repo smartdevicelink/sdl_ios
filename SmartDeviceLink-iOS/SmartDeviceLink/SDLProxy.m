eec7cf33b6c255de7a7180da567bdea4ab88d449
Joel Fischer <joeljfischer@gmail.com>
2016-01-25 09:26:59 -0500

    Fix removing observers that don't exist in SDLProxy

--- SmartDeviceLink-iOS/SmartDeviceLink/SDLProxy.m
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLProxy.m
@@ -166,9 +166,6 @@ const int POLICIES_CORRELATION_ID = 65535;
 }
 
 - (void)onProtocolClosed {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillResignActiveNotification object:nil];
-
     [self notifyProxyClosed];
 }
 
