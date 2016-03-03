5907c3160de279293017ea79784f17bf882d0c0b
Justin Dickow <jjdickow@gmail.com>
2016-03-03 15:26:16 -0500

    Handle System Request type HTTP according to PTU
    based on implementation described in integration guidelines for updating policy table according to GENIVI implementation
    
    Signed-off-by: Justin Dickow <jjdickow@gmail.com>

--- SmartDeviceLink-iOS/SmartDeviceLink/SDLV2ProtocolMessage.m
+++ SmartDeviceLink-iOS/SmartDeviceLink/SDLV2ProtocolMessage.m
@@ -21,8 +21,9 @@
 
 // Convert RPC payload to dictionary (for consumption by RPC layer)
 - (NSDictionary *)rpcDictionary {
+    
     // Only applicable to RPCs
-    if (self.header.serviceType != SDLServiceType_RPC) {
+    if (self.header.serviceType != SDLServiceType_RPC && self.header.serviceType != SDLServiceType_BulkData) {
         return nil;
     }
 
