969c36a5a6a7e7275675f8de1243641e912177c2
Joel Fischer <joeljfischer@gmail.com>
2016-05-26 11:14:10 -0400

    Add additional ListFilesOperation tests

--- SmartDeviceLink-iOS/SmartDeviceLinkTests/DevAPISpecs/SDLListFilesOperationSpec.m
+++ SmartDeviceLink-iOS/SmartDeviceLinkTests/DevAPISpecs/SDLListFilesOperationSpec.m
@@ -1,7 +1,9 @@
 #import <Quick/Quick.h>
 #import <Nimble/Nimble.h>
 
+#import "SDLError.h"
 #import "SDLListFiles.h"
+#import "SDLListFilesResponse.h"
 #import "SDLListFilesOperation.h"
 #import "TestConnectionManager.h"
 
@@ -22,9 +24,13 @@ describe(@"List Files Operation", ^{
         testOperation = [[SDLListFilesOperation alloc] initWithConnectionManager:testConnectionManager completionHandler:^(BOOL success, NSUInteger bytesAvailable, NSArray<NSString *> * _Nonnull fileNames, NSError * _Nullable error) {
             successResult = success;
             bytesAvailableResult = bytesAvailable;
-            errorResult = error;
             fileNamesResult = fileNames;
+            errorResult = error;
         }];
+    });
+    
+    it(@"should have a priority of 'very high'", ^{
+        expect(@(testOperation.queuePriority)).to(equal(@(NSOperationQueuePriorityVeryHigh)));
     });
     
     describe(@"running the operation", ^{
@@ -37,12 +43,63 @@ describe(@"List Files Operation", ^{
         });
         
         context(@"when a good response comes back", ^{
+            __block SDLListFilesResponse *goodResponse = nil;
+            __block NSNumber *responseSpaceAvailable = nil;
+            __block NSMutableArray *responseFileNames = nil;
             
+            beforeEach(^{
+                responseSpaceAvailable = @(11212512);
+                responseFileNames = [NSMutableArray arrayWithArray:@[@"test1", @"test2"]];
+                
+                goodResponse = [[SDLListFilesResponse alloc] init];
+                goodResponse.success = @YES;
+                goodResponse.spaceAvailable = responseSpaceAvailable;
+                goodResponse.filenames = responseFileNames;
+                
+                [testConnectionManager respondToLastRequestWithResponse:goodResponse];
+            });
+            
+            it(@"should have called the completion handler with proper data", ^{
+                expect(@(successResult)).to(equal(@YES));
+                expect(@(bytesAvailableResult)).to(equal(responseSpaceAvailable));
+                expect(fileNamesResult).to(haveCount(@(responseFileNames.count)));
+                expect(fileNamesResult).to(contain(responseFileNames.firstObject));
+                expect(errorResult).to(beNil());
+            });
+            
+            it(@"should be set to finished", ^{
+                expect(@(testOperation.finished)).to(equal(@YES));
+                expect(@(testOperation.executing)).to(equal(@NO));
+            });
         });
         
         context(@"when a bad response comes back", ^{
+            __block SDLListFilesResponse *badResponse = nil;
+            __block NSNumber *responseSpaceAvailable = nil;
+            __block NSMutableArray *responseFileNames = nil;
+            
+            __block NSString *responseErrorDescription = nil;
+            __block NSString *responseErrorReason = nil;
+            
             beforeEach(^{
+                responseSpaceAvailable = @(0);
+                responseFileNames = [NSMutableArray arrayWithArray:@[]];
                 
+                responseErrorDescription = @"some description";
+                responseErrorReason = @"some reason";
+                
+                badResponse = [[SDLListFilesResponse alloc] init];
+                badResponse.success = @NO;
+                badResponse.spaceAvailable = responseSpaceAvailable;
+                badResponse.filenames = responseFileNames;
+                
+                [testConnectionManager respondToLastRequestWithResponse:badResponse error:[NSError sdl_lifecycle_unknownRemoteErrorWithDescription:responseErrorDescription andReason:responseErrorReason]];
+            });
+            
+            it(@"should have called completion handler with error", ^{
+                expect(errorResult.localizedDescription).to(match(responseErrorDescription));
+                expect(errorResult.localizedFailureReason).to(match(responseErrorReason));
+                expect(@(successResult)).to(equal(@NO));
             });
         });
     });
