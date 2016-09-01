d079edf2dfd016910e81dac615c76ce1cfb39aba
Joel Fischer <joeljfischer@gmail.com>
2016-09-01 09:59:14 -0400

    Add some more example usage to the example app

--- SmartDeviceLink_Example/Classes/ProxyManager.m
+++ SmartDeviceLink_Example/Classes/ProxyManager.m
@@ -12,8 +12,17 @@
 NSString *const SDLAppName = @"SDL Example App";
 NSString *const SDLAppId = @"9999";
 
+typedef NS_ENUM(NSUInteger, SDLHMIFirstState) {
+    SDLHMIFirstStateNone,
+    SDLHMIFirstStateNonNone,
+    SDLHMIFirstStateFull
+};
+
 
 @interface ProxyManager () <SDLManagerDelegate>
+
+// Describes the first time the HMI state goes non-none and full.
+@property (assign, nonatomic) SDLHMIFirstState firstTimeState;
 
 @end
 
@@ -38,11 +47,17 @@ NSString *const SDLAppId = @"9999";
         return nil;
     }
     
+    _firstTimeState = SDLHMIFirstStateNone;
+    
     return self;
 }
 
 - (void)startIAP {
     SDLLifecycleConfiguration *lifecycleConfig = [self.class sdlex_setLifecycleConfigurationPropertiesOnConfiguration:[SDLLifecycleConfiguration defaultConfigurationWithAppName:SDLAppName appId:SDLAppId]];
+    
+    // Assume this is production and disable logging
+    lifecycleConfig.logFlags = SDLLogOutputNone;
+    
     SDLConfiguration *config = [SDLConfiguration configurationWithLifecycle:lifecycleConfig lockScreen:[SDLLockScreenConfiguration enabledConfiguration]];
     self.sdlManager = [[SDLManager alloc] initWithConfiguration:config delegate:self];
     
@@ -77,10 +92,117 @@ NSString *const SDLAppId = @"9999";
 + (SDLLifecycleConfiguration *)sdlex_setLifecycleConfigurationPropertiesOnConfiguration:(SDLLifecycleConfiguration *)config {
     SDLArtwork *appIconArt = [SDLArtwork persistentArtworkWithImage:[UIImage imageNamed:@"AppIcon60x60@2x"] name:@"AppIcon" asImageFormat:SDLArtworkImageFormatPNG];
     
+    config.appType = [SDLAppHMIType DEFAULT];
     config.shortAppName = @"SDL Example";
     config.appIcon = appIconArt;
+    config.voiceRecognitionCommandNames = @[@"S D L Example"];
+    config.ttsName = @[[SDLTTSChunkFactory buildTTSChunkForString:config.shortAppName type:[SDLSpeechCapabilities TEXT]]];
     
     return config;
+}
+
+
+#pragma mark - RPC builders
+
+- (SDLAddCommand *)speakNameCommand {
+    NSString *commandName = @"Speak App Name";
+    
+    SDLMenuParams *commandMenuParams = [[SDLMenuParams alloc] init];
+    commandMenuParams.menuName = commandName;
+    
+    SDLAddCommand *speakNameCommand = [[SDLAddCommand alloc] init];
+    speakNameCommand.vrCommands = [NSMutableArray arrayWithObject:commandName];
+    speakNameCommand.menuParams = commandMenuParams;
+    speakNameCommand.cmdID = @0;
+    
+    __weak typeof(self) weakSelf = self;
+    speakNameCommand.handler = ^void(SDLOnCommand *notification) {
+        [weakSelf.sdlManager sendRequest:[self.class appNameSpeak]];
+    };
+    
+    return speakNameCommand;
+}
+
+- (SDLAddCommand *)interactionSetCommand {
+    NSString *commandName = @"Perform Interaction";
+    
+    SDLMenuParams *commandMenuParams = [[SDLMenuParams alloc] init];
+    commandMenuParams.menuName = commandName;
+    
+    SDLAddCommand *performInteractionCommand = [[SDLAddCommand alloc] init];
+    performInteractionCommand.vrCommands = [NSMutableArray arrayWithObject:commandName];
+    performInteractionCommand.menuParams = commandMenuParams;
+    performInteractionCommand.cmdID = @1;
+    
+    // NOTE: You may want to preload your interaction sets, because they can take a while for the remote system to process. We're going to ignore our own advice here.
+    __weak typeof(self) weakSelf = self;
+    performInteractionCommand.handler = ^void(SDLOnCommand *notification) {
+        [weakSelf.sdlManager sendRequest:[self.class createOnlyChoiceInteractionSet] withResponseHandler:^(__kindof SDLRPCRequest * _Nullable request, __kindof SDLRPCResponse * _Nullable response, NSError * _Nullable error) {
+            [weakSelf.class sendPerformOnlyChoiceInteractionWithManager:weakSelf.sdlManager];
+        }];
+    };
+    
+    return performInteractionCommand;
+}
+
++ (SDLSpeak *)appNameSpeak {
+    SDLSpeak *speak = [[SDLSpeak alloc] init];
+    speak.ttsChunks = [NSMutableArray arrayWithObject:[SDLTTSChunkFactory buildTTSChunkForString:@"S D L Example App" type:[SDLSpeechCapabilities TEXT]]];
+    
+    return speak;
+}
+
++ (SDLSpeak *)goodJobSpeak {
+    SDLSpeak *speak = [[SDLSpeak alloc] init];
+    speak.ttsChunks = [NSMutableArray arrayWithObject:[SDLTTSChunkFactory buildTTSChunkForString:@"Good job" type:[SDLSpeechCapabilities TEXT]]];
+    
+    return speak;
+}
+
++ (SDLSpeak *)youMissedItSpeak {
+    SDLSpeak *speak = [[SDLSpeak alloc] init];
+    speak.ttsChunks = [NSMutableArray arrayWithObject:[SDLTTSChunkFactory buildTTSChunkForString:@"You missed it" type:[SDLSpeechCapabilities TEXT]]];
+    
+    return speak;
+}
+
++ (SDLCreateInteractionChoiceSet *)createOnlyChoiceInteractionSet {
+    SDLCreateInteractionChoiceSet *createInteractionSet = [[SDLCreateInteractionChoiceSet alloc] init];
+    createInteractionSet.interactionChoiceSetID = @0;
+    
+    NSString *theOnlyChoiceName = @"The Only Choice";
+    SDLChoice *theOnlyChoice = [[SDLChoice alloc] init];
+    theOnlyChoice.choiceID = @0;
+    theOnlyChoice.menuName = theOnlyChoiceName;
+    theOnlyChoice.vrCommands = [NSMutableArray arrayWithObject:theOnlyChoiceName];
+    
+    createInteractionSet.choiceSet = [NSMutableArray arrayWithArray:@[theOnlyChoice]];
+    
+    return createInteractionSet;
+}
+
++ (void)sendPerformOnlyChoiceInteractionWithManager:(SDLManager *)manager {
+    SDLPerformInteraction *performOnlyChoiceInteraction = [[SDLPerformInteraction alloc] init];
+    performOnlyChoiceInteraction.initialText = @"Choose the only one! You have 5 seconds...";
+    performOnlyChoiceInteraction.initialPrompt = [NSMutableArray arrayWithObject:[SDLTTSChunkFactory buildTTSChunkForString:@"Choose it" type:[SDLSpeechCapabilities TEXT]]];
+    performOnlyChoiceInteraction.interactionMode = [SDLInteractionMode BOTH];
+    performOnlyChoiceInteraction.interactionChoiceSetIDList = [NSMutableArray arrayWithObject:@0];
+    performOnlyChoiceInteraction.helpPrompt = [NSMutableArray arrayWithObject:[SDLTTSChunkFactory buildTTSChunkForString:@"Do it" type:[SDLSpeechCapabilities TEXT]]];
+    performOnlyChoiceInteraction.timeoutPrompt = [NSMutableArray arrayWithObject:[SDLTTSChunkFactory buildTTSChunkForString:@"Too late" type:[SDLSpeechCapabilities TEXT]]];
+    performOnlyChoiceInteraction.timeout = @5000;
+    performOnlyChoiceInteraction.interactionLayout = [SDLLayoutMode LIST_ONLY];
+    
+    [manager sendRequest:performOnlyChoiceInteraction withResponseHandler:^(__kindof SDLRPCRequest * _Nullable request, __kindof SDLPerformInteractionResponse * _Nullable response, NSError * _Nullable error) {
+        if ((response == nil) || (error != nil)) {
+            NSLog(@"Something went wrong, no perform interaction response: %@", error);
+        }
+        
+        if ([response.choiceID isEqualToNumber:@0]) {
+            [manager sendRequest:[self goodJobSpeak]];
+        } else {
+            [manager sendRequest:[self youMissedItSpeak]];
+        }
+    }];
 }
 
 
@@ -92,10 +214,21 @@ NSString *const SDLAppId = @"9999";
     }
 }
 
-- (void)managerDidDisconnect {}
+- (void)managerDidDisconnect {
+    self.firstTimeState = SDLHMIFirstStateNone;
+}
 
 - (void)hmiLevel:(SDLHMILevel *)oldLevel didChangeToLevel:(SDLHMILevel *)newLevel {
-    if ([newLevel isEqualToEnum:[SDLHMILevel FULL]]) {
+    if (![newLevel isEqualToEnum:[SDLHMILevel NONE]] && (self.firstTimeState == SDLHMIFirstStateNone)) {
+        // This is our first time in a non-NONE state
+        
+        // Send AddCommands
+        [self.sdlManager sendRequest:[self speakNameCommand]];
+        [self.sdlManager sendRequest:[self interactionSetCommand]];
+    }
+    
+    if ([newLevel isEqualToEnum:[SDLHMILevel FULL]] && (self.firstTimeState != SDLHMIFirstStateFull)) {
+        // This is our first time in a FULL state
         [self showInitialData];
     }
 }
