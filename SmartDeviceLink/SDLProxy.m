7fcd7ce8e695dff700d3fc58f97b534cf97f8c72
Joel Fischer <joeljfischer@gmail.com>
2016-11-07 10:24:17 -0500

    Update to v4.4.1

--- SmartDeviceLink/SDLProxy.m
+++ SmartDeviceLink/SDLProxy.m
@@ -49,7 +49,7 @@ typedef NSString SDLVehicleMake;
 typedef void (^URLSessionTaskCompletionHandler)(NSData *data, NSURLResponse *response, NSError *error);
 typedef void (^URLSessionDownloadTaskCompletionHandler)(NSURL *location, NSURLResponse *response, NSError *error);
 
-NSString *const SDLProxyVersion = @"4.4.0";
+NSString *const SDLProxyVersion = @"4.4.1";
 const float startSessionTime = 10.0;
 const float notifyProxyClosedDelay = 0.1;
 const int POLICIES_CORRELATION_ID = 65535;
