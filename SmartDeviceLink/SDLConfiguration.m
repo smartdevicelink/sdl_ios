461e7c4c555b2cbee58cfa1b1d13cb1d70eb35c0
Joel Fischer <joeljfischer@gmail.com>
2017-03-02 14:29:23 -0500

    Update `SDLConfiguration` to take a log configuration
    * This is a minor change, purely additive
    * Update with nullable pass-ins, so that defaults can be used more easily.

--- SmartDeviceLink/SDLConfiguration.m
+++ SmartDeviceLink/SDLConfiguration.m
@@ -10,29 +10,42 @@
 
 #import "SDLLifecycleConfiguration.h"
 #import "SDLLockScreenConfiguration.h"
+#import "SDLLogConfiguration.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @implementation SDLConfiguration
 
-- (instancetype)initWithLifecycle:(SDLLifecycleConfiguration *)lifecycleConfig lockScreen:(SDLLockScreenConfiguration *)lockScreenConfig {
+- (instancetype)initWithLifecycle:(SDLLifecycleConfiguration *)lifecycleConfig lockScreen:(nullable SDLLockScreenConfiguration *)lockScreenConfig {
+    return [self initWithLifecycle:lifecycleConfig lockScreen:lockScreenConfig logging:nil];
+}
+
++ (instancetype)configurationWithLifecycle:(SDLLifecycleConfiguration *)lifecycleConfig lockScreen:(nullable SDLLockScreenConfiguration *)lockScreenConfig {
+    return [[self alloc] initWithLifecycle:lifecycleConfig lockScreen:lockScreenConfig];
+}
+
+- (instancetype)initWithLifecycle:(SDLLifecycleConfiguration *)lifecycleConfig lockScreen:(nullable SDLLockScreenConfiguration *)lockScreenConfig logging:(nullable SDLLogConfiguration *)logConfig {
     self = [super init];
     if (!self) {
         return nil;
     }
 
     _lifecycleConfig = lifecycleConfig;
-    _lockScreenConfig = lockScreenConfig;
+    _lockScreenConfig = lockScreenConfig ? lockScreenConfig : [SDLLockScreenConfiguration enabledConfiguration];
+    _loggingConfig = logConfig ? logConfig : [SDLLogConfiguration defaultConfiguration];
 
     return self;
 }
 
-+ (instancetype)configurationWithLifecycle:(SDLLifecycleConfiguration *)lifecycleConfig lockScreen:(SDLLockScreenConfiguration *)lockScreenConfig {
-    return [[self alloc] initWithLifecycle:lifecycleConfig lockScreen:lockScreenConfig];
++ (instancetype)configurationWithLifecycle:(SDLLifecycleConfiguration *)lifecycleConfig lockScreen:(nullable SDLLockScreenConfiguration *)lockScreenConfig logging:(nullable SDLLogConfiguration *)logConfig {
+    return [[self alloc] initWithLifecycle:lifecycleConfig lockScreen:lockScreenConfig logging:logConfig];
 }
 
+
+#pragma mark - NSCopying
+
 - (id)copyWithZone:(nullable NSZone *)zone {
-    SDLConfiguration *new = [[SDLConfiguration allocWithZone:zone] initWithLifecycle : _lifecycleConfig lockScreen : _lockScreenConfig];
+    SDLConfiguration *new = [[SDLConfiguration allocWithZone:zone] initWithLifecycle: _lifecycleConfig lockScreen: _lockScreenConfig logging:_loggingConfig];
 
     return new;
 }
