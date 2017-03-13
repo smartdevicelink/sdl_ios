23e539c6fd29128075312885ab1c400532c89250
Muller, Alexander (A.) <amulle19@ford.com>
2017-03-13 11:27:46 -0700

    Added convenience isEqualToEnum function.

--- SmartDeviceLink/SDLEnum.h
+++ SmartDeviceLink/SDLEnum.h
@@ -5,4 +5,14 @@
 #import <Foundation/Foundation.h>
 #import "SDLMacros.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 typedef NSString* SDLEnum SDL_SWIFT_ENUM;
+
+@interface NSString (Enum)
+
+- (BOOL)isEqualToEnum:(SDLEnum)enumObj;
+
+@end
+
+NS_ASSUME_NONNULL_END
