0725229016400fdf89c2716579054d07c7d82fe3
Joel Fischer <joeljfischer@gmail.com>
2017-03-14 16:27:55 -0400

    THIS IS ONLY A TEST

---
+++ Test.swift
@@ -0,0 +1,17 @@
+//
+//  Test.swift
+//  SmartDeviceLink-iOS
+//
+//  Created by Joel Fischer on 3/14/17.
+//  Copyright © 2017 smartdevicelink. All rights reserved.
+//
+
+import Foundation
+import SmartDeviceLink
+
+class Test {
+    func test() {
+        let config = SDLLogConfiguration.debug()
+        let filter = SDLLogManager
+    }
+}
