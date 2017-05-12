de960280d9d7e18c7dcd87c4453beff40a4f0a92
BrettyWhite <geekman3454@protonmail.com>
2017-05-12 11:07:51 -0400

    adding SDL

---
+++ SmartDeviceLink-ExampleSwift/AppProperties.swift
@@ -0,0 +1,9 @@
+//
+//  AppProperties.swift
+//  SmartDeviceLink-iOS
+//
+//  Created by Brett McIsaac on 5/12/17.
+//  Copyright © 2017 smartdevicelink. All rights reserved.
+//
+
+import Foundation
---
+++ SmartDeviceLink-ExampleSwift/ProxyManager.swift
@@ -0,0 +1,9 @@
+//
+//  ProxyManager.swift
+//  SmartDeviceLink-iOS
+//
+//  Created by Brett McIsaac on 5/12/17.
+//  Copyright © 2017 smartdevicelink. All rights reserved.
+//
+
+import Foundation
