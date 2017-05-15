881bd5f9649713494439e518fa8aa63dd42b5102
BrettyWhite <geekman3454@protonmail.com>
2017-05-15 16:08:45 -0400

    removed custom preferences class, added nsdefaults

--- SmartDeviceLink-ExampleSwift/ConnectionTCPTableViewController.swift
+++ SmartDeviceLink-ExampleSwift/ConnectionTCPTableViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import SmartDeviceLink
 
-class ConnectionTCPTableViewController: UITableViewController {
+class ConnectionTCPTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var ipAddressTextField: UITextField!
     @IBOutlet weak var portTextField: UITextField!
     @IBOutlet weak var connectTableViewCell: UITableViewCell!
     @IBOutlet weak var connectButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Tableview setup
+        tableView.keyboardDismissMode = .onDrag
+        ipAddressTextField.text = UserDefaults.standard.string(forKey: "ipAddress")
+        portTextField.text = UserDefaults.standard.string(forKey: "port")
+        // Connect Button setup
+        connectButton.tintColor = UIColor.white
+    }
     
     
 }
