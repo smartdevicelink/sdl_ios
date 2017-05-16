8f590db900022510e49116d43810b07bdbe57aae
BrettyWhite <geekman3454@protonmail.com>
2017-05-16 14:14:03 -0400

    working build on actual phone

--- SmartDeviceLink-ExampleSwift/ConnectionIAPTableViewController.swift
+++ SmartDeviceLink-ExampleSwift/ConnectionIAPTableViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ConnectionIAPTableViewController: UITableViewController {
+class ConnectionIAPTableViewController: UITableViewController, ProxyManagerDelegate {
     
     @IBOutlet weak var connectTableViewCell: UITableViewCell!
     @IBOutlet weak var connectButton: UIButton!
+    
+    var state: ProxyState = ProxyState.ProxyStateStopped
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +27,43 @@ class ConnectionIAPTableViewController: UITableViewController {
     
     // MARK: - IBActions
     @IBAction func connectButtonWasPressed(_ sender: UIButton) {
-        // Initialize the SDL manager
-        _ = ProxyManager.sharedManager.connectIAP()
+        
+        // Initialize (or reset) the SDL manager
+        switch state {
+        case ProxyState.ProxyStateStopped:
+            ProxyManager.sharedManager.connectIAP()
+        case ProxyState.ProxyStateSearchingForConnection:
+            ProxyManager.sharedManager.reset()
+        case ProxyState.ProxyStateConnected:
+            ProxyManager.sharedManager.reset()
+        }
     }
     
-    
+    // MARK: - Delegate Functions
+    func didChangeProxyState(_ newState: ProxyState){
+        // Updates state from ProxyManager
+        state = newState
+        var newColor: UIColor? = nil
+        var newTitle: String? = nil
+        
+        switch newState {
+        case .ProxyStateStopped:
+            newColor = UIColor.red
+            newTitle = "Connect"
+        case .ProxyStateSearchingForConnection:
+            newColor = UIColor.blue
+            newTitle = "Stop Searching"
+        case .ProxyStateConnected:
+            newColor = UIColor.green
+            newTitle = "Disconnect"
+        }
+        
+        if (newColor != nil) || (newTitle != nil) {
+            DispatchQueue.main.async(execute: {() -> Void in
+                self.connectTableViewCell.backgroundColor = newColor
+                self.connectButton.setTitle(newTitle, for: .normal)
+                self.connectButton.setTitleColor(.white, for: .normal)
+            })
+        }
+    }
 }
