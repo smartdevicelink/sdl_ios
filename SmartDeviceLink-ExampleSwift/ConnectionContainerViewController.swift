f8aee24ed1f53e1576c2eb0c25f0035aad47cbf1
BrettyWhite <geekman3454@protonmail.com>
2017-05-19 09:41:33 -0400

    added swipe gesture

--- SmartDeviceLink-ExampleSwift/ConnectionContainerViewController.swift
+++ SmartDeviceLink-ExampleSwift/ConnectionContainerViewController.swift
@@ -15,21 +15,26 @@ class ConnectionContainerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
         navigationController?.navigationBar.isTranslucent = false
-        // Create object of storyboards
+
         let tcpControllerStoryboard = UIStoryboard(name: "ConnectionTCPTableViewController", bundle: nil)
         let iapControllerStoryboard = UIStoryboard(name: "ConnectionIAPTableViewController", bundle: nil)
-        // Create object of viewController
         let tcpController = tcpControllerStoryboard.instantiateViewController(withIdentifier :"ConnectionTCPTableViewController")
         let iapController = iapControllerStoryboard.instantiateViewController(withIdentifier :"ConnectionIAPTableViewController")
-        // Add view controllers to array
         viewControllers.add(tcpController)
         viewControllers.add(iapController)
-        // set segmentedControl to left value (TCP)
+
         segmentedControl.selectedSegmentIndex = 0
-        // Call the to add initial VC as SubView
+
         loadChildViewController(index: 0)
+
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(slideToLeftWithGestureRecognizer(gestureRecognizer:)))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(slideToRightWithGestureRecognizer(gestureRecognizer:)))
+        leftSwipe.direction = .left
+        rightSwipe.direction = .right
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,19 +42,33 @@ class ConnectionContainerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func  slideToLeftWithGestureRecognizer(gestureRecognizer:UISwipeGestureRecognizer)
+    {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            segmentedControl.selectedSegmentIndex = 1
+            removeFromView()
+            loadChildViewController(index: 1)
+        }
+    }
+    @IBAction func slideToRightWithGestureRecognizer
+        (gestureRecognizer:UISwipeGestureRecognizer)
+    {
+        if segmentedControl.selectedSegmentIndex == 1 {
+            segmentedControl.selectedSegmentIndex = 0
+            removeFromView()
+            loadChildViewController(index: 0)
+        }
+    }
+    
     // Grab changes in segmentedControl
     @IBAction func indexChanged(_ sender: AnyObject) {
         switch segmentedControl.selectedSegmentIndex
         {
         case 0:
-            // Remove other VC from view
             removeFromView()
-            // Load up the VC we want
             loadChildViewController(index: 0)
         case 1:
-            // Remove other VC from view
             removeFromView()
-            // Load up the VC we want
             loadChildViewController(index: 1)
         default:
             break
