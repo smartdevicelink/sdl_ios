bda8e3d7639510e9e25a5141412cf8fb08e7c318
Joel Fischer <joeljfischer@gmail.com>
2017-07-31 10:55:34 -0400

    Add updates for SDLTouchType enum with cancel

--- SmartDeviceLink/SDLTouchType.h
+++ SmartDeviceLink/SDLTouchType.h
@@ -9,3 +9,4 @@ typedef SDLEnum SDLTouchType SDL_SWIFT_ENUM;
 extern SDLTouchType const SDLTouchTypeBegin;
 extern SDLTouchType const SDLTouchTypeMove;
 extern SDLTouchType const SDLTouchTypeEnd;
+extern SDLTouchType const SDLTouchTypeCancel;
