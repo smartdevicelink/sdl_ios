9870bf4552922f96c9566fac6d144401f237003f
Brett W <brett@buhz.com>
2017-08-02 13:56:14 -0400

    started work, added enum

---
+++ SmartDeviceLink/SDLTextFieldType.h
@@ -0,0 +1,75 @@
+//
+//  SDLTextFieldType.h
+//  SmartDeviceLink-iOS
+//
+//  Created by Brett McIsaac on 8/2/17.
+//  Copyright © 2017 smartdevicelink. All rights reserved.
+//
+
+#import "SDLEnum.h"
+
+/**
+ * Text Field Types
+ */
+typedef SDLEnum SDLTextFieldType SDL_SWIFT_ENUM;
+
+/**
+ * @abstract SDLTextFieldType: *mediaTitle*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeMediaTitle;
+
+/**
+ * @abstract SDLTextFieldType: *mediaArtist*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeMediaArtist;
+
+/**
+ * @abstract SDLTextFieldType: *mediaAlbum*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeMediaAlbum;
+
+/**
+ * @abstract SDLTextFieldType: *mediaYear*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeMediaYear;
+
+/**
+ * @abstract SDLTextFieldType: *mediaGenre*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeMediaGenre;
+
+/**
+ * @abstract SDLTextFieldType: *mediaStation*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeMediaStation;
+
+/**
+ * @abstract SDLTextFieldType: *rating*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeRating;
+
+/**
+ * @abstract SDLTextFieldType: *currentTemperature*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeCurrentTemperature;
+
+/**
+ * @abstract SDLTextFieldType: *maximumTemperature*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeMaximumTemperature;
+
+/**
+ * @abstract SDLTextFieldType: *minimumTemperature*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeMinimumTemperature;
+
+/**
+ * @abstract SDLTextFieldType: *weatherTerm*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeWeatherTerm;
+
+/**
+ * @abstract SDLTextFieldType: *humidity*
+ */
+extern SDLTextFieldType const SDLTextFieldTypeHumidity;
+
