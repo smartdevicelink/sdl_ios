c988e502a7bf21524976315eff95629660f7d97f
NicoleYarroch <nicole@livio.io>
2017-08-03 15:53:22 -0400

    Added SDLSpatialStruct class
    Signed-off-by: NicoleYarroch <nicole@livio.io>

---
+++ SmartDeviceLink/SDLSpatialStruct.h
@@ -0,0 +1,51 @@
+//
+//  SDLSpatialStruct.h
+//  SmartDeviceLink-iOS
+//
+//  Created by Nicole on 8/3/17.
+//  Copyright © 2017 smartdevicelink. All rights reserved.
+//
+
+#import "SDLRPCStruct.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ *  Defines spatial for each user control object for video streaming application
+ */
+@interface SDLSpatialStruct : SDLRPCStruct
+
+- (instancetype)initWithId:(UInt32)id x:(NSN)
+/**
+ *  A user control spatial identifier
+ *  Required, Integer, 0 - 2,000,000,000
+ */
+@property (strong, nonatomic) NSNumber<SDLUInt> *id;
+
+/**
+ *  The X-coordinate of the user control
+ *  Required, Float
+ */
+@property (strong, nonatomic) NSNumber<SDLFloat> *x;
+
+/**
+ *  The Y-coordinate of the user control
+ *  Required, Float
+ */
+@property (strong, nonatomic) NSNumber<SDLFloat> *y;
+
+/**
+ *  The width of the user control's bounding rectangle
+ *  Required, Float
+ */
+@property (strong, nonatomic) NSNumber<SDLFloat> *width;
+
+/**
+ *  The height of the user control's bounding rectangle
+ *  Required, Float
+ */
+@property (strong, nonatomic) NSNumber<SDLFloat> *height;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SmartDeviceLink/SDLSpatialStruct.m
@@ -0,0 +1,86 @@
+//
+//  SDLSpatialStruct.m
+//  SmartDeviceLink-iOS
+//
+//  Created by Nicole on 8/3/17.
+//  Copyright © 2017 smartdevicelink. All rights reserved.
+//
+
+#import "SDLSpatialStruct.h"
+#import "SDLNames.h"
+
+@implementation SDLSpatialStruct
+
+- (instancetype)init {
+    if (self = [super init]) {
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSMutableDictionary *)dict {
+    if (self = [super initWithDictionary:dict]) {
+    }
+    return self;
+}
+
+//- (void)setId:(NSNumber *)id {
+//    if (id != nil) {
+//        [store setObject:id forKey:NAMES_id];
+//    } else {
+//        [store removeObjectForKey:NAMES_id];
+//    }
+//}
+//
+//- (NSNumber *)id {
+//    return [store objectForKey:NAMES_id];
+//}
+//
+//- (void)setX:(NSNumber *)x {
+//    if (x != nil) {
+//        [store setObject:x forKey:NAMES_x];
+//    } else {
+//        [store removeObjectForKey:NAMES_x];
+//    }
+//}
+//
+//- (NSNumber *)x {
+//    return [store objectForKey:NAMES_x];
+//}
+//
+//- (void)setY:(NSNumber *)y {
+//    if (y != nil) {
+//        [store setObject:y forKey:NAMES_y];
+//    } else {
+//        [store removeObjectForKey:NAMES_y];
+//    }
+//}
+//
+//- (NSNumber *)y {
+//    return [store objectForKey:NAMES_y];
+//}
+//
+//- (void)setWidth:(NSNumber *)width {
+//    if (width != nil) {
+//        [store setObject:width forKey:NAMES_width];
+//    } else {
+//        [store removeObjectForKey:NAMES_width];
+//    }
+//}
+//
+//- (NSNumber *)width {
+//    return [store objectForKey:NAMES_width];
+//}
+//
+//- (void)setHeight:(NSNumber *)height {
+//    if (height != nil) {
+//        [store setObject:height forKey:NAMES_height];
+//    } else {
+//        [store removeObjectForKey:NAMES_height];
+//    }
+//}
+//
+//- (NSNumber *)height {
+//    return [store objectForKey:NAMES_height];
+//}
+
+@end
