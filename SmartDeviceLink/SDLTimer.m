4c5f31cd2824c642cfd668d8495b541de3b91ea1
William Wallace <wwallace@mobiquityinc.com>
2017-08-24 18:12:29 -0700

    #382 Timers are not appropriately stopped when deallocated.
    https://github.com/smartdevicelink/sdl_ios/issues/382
    
    NSTimer has a strong reference to its target, therefore the SDLTimer never gets released and dealloc isn’t called.
    I eliminated the deadlock by adding a separate target.

--- SmartDeviceLink/SDLTimer.m
+++ SmartDeviceLink/SDLTimer.m
@@ -4,6 +4,36 @@
 
 #import "SDLTimer.h"
 
+@protocol SDLTimerTargetDelegate <NSObject>
+
+- (void)timerElapsed;
+
+@end
+
+@interface SDLTimerTarget : NSObject
+
+@property (nonatomic, weak) id<SDLTimerTargetDelegate> delegate;
+
+@end
+
+@implementation SDLTimerTarget
+
+- (instancetype)initWithDelegate:(id)delegate {
+    self = [super init];
+    if (self) {
+        _delegate = delegate;
+    }
+    return self;
+}
+
+- (void)timerElapsed {
+    if ([self.delegate conformsToProtocol:@protocol(SDLTimerTargetDelegate)]) {
+        [_delegate timerElapsed];
+    }
+}
+
+@end
+
 
 @interface SDLTimer ()
 
@@ -40,7 +70,9 @@
 - (void)start {
     if (self.duration > 0) {
         [self stopAndDestroyTimer];
-        self.timer = [NSTimer timerWithTimeInterval:self.duration target:self selector:@selector(timerElapsed) userInfo:nil repeats:self.repeat];
+        
+        SDLTimerTarget *timerTarget = [[SDLTimerTarget alloc] initWithDelegate:self];
+        self.timer = [NSTimer timerWithTimeInterval:_duration target:timerTarget selector:@selector(timerElapsed) userInfo:nil repeats:_repeat];
         [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
         self.timerRunning = YES;
     }
