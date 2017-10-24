e87a42cc12c337ffd18f8acc2ca900933514474c
Joel Fischer <joeljfischer@gmail.com>
2017-10-24 14:26:51 +0300

    Create a better crash in DEBUG if the dev forgets to add a protocol string

--- SmartDeviceLink/SDLIAPTransport.m
+++ SmartDeviceLink/SDLIAPTransport.m
@@ -253,6 +253,12 @@ int const ProtocolIndexTimeoutSeconds = 20;
  */
 - (BOOL)sdl_connectAccessory:(EAAccessory *)accessory {
     BOOL connecting = NO;
+    if ([self.class sdl_supportsRequiredProtocolStrings] != nil) {
+        NSString *failedString = [self.class sdl_supportsRequiredProtocolStrings];
+        NSAssert(NO, @"Some SDL protocol strings are not supported, check the README for all strings that must be included in your info.plist file. Missing string: %@", failedString);
+        return connecting;
+    }
+
     if ([accessory supportsProtocol:MultiSessionProtocolString] && SDL_SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"9")) {
         [self sdl_createIAPDataSessionWithAccessory:accessory forProtocol:MultiSessionProtocolString];
         connecting = YES;
@@ -264,6 +270,32 @@ int const ProtocolIndexTimeoutSeconds = 20;
         connecting = YES;
     }
     return connecting;
+}
+
+/**
+ Check all required protocol strings in the info.plist dictionary.
+
+ @return A missing protocol string or nil if all strings are supported.
+ */
++ (nullable NSString *)sdl_supportsRequiredProtocolStrings {
+    NSArray<NSString *> *protocolStrings = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"UISupportedExternalAccessoryProtocols"];
+
+    if (![protocolStrings containsObject:MultiSessionProtocolString]) {
+        return MultiSessionProtocolString;
+    }
+
+    if (![protocolStrings containsObject:LegacyProtocolString]) {
+        return LegacyProtocolString;
+    }
+
+    for (int i = 0; i < 30; i++) {
+        NSString *indexedProtocolString = [NSString stringWithFormat:@"%@%i", IndexedProtocolStringPrefix, i];
+        if (![protocolStrings containsObject:indexedProtocolString]) {
+            return indexedProtocolString;
+        }
+    }
+
+    return nil;
 }
 
 /**
