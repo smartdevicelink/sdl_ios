43089746131b08ba459aa2fc6851e1f0206580cf
Joel Fischer <joeljfischer@gmail.com>
2017-12-13 10:27:06 -0500

    CarWindow ViewController to make orientations easier

---
+++ SmartDeviceLink/SDLCarWindowViewController.h
@@ -0,0 +1,15 @@
+//
+//  SDLCarWindowViewController.h
+//  SmartDeviceLink
+//
+//  Created by Joel Fischer on 12/12/17.
+//  Copyright © 2017 smartdevicelink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDLCarWindowViewController : UIViewController
+
+@property (nonatomic, assign) UIInterfaceOrientation supportedOrientation;
+
+@end
