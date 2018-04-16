32e5207b8f277325931f67c49bf9846bd07e5fa2
NicoleYarroch <nicole@livio.io>
2018-04-16 16:21:33 -0400

    Replaced deleted SDLSwift Target
    - added embedded binary to Swift example app
    
    Signed-off-by: NicoleYarroch <nicole@livio.io>

--- SmartDeviceLink_Example/ProxyManager.swift
+++ SmartDeviceLink_Example/ProxyManager.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import SmartDeviceLink
-//import SmartDeviceLinkSwift
+import SmartDeviceLinkSwift
 
 class ProxyManager: NSObject {
     fileprivate var sdlManager: SDLManager!
@@ -93,7 +93,7 @@ private extension ProxyManager {
     func startManager() {
         sdlManager.start(readyHandler: { [unowned self] (success, error) in
             guard success else {
-//                SDLLog.d("something")
+                SDLLog.d("something")
                 print("There was an error while starting up: \(String(describing: error))")
                 self.resetConnection()
                 return
