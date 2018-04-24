35b95b97574eac28c516212ff83632a2c34817e1
NicoleYarroch <nicole@livio.io>
2018-04-24 10:50:51 -0400

    Vehicle data framework added to Obj-C ex. app
    Signed-off-by: NicoleYarroch <nicole@livio.io>

---
+++ SmartDeviceLink_Example/VehicleDataManager.h
@@ -0,0 +1,13 @@
+//
+//  VehicleDataManager.h
+//  SmartDeviceLink-Example-ObjC
+//
+//  Created by Nicole on 4/23/18.
+//  Copyright © 2018 smartdevicelink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VehicleDataManager : NSObject
+
+@end
---
+++ SmartDeviceLink_Example/VehicleDataManager.m
@@ -0,0 +1,13 @@
+//
+//  VehicleDataManager.m
+//  SmartDeviceLink-Example-ObjC
+//
+//  Created by Nicole on 4/23/18.
+//  Copyright © 2018 smartdevicelink. All rights reserved.
+//
+
+#import "VehicleDataManager.h"
+
+@implementation VehicleDataManager
+
+@end
