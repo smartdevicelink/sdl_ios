1295e6eefb4a381460274ca4f2c2ec51dea5c268
NicoleYarroch <nicole@livio.io>
2018-05-08 10:40:33 -0400

    Fixed incorrect data type
    Signed-off-by: NicoleYarroch <nicole@livio.io>

--- SmartDeviceLink/SDLMenuManager.m
+++ SmartDeviceLink/SDLMenuManager.m
@@ -101,7 +101,7 @@ UInt32 const MenuCellIdMin = 1;
     _inProgressUpdate = nil;
     _hasQueuedUpdate = NO;
     _waitingOnHMIUpdate = NO;
-    _waitingUpdateMenuCells = NO;
+    _waitingUpdateMenuCells = nil;
 }
 
 #pragma mark - Setters
