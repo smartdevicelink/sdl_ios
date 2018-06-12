470c3203dff6739be6209adb8df5752306bf6977
Joel Fischer <joeljfischer@gmail.com>
2018-06-12 17:27:10 -0400

    Stub present choice set tests

--- SmartDeviceLinkTests/DevAPISpecs/SDLPresentChoiceSetOperationSpec.m
+++ SmartDeviceLinkTests/DevAPISpecs/SDLPresentChoiceSetOperationSpec.m
@@ -1,6 +1,12 @@
 #import <Quick/Quick.h>
 #import <Nimble/Nimble.h>
 
+#import "SDLPresentChoiceSetOperation.h"
+
 QuickSpecBegin(SDLPresentChoiceSetOperationSpec)
+
+describe(@"present choice operation", ^{
+    
+});
 
 QuickSpecEnd
