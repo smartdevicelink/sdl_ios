650f235dc0eac9a15159c4f922ebade46ad8c56f
Joel Fischer <joeljfischer@gmail.com>
2018-06-12 17:35:43 -0400

    Additional small updates

--- SmartDeviceLinkTests/DevAPISpecs/SDLPresentChoiceSetOperationSpec.m
+++ SmartDeviceLinkTests/DevAPISpecs/SDLPresentChoiceSetOperationSpec.m
@@ -1,12 +1,36 @@
 #import <Quick/Quick.h>
 #import <Nimble/Nimble.h>
+#import <OCMock/OCMock.h>
 
 #import "SDLPresentChoiceSetOperation.h"
 
+#import "SDLKeyboardDelegate.h"
+#import "SDLKeyboardProperties.h"
+#import "TestConnectionManager.h"
+
 QuickSpecBegin(SDLPresentChoiceSetOperationSpec)
 
-describe(@"present choice operation", ^{
-    
+fdescribe(@"present choice operation", ^{
+    __block TestConnectionManager *testConnectionManager = nil;
+    __block SDLPresentChoiceSetOperation *testOp = nil;
+
+    __block NSString *testInitialText = @"Initial Text";
+    __block id<SDLKeyboardDelegate> testDelegate = nil;
+    __block SDLKeyboardProperties *testInitialProperties = nil;
+
+    __block BOOL hasCalledOperationCompletionHandler = NO;
+    __block NSError *resultError = nil;
+
+    beforeEach(^{
+        resultError = nil;
+        hasCalledOperationCompletionHandler = NO;
+
+        testConnectionManager = [[TestConnectionManager alloc] init];
+        testDelegate = OCMProtocolMock(@protocol(SDLKeyboardDelegate));
+        OCMStub([testDelegate customKeyboardConfiguration]).andReturn(nil);
+
+        testInitialProperties = [[SDLKeyboardProperties alloc] initWithLanguage:SDLLanguageArSa layout:SDLKeyboardLayoutAZERTY keypressMode:SDLKeypressModeResendCurrentEntry limitedCharacterList:nil autoCompleteText:nil];
+    });
 });
 
 QuickSpecEnd
