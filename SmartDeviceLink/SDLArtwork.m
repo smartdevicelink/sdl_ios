36d0bee4d5ed389df8d1b6c13ab8527a5f0f5acf
NicoleYarroch <nicole@livio.io>
2018-06-18 10:06:03 -0400

    Fixed function spacing in `SDLArtwork` class
    Signed-off-by: NicoleYarroch <nicole@livio.io>

--- SmartDeviceLink/SDLArtwork.m
+++ SmartDeviceLink/SDLArtwork.m
@@ -22,7 +22,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @implementation SDLArtwork
 
--(void)setImage:(UIImage *)image {
+- (void)setImage:(UIImage *)image {
     _image = image;
     _isTemplate = (image.renderingMode == UIImageRenderingModeAlwaysTemplate);
 }
