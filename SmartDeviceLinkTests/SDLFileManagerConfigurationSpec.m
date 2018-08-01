64b85f58626735ae29771ed7f5bbeb0a296a6559
NicoleYarroch <nicole@livio.io>
2018-08-01 10:11:38 -0400

    Added a SDLFileManagerConfigurationSpec file
    Signed-off-by: NicoleYarroch <nicole@livio.io>

---
+++ SmartDeviceLinkTests/SDLFileManagerConfigurationSpec.m
@@ -0,0 +1,9 @@
+//
+//  SDLFileManagerConfigurationSpec.m
+//  SmartDeviceLinkTests
+//
+//  Created by Nicole on 8/1/18.
+//  Copyright © 2018 smartdevicelink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
