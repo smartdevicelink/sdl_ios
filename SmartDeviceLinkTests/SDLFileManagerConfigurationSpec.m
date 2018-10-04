bf25720ccd78051d22438d2dcbe2a16354ca1254
Joel Fischer <joeljfischer@gmail.com>
2018-10-04 10:30:43 -0400

    Fix tests

--- SmartDeviceLinkTests/SDLFileManagerConfigurationSpec.m
+++ SmartDeviceLinkTests/SDLFileManagerConfigurationSpec.m
@@ -17,18 +17,11 @@ describe(@"A file manager configuration", ^{
     __block SDLFileManagerConfiguration *testConfig = nil;
 
     it(@"should get and set correctly", ^{
-        testConfig = [[SDLFileManagerConfiguration alloc] init];
+        testConfig = [SDLFileManagerConfiguration defaultConfiguration];
         testConfig.artworkRetryCount = 5;
         testConfig.fileRetryCount = 1;
 
         expect(testConfig.artworkRetryCount).to(equal(5));
-        expect(testConfig.fileRetryCount).to(equal(1));
-    });
-
-    it(@"should be set to default configuration if parameters are not set", ^{
-        testConfig = [[SDLFileManagerConfiguration alloc] init];
-
-        expect(testConfig.artworkRetryCount).to(equal(1));
         expect(testConfig.fileRetryCount).to(equal(1));
     });
 
