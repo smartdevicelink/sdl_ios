f9c3a079aa58d70af8aa038e7fa6eba0b8d44ac9
NicoleYarroch <nicole@livio.io>
2019-01-28 09:03:29 -0500

    Added `AppServiceRecord` interface file

---
+++ SmartDeviceLink/SDLAppServiceRecord.m
@@ -0,0 +1,16 @@
+//
+//  SDLAppServiceRecord.m
+//  SmartDeviceLink
+//
+//  Created by Nicole on 1/25/19.
+//  Copyright © 2019 smartdevicelink. All rights reserved.
+//
+
+#import "SDLAppServiceRecord.h"
+#import "NSMutableDictionary+Store.h"
+#import "SDLNames.h"
+
+@implementation SDLAppServiceRecord
+
+
+@end
