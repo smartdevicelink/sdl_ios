53d67258b1d67604309fc78b6874838ce799b58d
NicoleYarroch <nicole@livio.io>
2019-01-30 13:37:04 -0500

    Added enum test cases
    Added the SDLAppServiceTypeSpec and SDLServiceUpdateReasonSpec

--- SmartDeviceLinkTests/RPCSpecs/EnumSpecs/SDLAppServiceTypeSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/EnumSpecs/SDLAppServiceTypeSpec.m
@@ -7,3 +7,27 @@
 //
 
 #import <Foundation/Foundation.h>
+
+#import <Quick/Quick.h>
+#import <Nimble/Nimble.h>
+
+#import "SDLAppServiceType.h"
+
+
+QuickSpecBegin(SDLAppServiceTypeSpec)
+
+describe(@"Individual Enum Value Tests", ^{
+    it(@"Should match internal values", ^{
+        expect(SDLAppServiceTypeMedia).to(equal(@"MEDIA"));
+        expect(SDLAppServiceTypeWeather).to(equal(@"WEATHER"));
+        expect(SDLAppServiceTypeNavigation).to(equal(@"NAVIGATION"));
+        expect(SDLAppServiceTypeVoiceAssistant).to(equal(@"VOICE_ASSISTANT"));
+        expect(SDLAppServiceTypeGeneric).to(equal(@"GENERIC"));
+        expect(SDLAppServiceTypeCommunicationVOIP).to(equal(@"COMMUNICATION_VOIP"));
+        expect(SDLAppServiceTypeMessaging).to(equal(@"MESSAGING"));
+        expect(SDLAppServiceTypeTTS).to(equal(@"TTS"));
+    });
+});
+
+QuickSpecEnd
+
--- SmartDeviceLinkTests/RPCSpecs/EnumSpecs/SDLServiceUpdateReasonSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/EnumSpecs/SDLServiceUpdateReasonSpec.m
@@ -6,4 +6,21 @@
 //  Copyright © 2019 smartdevicelink. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Quick/Quick.h>
+#import <Nimble/Nimble.h>
+
+#import "SDLServiceUpdateReason.h"
+
+QuickSpecBegin(SDLServiceUpdateReasonSpec)
+
+describe(@"Individual Enum Value Tests", ^{
+    it(@"Should match internal values", ^{
+        expect(SDLServiceUpdatePublished).to(equal(@"PUBLISHED"));
+        expect(SDLServiceUpdateRemoved).to(equal(@"REMOVED"));
+        expect(SDLServiceUpdateActivated).to(equal(@"ACTIVATED"));
+        expect(SDLServiceUpdateDeactivated).to(equal(@"DEACTIVATED"));
+        expect(SDLServiceUpdateManifestUpdate).to(equal(@"MANIFEST_UPDATE"));
+    });
+});
+
+QuickSpecEnd
