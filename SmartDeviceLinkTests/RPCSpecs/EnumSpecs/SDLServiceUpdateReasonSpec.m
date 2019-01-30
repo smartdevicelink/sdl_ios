9f3548220a06089b94ed5a99203730f42d8fc88a
NicoleYarroch <nicole@livio.io>
2019-01-30 13:32:26 -0500

    Added enum test cases
    Added the SDLServiceUpdateReasonSpec and the SDLAppServiceTypeSpec

---
+++ SmartDeviceLinkTests/RPCSpecs/EnumSpecs/SDLServiceUpdateReasonSpec.m
@@ -0,0 +1,9 @@
+//
+//  SDLServiceUpdateReasonSpec.m
+//  SmartDeviceLinkTests
+//
+//  Created by Nicole on 1/30/19.
+//  Copyright © 2019 smartdevicelink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
