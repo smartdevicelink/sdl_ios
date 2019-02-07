d7ebe748aa3630ecce433d8cf63c00fb5521421a
NicoleYarroch <nicole@livio.io>
2019-02-07 16:16:22 -0500

    Added GetFileResponse class and tests

---
+++ SmartDeviceLink/SDLGetFileResponse.m
@@ -0,0 +1,57 @@
+//
+//  SDLGetFileResponse.m
+//  SmartDeviceLink
+//
+//  Created by Nicole on 2/7/19.
+//  Copyright © 2019 smartdevicelink. All rights reserved.
+//
+
+#import "SDLGetFileResponse.h"
+#import "NSMutableDictionary+Store.h"
+#import "SDLNames.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation SDLGetFileResponse
+
+- (instancetype)init {
+    if (self = [super initWithName:SDLNameGetFile]) {
+    }
+    return self;
+}
+
+- (void)setOffset:(nullable NSNumber<SDLUInt> *)offset {
+    [parameters sdl_setObject:offset forName:SDLNameOffset];
+}
+
+- (nullable NSNumber<SDLUInt> *)offset {
+    return [parameters sdl_objectForName:SDLNameOffset];
+}
+
+- (void)setLength:(nullable NSNumber<SDLUInt> *)length {
+    [parameters sdl_setObject:length forName:SDLNameLength];
+}
+
+- (nullable NSNumber<SDLUInt> *)length {
+    return [parameters sdl_objectForName:SDLNameLength];
+}
+
+- (void)setFileType:(nullable SDLFileType)fileType {
+    [parameters sdl_setObject:fileType forName:SDLNameFileType];
+}
+
+- (nullable SDLFileType)fileType {
+    return [parameters sdl_objectForName:SDLNameFileType];
+}
+
+- (void)setCrc:(nullable NSNumber<SDLUInt> *)crc {
+    [parameters sdl_setObject:crc forName:SDLNameCRC];
+}
+
+- (nullable NSNumber<SDLUInt> *)crc {
+    return [parameters sdl_objectForName:SDLNameCRC];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SmartDeviceLinkTests/RPCSpecs/ResponseSpecs/SDLGetFileResponseSpec.m
@@ -0,0 +1,71 @@
+//
+//  SDLGetFileResponseSpec.m
+//  SmartDeviceLinkTests
+//
+//  Created by Nicole on 2/7/19.
+//  Copyright © 2019 smartdevicelink. All rights reserved.
+//
+
+#import <Quick/Quick.h>
+#import <Nimble/Nimble.h>
+
+#import "SDLFileType.h"
+#import "SDLGetFileResponse.h"
+#import "SDLNames.h"
+
+QuickSpecBegin(SDLGetFileResponseSpec)
+
+describe(@"Getter/Setter Tests", ^{
+    __block int testOffset = 34;
+    __block int testLength = 2;
+    __block SDLFileType testFileType = nil;
+    __block long testCrc = 4294967295;
+
+    beforeEach(^{
+        testFileType = SDLFileTypeJPEG;
+    });
+
+    it(@"Should set and get correctly", ^{
+        SDLGetFileResponse *testResponse = [[SDLGetFileResponse alloc] init];
+        testResponse.offset = @(testOffset);
+        testResponse.length = @(testLength);
+        testResponse.fileType = testFileType;
+        testResponse.crc = @(testCrc);
+
+        expect(testResponse.offset).to(equal(testOffset));
+        expect(testResponse.length).to(equal(testLength));
+        expect(testResponse.fileType).to(equal(testFileType));
+        expect(testResponse.crc).to(equal(testCrc));
+    });
+
+    it(@"Should get correctly when initialized with a dictionary", ^{
+        NSDictionary *dict = @{SDLNameResponse:@{
+                                       SDLNameParameters:@{
+                                               SDLNameOffset:@(testOffset),
+                                               SDLNameLength:@(testLength),
+                                               SDLNameFileType:testFileType,
+                                               SDLNameCRC:@(testCrc)
+                                               },
+                                       SDLNameOperationName:SDLNameGetFile}};
+        SDLGetFileResponse *testResponse = [[SDLGetFileResponse alloc] initWithDictionary:dict];
+
+        expect(testResponse.offset).to(equal(testOffset));
+        expect(testResponse.length).to(equal(testLength));
+        expect(testResponse.fileType).to(equal(testFileType));
+        expect(testResponse.crc).to(equal(testCrc));
+    });
+
+    it(@"Should return nil if not set", ^{
+        SDLGetFileResponse *testResponse = [[SDLGetFileResponse alloc] init];
+
+        expect(testResponse.offset).to(beNil());
+        expect(testResponse.length).to(beNil());
+        expect(testResponse.fileType).to(beNil());
+        expect(testResponse.crc).to(beNil());
+    });
+});
+
+QuickSpecEnd
+
+
+
