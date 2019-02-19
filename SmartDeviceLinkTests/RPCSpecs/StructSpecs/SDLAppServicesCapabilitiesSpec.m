713b7fdc154bf1760bed77944834163f208f4e28
NicoleYarroch <nicole@livio.io>
2019-02-19 09:59:09 -0500

    Removed param from `AppServicesCapabilities`
    Removed the `servicesSupported` param from `AppServicesCapabilities` and test cases

--- SmartDeviceLinkTests/RPCSpecs/StructSpecs/SDLAppServicesCapabilitiesSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/StructSpecs/SDLAppServicesCapabilitiesSpec.m
@@ -16,44 +16,36 @@
 QuickSpecBegin(SDLAppServicesCapabilitiesSpec)
 
 describe(@"Getter/Setter Tests", ^{
-    __block NSArray<SDLAppServiceType> *testServicesSupported = nil;
     __block NSArray<SDLAppServiceCapability *> *testAppServices = nil;
 
     beforeEach(^{
-        testServicesSupported = @[SDLAppServiceTypeMedia, SDLAppServiceTypeWeather];
         testAppServices = @[[[SDLAppServiceCapability alloc] initWithDictionary:@{SDLNameUpdateReason:SDLServiceUpdateRemoved}]];
     });
 
     it(@"Should set and get correctly", ^{
         SDLAppServicesCapabilities *testStruct = [[SDLAppServicesCapabilities alloc] init];
-        testStruct.servicesSupported = testServicesSupported;
         testStruct.appServices = testAppServices;
 
-        expect(testStruct.servicesSupported).to(equal(testServicesSupported));
         expect(testStruct.appServices).to(equal(testAppServices));
     });
 
     it(@"Should set and get with initWithServicesSupported:appServices:", ^{
-        SDLAppServicesCapabilities *testStruct = [[SDLAppServicesCapabilities alloc] initWithServicesSupported:testServicesSupported appServices:testAppServices];
+        SDLAppServicesCapabilities *testStruct = [[SDLAppServicesCapabilities alloc] initWithAppServices:testAppServices];
 
-        expect(testStruct.servicesSupported).to(equal(testServicesSupported));
         expect(testStruct.appServices).to(equal(testAppServices));
     });
 
     it(@"Should get correctly when initialized with a dictionary", ^{
-        NSDictionary *dict = @{SDLNameServicesSupported:testServicesSupported,
-                               SDLNameAppServices:testAppServices
+        NSDictionary *dict = @{SDLNameAppServices:testAppServices
                                };
         SDLAppServicesCapabilities *testStruct = [[SDLAppServicesCapabilities alloc] initWithDictionary:dict];
 
-        expect(testStruct.servicesSupported).to(equal(testServicesSupported));
         expect(testStruct.appServices).to(equal(testAppServices));
     });
 
     it(@"Should return nil if not set", ^{
         SDLAppServicesCapabilities *testStruct = [[SDLAppServicesCapabilities alloc] init];
 
-        expect(testStruct.servicesSupported).to(beNil());
         expect(testStruct.appServices).to(beNil());
     });
 });
