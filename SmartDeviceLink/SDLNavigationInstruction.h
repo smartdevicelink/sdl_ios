6b69fd4fc3f4daa763a694412c14e29dabd4f883
NicoleYarroch <nicole@livio.io>
2019-02-22 11:33:48 -0500

    Added SDLNavigationInstruction class

---
+++ SmartDeviceLink/SDLNavigationInstruction.h
@@ -0,0 +1,20 @@
+//
+//  SDLNavigationInstruction.h
+//  SmartDeviceLink
+//
+//  Created by Nicole on 2/22/19.
+//  Copyright © 2019 smartdevicelink. All rights reserved.
+//
+
+#import "SDLRPCMessage.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/*
+ *
+ */
+@interface SDLNavigationInstruction : SDLRPCStruct
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SmartDeviceLink/SDLNavigationInstruction.m
@@ -0,0 +1,17 @@
+//
+//  SDLNavigationInstruction.m
+//  SmartDeviceLink
+//
+//  Created by Nicole on 2/22/19.
+//  Copyright © 2019 smartdevicelink. All rights reserved.
+//
+
+#import "SDLNavigationInstruction.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation SDLNavigationInstruction
+
+@end
+
+NS_ASSUME_NONNULL_END
