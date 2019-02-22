5a6314aaa584e5373b73561f531129b2e5729103
NicoleYarroch <nicole@livio.io>
2019-02-22 16:08:07 -0500

    Added SDLNavigationServiceData header params

---
+++ SmartDeviceLink/SDLNavigationServiceData.m
@@ -0,0 +1,17 @@
+//
+//  SDLNavigationServiceData.m
+//  SmartDeviceLink
+//
+//  Created by Nicole on 2/22/19.
+//  Copyright © 2019 smartdevicelink. All rights reserved.
+//
+
+#import "SDLNavigationServiceData.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation SDLNavigationServiceData
+
+@end
+
+NS_ASSUME_NONNULL_END
