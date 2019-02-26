b68b9481e1e12c69a410aba05344069a81578a1e
NicoleYarroch <nicole@livio.io>
2019-02-26 16:23:08 -0500

    Added SetCloudAppPropertiesResponse class and tests

---
+++ SmartDeviceLinkTests/RPCSpecs/ResponseSpecs/SDLSetCloudAppPropertiesResponseSpec.m
@@ -0,0 +1,35 @@
+//
+//  SDLSetCloudAppPropertiesResponseSpec.m
+//  SmartDeviceLinkTests
+//
+//  Created by Nicole on 2/26/19.
+//  Copyright © 2019 smartdevicelink. All rights reserved.
+//
+
+#import <Quick/Quick.h>
+#import <Nimble/Nimble.h>
+
+#import "SDLNames.h"
+#import "SDLSetCloudAppPropertiesResponse.h"
+
+QuickSpecBegin(SDLSetCloudAppPropertiesResponseSpec)
+
+describe(@"Getter/Setter Tests", ^ {
+    it(@"Should set and get correctly", ^{
+        SDLSetCloudAppPropertiesResponse *testResponse = [[SDLSetCloudAppPropertiesResponse alloc] init];
+        
+        expect(testResponse).toNot(beNil());
+    });
+
+    it(@"Should get correctly when initialized with a dictionary", ^{
+        NSDictionary *dict = @{SDLNameResponse:@{
+                                       SDLNameParameters:@{},
+                                       SDLNameOperationName:SDLNameSetCloudAppProperties}};
+        SDLSetCloudAppPropertiesResponse *testResponse = [[SDLSetCloudAppPropertiesResponse alloc] initWithDictionary:dict];
+
+        expect(testResponse).toNot(beNil());
+    });
+});
+
+QuickSpecEnd
+
