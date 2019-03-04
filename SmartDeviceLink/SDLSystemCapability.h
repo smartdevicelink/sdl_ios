90c91d45131362bda76d56929f4d8a6ab76bd52f
NicoleYarroch <nicole@livio.io>
2019-03-04 14:56:03 -0500

    Added doc. to `SystemCapability` conv. inits

--- SmartDeviceLink/SDLSystemCapability.h
+++ SmartDeviceLink/SDLSystemCapability.h
@@ -18,20 +18,50 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
- The systemCapabilityType indicates which type of data should be changed and identifies which data object exists in this struct. For example, if the SystemCapability Type is NAVIGATION then a "navigationCapability" should exist.
-
- First implemented in SDL Core v4.4
+ *  The systemCapabilityType indicates which type of data should be changed and identifies which data object exists in this struct. For example, if the SystemCapability Type is NAVIGATION then a "navigationCapability" should exist.
+ *
+ *  First implemented in SDL Core v4.4
  */
 @interface SDLSystemCapability : SDLRPCStruct
 
+/**
+ *  Convenience init for an App Service Capability
+ *
+ *  @param capability   Describes the capabilities of app services including what service types are supported and the current state of services
+ *  @return             A SDLSystemCapability object
+ */
 - (instancetype)initWithAppServicesCapabilities:(SDLAppServicesCapabilities *)capability;
 
+/**
+ *  Convenience init for a Navigation Capability
+ *
+ *  @param capability   Describes the extended capabilities of the onboard navigation system
+ *  @return             A SDLSystemCapability object
+ */
 - (instancetype)initWithNavigationCapability:(SDLNavigationCapability *)capability;
 
+/**
+ *  Convenience init for a Phone Capability
+ *
+ *  @param capability   Describes the extended capabilities of the module's phone feature
+ *  @return             A SDLSystemCapability object
+ */
 - (instancetype)initWithPhoneCapability:(SDLPhoneCapability *)capability;
 
+/**
+ *  Convenience init for a Video Streaming Capability
+ *
+ *  @param capability   Describes the capabilities of the module's video streaming feature
+ *  @return             A SDLSystemCapability object
+ */
 - (instancetype)initWithVideoStreamingCapability:(SDLVideoStreamingCapability *)capability;
 
+/**
+ *  Convenience init for a Remote Control Capability
+ *
+ *  @param capability   Describes the extended capabilities of the module's remote control feature
+ *  @return             A SDLSystemCapability object
+ */
 - (instancetype)initWithRemoteControlCapability:(SDLRemoteControlCapabilities *)capability;
 
 /**
