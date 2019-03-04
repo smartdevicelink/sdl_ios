f8c827ba5df5307950246723c5590d3c4a7647cd
NicoleYarroch <nicole@livio.io>
2019-03-04 08:42:21 -0500

    Removed extra semicolon

--- SmartDeviceLinkTests/DevAPISpecs/SDLLifecycleManagerSpec.m
+++ SmartDeviceLinkTests/DevAPISpecs/SDLLifecycleManagerSpec.m
@@ -78,7 +78,7 @@ describe(@"a lifecycle manager", ^{
     __block SDLConfiguration *testConfig = nil;
     
     __block id protocolMock = OCMClassMock([SDLProtocol class]);
-    __block id proxyMock = OCMClassMock([SDLProxy class]);;
+    __block id proxyMock = OCMClassMock([SDLProxy class]);
     __block id lockScreenManagerMock = OCMClassMock([SDLLockScreenManager class]);
     __block id fileManagerMock = OCMClassMock([SDLFileManager class]);
     __block id permissionManagerMock = OCMClassMock([SDLPermissionManager class]);
