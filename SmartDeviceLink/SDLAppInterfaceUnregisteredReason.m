0462ffb10db09386b542c053dca5ac9ca8a12fc7
Justin Gluck <justin.gluck@livio.io>
2019-04-04 13:01:16 -0400

    JG- adding missing SDLAppInterfaceUnregisteredReason that were in the RPC spec but not the library

--- SmartDeviceLink/SDLAppInterfaceUnregisteredReason.m
+++ SmartDeviceLink/SDLAppInterfaceUnregisteredReason.m
@@ -14,3 +14,5 @@ SDLAppInterfaceUnregisteredReason const SDLAppInterfaceUnregisteredReasonLanguag
 SDLAppInterfaceUnregisteredReason const SDLAppInterfaceUnregisteredReasonMasterReset = @"MASTER_RESET";
 SDLAppInterfaceUnregisteredReason const SDLAppInterfaceUnregisteredReasonFactoryDefaults = @"FACTORY_DEFAULTS";
 SDLAppInterfaceUnregisteredReason const SDLAppInterfaceUnregisteredReasonAppUnauthorized = @"APP_UNAUTHORIZED";
+SDLAppInterfaceUnregisteredReason const SDLAppInterfaceUnregisteredReasonProtocolViolation = @"PROTOCOL_VIOLATION";
+SDLAppInterfaceUnregisteredReason const SDLAppInterfaceUnregisteredReasonUnsupportedHMIResource = @"UNSUPPORTED_HMI_RESOURCE";
