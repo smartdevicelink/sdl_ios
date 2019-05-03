b656990f7cfad3c0747895a6159f332901f1430f
Justin Gluck <justin.gluck@livio.io>
2019-05-03 16:05:18 -0400

    Create Package file

---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "SmartDeviceLink",
+    products: [
+        .library(name: "SmartDeviceLink", targets: ["SmartDeviceLink"])
+            .library(name: "SmartDeviceLinkSwift", targets: ["SmartDeviceLinkSwift"])
+    ],
+    targets: [
+        .target(
+            name: "SmartDeviceLink",
+            path: "SmartDeviceLink"
+        ),
+        .target(
+            name: "SmartDeviceLinkSwift",
+            path: "SmartDeviceLinkSwift"
+        ),
+    ]
+)
+
