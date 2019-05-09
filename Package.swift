7c523982107255319e1823150be2855bfddbd475
Justin Gluck <justin.gluck@livio.io>
2019-05-09 10:25:26 -0400

    adding SDLswift as a Dependency

--- Package.swift
+++ Package.swift
@@ -4,19 +4,20 @@ import PackageDescription
 let package = Package(
     name: "SmartDeviceLink-iOS",
     products: [
-        .library(name: "SmartDeviceLinkSwift", targets: ["SmartDeviceLinkSwift"]),
-        .library(name: "SmartDeviceLink", targets: ["SmartDeviceLink"])
-
+        .library(name: "SmartDeviceLink", targets: ["SmartDeviceLink"]),
+        .library(name: "SmartDeviceLinkSwift", targets: ["SmartDeviceLinkSwift"])
     ],
     targets: [
         .target(
-            name: "SmartDeviceLinkSwift",
-            path: "SmartDeviceLinkSwift"
-        ),
-        .target(
             name: "SmartDeviceLink",
             path: "SmartDeviceLink"
-        )
+        ),
+        .target(
+            name: "SmartDeviceLinkSwift",
+            path: "SmartDeviceLinkSwift"
+            dependencies: [
+                "SmartDeviceLink"
+            ]
+        ),
     ]
 )
-
