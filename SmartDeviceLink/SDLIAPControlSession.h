5600b7f54dbf78587298f20a2668db538fa43fff
NicoleYarroch <nicole@livio.io>
2019-05-14 13:41:00 -0400

    Fixed documentation for control session class

--- SmartDeviceLink/SDLIAPControlSession.h
+++ SmartDeviceLink/SDLIAPControlSession.h
@@ -18,7 +18,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
- *  A control session is used to get the unique protocol string needed to create a session with Core. The control session is only used with legacy head units that do not support the multisession protocol. The multisession protocol allows multiple apps to connect over the same protocol string. A control session is needed because each app on the device needs to connect to the accessory using a unique protocol string.
+ *  The communications channel between the app and the SDL enabled accessory. The control session is used to get the unique protocol string needed to create a session with Core. The control session is only used with legacy head units that do not support the multisession protocol (the multisession protocol allows multiple apps to connect over the same protocol string). A control session is needed because each SDL enabled app needs to connect to the accessory using a unique protocol string.
  *
  *  When the protocol string is received from Core, the control session is closed as a new session with Core must be established with the received protocol string. Core has ~10 seconds to send the protocol string, otherwise the control session is closed and new attempt is made to establish a control session with Core.
  */
