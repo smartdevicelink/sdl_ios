eb51c542aefdf43512cf3cecd5138e5dc20b8988
Justin Gluck <justin.gluck@livio.io>
2019-05-28 10:24:37 -0400

    fixed pr issue

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SmartDeviceLink-iOS",
+    name: "SmartDeviceLink",
     products: [
         .library(name: "SmartDeviceLink", targets: ["SmartDeviceLink"]),
         .library(name: "SmartDeviceLinkSwift", targets: ["SmartDeviceLinkSwift"])
