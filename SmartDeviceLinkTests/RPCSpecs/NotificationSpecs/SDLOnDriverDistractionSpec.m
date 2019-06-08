fe9fa9801a2d218f88f47bc5fd50ec893065f132
Satbir Tanda <satbirtanda@gmail.com>
2019-06-07 18:35:14 -0700

    Update SDLOnDriverDistractionSpec.m
    Test getter and setter of lockScreenDismissalEnabled
    Update pointer syntax

--- SmartDeviceLinkTests/RPCSpecs/NotificationSpecs/SDLOnDriverDistractionSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/NotificationSpecs/SDLOnDriverDistractionSpec.m
@@ -17,27 +17,46 @@ QuickSpecBegin(SDLOnDriverDistractionSpec)
 
 describe(@"Getter/Setter Tests", ^ {
     it(@"Should set and get correctly", ^ {
-        SDLOnDriverDistraction* testNotification = [[SDLOnDriverDistraction alloc] init];
+        SDLOnDriverDistraction *testNotification = [[SDLOnDriverDistraction alloc] init];
         
         testNotification.state = SDLDriverDistractionStateOn;
+        testNotification.lockScreenDismissalEnabled = @1;
         
         expect(testNotification.state).to(equal(SDLDriverDistractionStateOn));
+        expect(testNotification.lockScreenDismissalEnabled).to(beTrue());
+        
+        testNotification.lockScreenDismissalEnabled = @0;
+        expect(testNotification.lockScreenDismissalEnabled).to(beFalse());
     });
     
     it(@"Should get correctly when initialized", ^ {
-        NSMutableDictionary* dict = [@{SDLRPCParameterNameNotification:
+        NSMutableDictionary *dictOn = [@{SDLRPCParameterNameNotification:
                                            @{SDLRPCParameterNameParameters:
-                                                 @{SDLRPCParameterNameState:SDLDriverDistractionStateOn},
+                                                 @{SDLRPCParameterNameState:SDLDriverDistractionStateOn,
+                                                   SDLRPCParameterNameLockScreenDismissalEnabled: @1},
                                              SDLRPCParameterNameOperationName:SDLRPCFunctionNameOnDriverDistraction}} mutableCopy];
-        SDLOnDriverDistraction* testNotification = [[SDLOnDriverDistraction alloc] initWithDictionary:dict];
+        SDLOnDriverDistraction* testNotificationOn = [[SDLOnDriverDistraction alloc] initWithDictionary:dictOn];
         
-        expect(testNotification.state).to(equal(SDLDriverDistractionStateOn));
+        expect(testNotificationOn.state).to(equal(SDLDriverDistractionStateOn));
+        expect(testNotificationOn.lockScreenDismissalEnabled).to(beTrue());
+        
+        NSMutableDictionary *dictOff = [@{SDLRPCParameterNameNotification:
+                                           @{SDLRPCParameterNameParameters:
+                                                 @{SDLRPCParameterNameState:SDLDriverDistractionStateOff,
+                                                   SDLRPCParameterNameLockScreenDismissalEnabled: @0},
+                                             SDLRPCParameterNameOperationName:SDLRPCFunctionNameOnDriverDistraction}} mutableCopy];
+        SDLOnDriverDistraction *testNotificationOff = [[SDLOnDriverDistraction alloc] initWithDictionary:dictOff];
+
+        expect(testNotificationOff.state).to(equal(SDLDriverDistractionStateOff));
+        expect(testNotificationOff.lockScreenDismissalEnabled).to(beFalse());
     });
     
     it(@"Should return nil if not set", ^ {
-        SDLOnDriverDistraction* testNotification = [[SDLOnDriverDistraction alloc] init];
+        SDLOnDriverDistraction *testNotification = [[SDLOnDriverDistraction alloc] init];
         
         expect(testNotification.state).to(beNil());
+        expect(testNotification.lockScreenDismissalEnabled).to(beNil());
+        expect(testNotification.lockScreenDismissalEnabled).to(beFalsy());
     });
 });
 
