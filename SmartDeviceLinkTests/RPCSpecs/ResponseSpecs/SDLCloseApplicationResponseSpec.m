d6db8f29262d719e525e8cc9ab06745c4b7f7b9b
NicoleYarroch <nicole@livio.io>
2019-07-10 13:48:01 -0400

    Added CloseApplication test cases

--- SmartDeviceLinkTests/RPCSpecs/ResponseSpecs/SDLCloseApplicationResponseSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/ResponseSpecs/SDLCloseApplicationResponseSpec.m
@@ -1,24 +1,24 @@
 //
-//  SDLCloseApplicationSpec.m
+//  SDLCloseApplicationResponseSpec.m
 //  SmartDeviceLinkTests
 //
 //  Created by Nicole on 7/10/19.
 //  Copyright © 2019 smartdevicelink. All rights reserved.
 //
 
-#import "SDLCloseApplication.h"
-#import "SDLRPCFunctionNames.h"
-#import "SDLRPCParameterNames.h"
-
 #import <Quick/Quick.h>
 #import <Nimble/Nimble.h>
 
-QuickSpecBegin(SDLCloseApplicationSpec)
+#import "SDLCloseApplicationResponse.h"
+#import "SDLRPCParameterNames.h"
+#import "SDLRPCFunctionNames.h"
+
+QuickSpecBegin(SDLCloseApplicationResponseSpec)
 
 describe(@"Getter/Setter Tests", ^{
     it(@"Should initialize correctly", ^{
-        SDLCloseApplication *testRequest = [[SDLCloseApplication alloc] init];
-        expect(testRequest.name).to(equal(SDLRPCFunctionNameCloseApplication));
+        SDLCloseApplicationResponse *testResponse = [[SDLCloseApplicationResponse alloc] init];
+        expect(testResponse.name).to(equal(SDLRPCFunctionNameCloseApplication));
     });
 
     it(@"Should initialize correctly with a dictionary", ^{
@@ -27,13 +27,12 @@ describe(@"Getter/Setter Tests", ^{
                                        SDLRPCParameterNameOperationName:SDLRPCFunctionNameCloseApplication}};
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        SDLCloseApplication *testRequest = [[SDLCloseApplication alloc] initWithDictionary:dict];
+        SDLCloseApplicationResponse *testResponse = [[SDLCloseApplicationResponse alloc] initWithDictionary:dict];
 #pragma clang diagnostic pop
 
-        expect(testRequest.name).to(equal(SDLRPCFunctionNameCloseApplication));
-        expect(testRequest.parameters).to(beEmpty());
+        expect(testResponse.name).to(equal(SDLRPCFunctionNameCloseApplication));
+        expect(testResponse.parameters).to(beEmpty());
     });
 });
 
 QuickSpecEnd
-
