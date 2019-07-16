e09e81120f330986e3d05cac12a389807b6efdaa
Mauricio Juarez <mjuarez44@ford.com>
2019-07-16 08:04:02 +0200

    Delete window response

--- SmartDeviceLink/SDLDeleteWindowResponse.m
+++ SmartDeviceLink/SDLDeleteWindowResponse.m
@@ -8,6 +8,19 @@
 
 #import "SDLDeleteWindowResponse.h"
 
+#import "NSMutableDictionary+Store.h"
+#import "SDLRPCParameterNames.h"
+#import "SDLRPCFunctionNames.h"
+
 @implementation SDLDeleteWindowResponse
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+- (instancetype)init {
+    if (self = [super initWithName:SDLRPCFunctionNameDeleteWindow]) {
+    }
+    return self;
+}
+#pragma clang diagnostic pop
 
 @end
