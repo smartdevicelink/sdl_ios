178470e1c962ba711e932c54b4d3f247bd767d25
Mauricio Juarez <mjuarez44@ford.com>
2019-07-16 16:03:08 +0200

    add SDL window capability documentation

--- SmartDeviceLink/SDLWindowCapability.m
+++ SmartDeviceLink/SDLWindowCapability.m
@@ -8,6 +8,71 @@
 
 #import "SDLWindowCapability.h"
 
+#import "NSMutableDictionary+Store.h"
+#import "SDLRPCParameterNames.h"
+#import "SDLTextField.h"
+#import "SDLImageField.h"
+#import "SDLButtonCapabilities.h"
+#import "SDLSoftButtonCapabilities.h"
+
 @implementation SDLWindowCapability
+
+
+- (void)setWindowID:(nullable NSNumber<SDLInt> *)windowID {
+    [self.store sdl_setObject:windowID forName:SDLRPCParameterNameWindowId];
+}
+
+- (nullable NSNumber<SDLInt> *)windowID {
+    return [self.store sdl_objectForName:SDLRPCParameterNameWindowId ofClass:NSNumber.class error:nil];
+}
+
+- (void)setTextFields:(nullable NSArray<SDLTextField *> *)textFields {
+    [self.store sdl_setObject:textFields forName:SDLRPCParameterNameTextFields];
+}
+
+- (nullable NSArray<SDLTextField *> *)textFields {
+    return [self.store sdl_objectsForName:SDLRPCParameterNameTextFields ofClass:SDLTextField.class error:nil];
+}
+
+- (void)setImageFields:(nullable NSArray<SDLImageField *> *)imageFields {
+    [self.store sdl_setObject:imageFields forName:SDLRPCParameterNameImageFields];
+}
+
+- (nullable NSArray<SDLImageField *> *)imageFields {
+    return [self.store sdl_objectsForName:SDLRPCParameterNameImageFields ofClass:SDLImageField.class error:nil];
+}
+
+- (void)setImageTypeSupported:(nullable NSArray<SDLImageType> *)imageTypeSupported {
+    [self.store sdl_setObject:imageTypeSupported forName:SDLRPCParameterNameImageTypeSupported];
+}
+
+- (nullable NSArray<SDLImageType> *)imageTypeSupported {
+    return [self.store sdl_enumsForName:SDLRPCParameterNameImageTypeSupported error:nil];
+}
+
+- (void)setNumCustomPresetsAvailable:(nullable NSNumber<SDLInt> *)numCustomPresetsAvailable {
+    [self.store sdl_setObject:numCustomPresetsAvailable forName:SDLRPCParameterNameNumberCustomPresetsAvailable];
+}
+
+- (nullable NSNumber<SDLInt> *)numCustomPresetsAvailable {
+    return [self.store sdl_objectForName:SDLRPCParameterNameNumberCustomPresetsAvailable ofClass:NSNumber.class error:nil];
+}
+
+- (void)setButtonCapabilities:(nullable NSArray<SDLButtonCapabilities *> *)buttonCapabilities {
+    [self.store sdl_setObject:buttonCapabilities forName:SDLRPCParameterNameButtonCapabilities];
+}
+
+- (nullable NSArray<SDLButtonCapabilities *> *)buttonCapabilities {
+    return [self.store sdl_objectsForName:SDLRPCParameterNameButtonCapabilities ofClass:SDLButtonCapabilities.class error:nil];
+}
+
+
+- (void)setSoftButtonCapabilities:(nullable NSArray<SDLSoftButtonCapabilities *> *)softButtonCapabilities {
+    [self.store sdl_setObject:softButtonCapabilities forName:SDLRPCParameterNameSoftButtonCapabilities];
+}
+
+- (nullable NSArray<SDLSoftButtonCapabilities *> *)softButtonCapabilities {
+    return [self.store sdl_objectsForName:SDLRPCParameterNameSoftButtonCapabilities ofClass:SDLSoftButtonCapabilities.class error:nil];
+}
 
 @end
