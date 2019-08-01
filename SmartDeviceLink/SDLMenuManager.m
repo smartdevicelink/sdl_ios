12d5bd61d7bd219a1d352f2add8a785671dcdecf
justingluck93 <47197545+justingluck93@users.noreply.github.com>
2019-08-01 15:26:15 -0400

    Apply suggestions from code review
    Co-Authored-By: Joel Fischer <joeljfischer@gmail.com>

--- SmartDeviceLink/SDLMenuManager.m
+++ SmartDeviceLink/SDLMenuManager.m
@@ -642,7 +642,7 @@ UInt32 const MenuCellIdMin = 1;
 }
 
 - (BOOL)openMenu {
-    if([SDLGlobals.sharedGlobals.rpcVersion isLessThanVersion:[[SDLVersion alloc] initWithMajor:6 minor:0 patch:0]]) {
+    if ([SDLGlobals.sharedGlobals.rpcVersion isLessThanVersion:[[SDLVersion alloc] initWithMajor:6 minor:0 patch:0]]) {
         SDLLogE(@"The openMenu method is not supported on this head unit.");
         return NO;
     }
@@ -662,10 +662,10 @@ UInt32 const MenuCellIdMin = 1;
     if (cell.subCells.count == 0) {
         SDLLogE(@"The cell %@ does not contain any sub cells, so no submenu can be opened", cell);
         return NO;
-    } else if([SDLGlobals.sharedGlobals.rpcVersion isLessThanVersion:[[SDLVersion alloc] initWithMajor:6 minor:0 patch:0]]) {
+    } else if ([SDLGlobals.sharedGlobals.rpcVersion isLessThanVersion:[[SDLVersion alloc] initWithMajor:6 minor:0 patch:0]]) {
         SDLLogE(@"The openSubmenu method is not supported on this head unit.");
         return NO;
-    } else if(![self.menuCells containsObject:cell]) {
+    } else if (![self.menuCells containsObject:cell]) {
         SDLLogE(@"This cell has not been sent to the head unit, so no submenu can be opened. Make sure that the cell exists in the SDLManager.menu array");
         return NO;
     }
