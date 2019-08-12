649792a6849f39686eb3398630685bb6939c04ec
mjuarez-ford <47789765+mjuarez-ford@users.noreply.github.com>
2019-08-12 13:39:18 +0200

    Update SmartDeviceLink/SDLRegisterAppInterfaceResponse.h
    Co-Authored-By: Joel Fischer <joeljfischer@gmail.com>

--- SmartDeviceLink/SDLRegisterAppInterfaceResponse.h
+++ SmartDeviceLink/SDLRegisterAppInterfaceResponse.h
@@ -100,7 +100,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  * @since SDL 2.0
  */
-@property (nullable, strong, nonatomic) SDLPresetBankCapabilities *presetBankCapabilities __attribute__((deprecated("If returned, the platform supports on-screen SoftButtons; see SoftButtonCapabilities.  This parameter is deprecated and replaced by  SystemCapability using DISPLAY. @since 6.0")));
+@property (nullable, strong, nonatomic) SDLPresetBankCapabilities *presetBankCapabilities __deprecated_msg("This parameter is deprecated and replaced by GetSystemCapability using DISPLAY. Deprecated in sdl_ios v6.4 / RPC spec 6.0. You can use the SystemCapabilityManager to have automatic full compatibility support.");
 
 /**
  * Contains information about the HMI zone capabilities.
