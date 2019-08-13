12ea34a2713a0be518b746dce39453a04b9a3c19
NicoleYarroch <nicole@livio.io>
2019-08-13 14:30:23 -0400

    Added documentation fo deprecated keyboard method

--- SmartDeviceLink/SDLChoiceSetManager.h
+++ SmartDeviceLink/SDLChoiceSetManager.h
@@ -98,6 +98,8 @@ extern SDLChoiceManagerState *const SDLChoiceManagerStateStartupError;
 /**
  Present a keyboard-only interface to the user and receive input. The user will be able to input text in the keyboard when in a non-driver distraction situation.
 
+ A keyboard created with this method can not be canceled.
+
  @param initialText The initial text within the keyboard input field. It will disappear once the user selects the field in order to enter text
  @param delegate The keyboard delegate called when the user interacts with the keyboard
  */
