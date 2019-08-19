c32c4140da39458cbdadc064cdbb822c82ecbe8a
Mauricio <mjuare44@ford.com>
2019-08-19 17:51:26 +0200

    Tests

--- SmartDeviceLinkTests/RPCSpecs/RequestSpecs/SDLCreateWindowSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/RequestSpecs/SDLCreateWindowSpec.m
@@ -6,7 +6,58 @@
 #import <Nimble/Nimble.h>
 
 #import "SDLCreateWindow.h"
+#import "SDLWindowType.h"
 
 QuickSpecBegin(SDLCreateWindowSpec)
 
+describe(@"Getter/Setter Tests", ^ {
+    __block SDLWindowType testWindowType = nil;
+    __block NSString *testAasociatedServiceType = nil;
+    __block NSString *testWindowName = nil;
+    __block int testWindowID = 4;
+    __block int testDuplicateUpdatesFromWindowID = 8;
+    
+    
+    beforeEach(^{
+        testWindowType = SDLWindowTypeMain;
+        testAasociatedServiceType = @"SDLWINDOW";
+        testWindowName = @"MAINWINDOW";
+    });
+    
+    it(@"Should set and get correctly", ^ {
+        SDLCreateWindow *testRPC = [[SDLCreateWindow alloc] init];
+        testRPC.windowID = @(testWindowID);
+        testRPC.windowName = testWindowName;
+        testRPC.type = testWindowType;
+        testRPC.associatedServiceType = testAasociatedServiceType;
+        testRPC.duplicateUpdatesFromWindowID = @(testDuplicateUpdatesFromWindowID);
+        
+        expect(testRPC.windowID).to(equal(testWindowID));
+        expect(testRPC.windowName).to(equal(testWindowName));
+        expect(testRPC.type).to(equal(testWindowType));
+        expect(testRPC.associatedServiceType).to(equal(testAasociatedServiceType));
+        expect(testRPC.duplicateUpdatesFromWindowID).to(equal(testDuplicateUpdatesFromWindowID));
+    });
+    
+    it(@"Should create correctrly", ^ {
+        SDLCreateWindow *testRPC = [[SDLCreateWindow alloc] initWithId:testWindowID windowName:testWindowName windowType:testWindowType];
+        
+        expect(testRPC.windowID).to(equal(testWindowID));
+        expect(testRPC.windowName).to(equal(testWindowName));
+        expect(testRPC.type).to(equal(testWindowType));
+        expect(testRPC.associatedServiceType).to(beNil());
+        expect(testRPC.duplicateUpdatesFromWindowID).to(beNil());
+    });
+    
+    it(@"Should create correctrly", ^ {
+        SDLCreateWindow *testRPC = [[SDLCreateWindow alloc] initWithId:testWindowID windowName:testWindowName windowType:testWindowType associatedServiceType:testAasociatedServiceType duplicateUpdatesFromWindowID:testDuplicateUpdatesFromWindowID];
+        
+        expect(testRPC.windowID).to(equal(testWindowID));
+        expect(testRPC.windowName).to(equal(testWindowName));
+        expect(testRPC.type).to(equal(testWindowType));
+        expect(testRPC.associatedServiceType).to(equal(testAasociatedServiceType));
+        expect(testRPC.duplicateUpdatesFromWindowID).to(equal(testDuplicateUpdatesFromWindowID));
+    });
+    
+});
 QuickSpecEnd
--- SmartDeviceLinkTests/RPCSpecs/RequestSpecs/SDLDeleteWindowSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/RequestSpecs/SDLDeleteWindowSpec.m
@@ -9,4 +9,24 @@
 
 QuickSpecBegin(SDLDeleteWindowSpec)
 
+
+describe(@"Getter/Setter Tests", ^ {
+    __block int testWindowID = 4;
+    
+    it(@"Should set and get correctly", ^ {
+        SDLDeleteWindow *testRPC = [[SDLDeleteWindow alloc] init];
+        testRPC.windowID = @(testWindowID);
+        
+        expect(testRPC.windowID).to(equal(testWindowID));
+    });
+    
+    it(@"Should create correctrly", ^ {
+        SDLDeleteWindow *testRPC = [[SDLDeleteWindow alloc] initWithId:testWindowID];
+        
+        expect(testRPC.windowID).to(equal(testWindowID));
+    });
+    
+    
+});
+
 QuickSpecEnd
