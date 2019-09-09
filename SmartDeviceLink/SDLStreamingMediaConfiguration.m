1e5d20a9bc998a0d0031269bf278e70abfcd8a92
Takamitsu Yoshii <tyoshii@xevo.com>
2019-09-09 15:03:17 +0900

    add test

--- SmartDeviceLink/SDLStreamingMediaConfiguration.m
+++ SmartDeviceLink/SDLStreamingMediaConfiguration.m
@@ -69,6 +69,7 @@ NS_ASSUME_NONNULL_BEGIN
     newConfig.carWindowRenderingType = self.carWindowRenderingType;
     newConfig.enableForcedFramerateSync = self.enableForcedFramerateSync;
     newConfig.allowMultipleViewControllerOrientations = self.allowMultipleViewControllerOrientations;
+    newConfig.allowOverrideEncoderSettings = self.allowOverrideEncoderSettings;
     
     return newConfig;
 }
