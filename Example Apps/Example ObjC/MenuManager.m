285dc95d09ed364b3f1837d87ffff355ed7188fa
Justin Gluck <justin.gluck@livio.io>
2019-09-20 10:19:15 -0400

    updating to show alert if slider or scrollable message fails

--- Example Apps/Example ObjC/MenuManager.m
+++ Example Apps/Example ObjC/MenuManager.m
@@ -135,14 +135,22 @@ NS_ASSUME_NONNULL_BEGIN
 + (SDLMenuCell *)sdlex_sliderMenuCellWithManager:(SDLManager *)manager {
     return [[SDLMenuCell alloc] initWithTitle:ACSliderMenuName icon:nil voiceCommands:@[ACSliderMenuName] handler:^(SDLTriggerSource  _Nonnull triggerSource) {
         SDLSlider *sliderRPC = [[SDLSlider alloc] initWithNumTicks:3 position:1 sliderHeader:@"Select a letter" sliderFooters:@[@"A", @"B", @"C"] timeout:10000];
-        [manager sendRequest:sliderRPC];
+        [manager sendRequest:sliderRPC withResponseHandler:^(__kindof SDLRPCRequest * _Nullable request, __kindof SDLRPCResponse * _Nullable response, NSError * _Nullable error) {
+            if(![response.resultCode isEqualToEnum:SDLResultSuccess]) {
+                [manager sendRequest:[AlertManager alertWithMessageAndCloseButton:@"Slider could not be displayed" textField2:nil iconName:nil]];
+            }
+        }];
     }];
 }
 
 + (SDLMenuCell *)sdlex_scrollableMessageMenuCellWithManager:(SDLManager *)manager {
     return [[SDLMenuCell alloc] initWithTitle:ACScrollableMessageMenuName icon:nil voiceCommands:@[ACScrollableMessageMenuName] handler:^(SDLTriggerSource  _Nonnull triggerSource) {
-        SDLScrollableMessage *messageRPC = [[SDLScrollableMessage alloc] initWithMessage:@"This is a scrollable message\nIt can contain many lines" timeout:10000 softButtons:nil];
-        [manager sendRequest:messageRPC];
+        SDLScrollableMessage *messageRPC = [[SDLScrollableMessage alloc] initWithMessage:@"This is a scrollable message\nIt can contain many lines" timeout:10000 softButtons:nil cancelID:5];
+        [manager sendRequest:messageRPC withResponseHandler:^(__kindof SDLRPCRequest * _Nullable request, __kindof SDLRPCResponse * _Nullable response, NSError * _Nullable error) {
+           if(![response.resultCode isEqualToEnum:SDLResultSuccess]) {
+                [manager sendRequest: [AlertManager alertWithMessageAndCloseButton:@"Scrollable Message could not be displayed" textField2:nil iconName:nil]];
+            }
+        }];
     }];
 }
 
--- Example Apps/Example Swift/MenuManager.swift
+++ Example Apps/Example Swift/MenuManager.swift
@@ -179,15 +179,25 @@ private extension MenuManager {
 
     private class func sliderMenuCell(with manager: SDLManager) -> SDLMenuCell {
         return SDLMenuCell(title: ACSliderMenuName, icon: nil, voiceCommands: [ACSliderMenuName], handler: { _ in
-            let slider = SDLSlider(numTicks: 3, position: 1, sliderHeader: "Select a letter", sliderFooters: ["A", "B", "C"], timeout: 10000)
-            manager.send(slider)
+            let slider = SDLSlider(numTicks: 3, position: 1, sliderHeader: "Select a letter", sliderFooters: ["A", "B", "C"], timeout: 3000)
+            manager.send(request: slider, responseHandler: { (request, response, error) in
+                guard response?.resultCode == .success else {
+                    manager.send(AlertManager.alertWithMessageAndCloseButton("Slider could not be displayed"))
+                    return
+                }
+            })
         })
     }
 
     private class func scrollableMessageMenuCell(with manager: SDLManager) -> SDLMenuCell {
         return SDLMenuCell(title: ACScrollableMessageMenuName, icon: nil, voiceCommands: [ACScrollableMessageMenuName], handler: { _ in
             let scrollableMessage = SDLScrollableMessage(message: "This is a scrollable message\nIt can contain many lines", timeout: 10000, softButtons: nil)
-            manager.send(scrollableMessage)
+            manager.send(request: scrollableMessage, responseHandler: { (request, response, error) in
+                guard response?.resultCode == .success else {
+                    manager.send(AlertManager.alertWithMessageAndCloseButton("Scrollable could not be displayed"))
+                    return
+                }
+            })
         })
     }
 }
