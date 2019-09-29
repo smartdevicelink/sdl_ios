7ae5a2eb3babff05fd75fc3bda6792731d4e2c79
yLeonid <YLokhmatov@luxoft.com>
2019-09-29 19:51:10 +0300

    0179_pixel_density_and_scale : fix UI tests for HapticManager aka SDLFocusableItemLocator

--- SmartDeviceLinkTests/ProxySpecs/SDLHapticManagerSpec.m
+++ SmartDeviceLinkTests/ProxySpecs/SDLHapticManagerSpec.m
@@ -18,6 +18,7 @@
 #import "SDLTouchCoord.h"
 #import "SDLTouchEvent.h"
 #import "SDLTouch.h"
+#import "CGGeometry+SDL.h"
 
 BOOL compareRectangle(SDLRectangle *sdlRectangle, CGRect cgRect) {
     expect(sdlRectangle.x).to(equal(cgRect.origin.x));
@@ -29,7 +30,7 @@ BOOL compareRectangle(SDLRectangle *sdlRectangle, CGRect cgRect) {
 
 QuickSpecBegin(SDLHapticManagerSpec)
 
-describe(@"the haptic manager", ^{
+describe(@"", ^{
     __block UIWindow *uiWindow;
     __block UIViewController *uiViewController;
 
@@ -39,15 +40,18 @@ describe(@"the haptic manager", ^{
     __block id sdlLifecycleManager = OCMClassMock([SDLLifecycleManager class]);
     __block CGRect viewRect1;
     __block CGRect viewRect2;
+    const float scale = 3.f;
 
     beforeEach(^{
         hapticManager = nil;
         sentHapticRequest = nil;
 
-        uiWindow = [[UIWindow alloc] init];
+        const CGRect mainFrame = CGRectMake(0, 0, 320, 480);
+        uiWindow = [[UIWindow alloc] initWithFrame:mainFrame];
         uiViewController = [[UIViewController alloc] init];
 
         uiWindow.rootViewController = uiViewController;
+        uiViewController.view.frame = uiWindow.bounds;
 
         OCMExpect([[sdlLifecycleManager stub] sendConnectionManagerRequest:[OCMArg checkWithBlock:^BOOL(id value){
             BOOL isFirstArg = [value isKindOfClass:[SDLSendHapticData class]];
@@ -159,7 +163,7 @@ describe(@"the haptic manager", ^{
             [hapticManager updateInterfaceLayout];
         });
 
-        it(@"should have two views", ^{
+        it(@"should have two views with predefined origin and size", ^{
             OCMVerify(sdlLifecycleManager);
 
             int expectedCount = 2;
@@ -175,6 +179,19 @@ describe(@"the haptic manager", ^{
 
                 compareRectangle(sdlRect1, viewRect2);
                 compareRectangle(sdlRect2, viewRect1);
+
+                // step 2: test with another scale factor
+                hapticManager.scale = scale;
+                hapticRectData = sentHapticRequest.hapticRectData;
+
+                sdlhapticRect1 = hapticRectData[0];
+                sdlRect1 = sdlhapticRect1.rect;
+
+                sdlhapticRect2 = hapticRectData[1];
+                sdlRect2 = sdlhapticRect2.rect;
+
+                compareRectangle(sdlRect1, CGRectScale(viewRect2, scale));
+                compareRectangle(sdlRect2, CGRectScale(viewRect1, scale));
             }
         });
     });
@@ -197,14 +214,15 @@ describe(@"the haptic manager", ^{
             [hapticManager updateInterfaceLayout];
         });
 
-        it(@"should have only leaf views added", ^{
+        it(@"should have only leaf views added with the coordinates predefined", ^{
             OCMVerify(sdlLifecycleManager);
 
-            int expectedCount = 2;
+            const int expectedCount = 2;
             expect(sentHapticRequest.hapticRectData.count).to(equal(expectedCount));
 
             if(sentHapticRequest.hapticRectData.count == expectedCount) {
                 NSArray<SDLHapticRect *> *hapticRectData = sentHapticRequest.hapticRectData;
+
                 SDLHapticRect *sdlhapticRect1 = hapticRectData[0];
                 SDLRectangle *sdlRect1 = sdlhapticRect1.rect;
 
@@ -213,6 +231,19 @@ describe(@"the haptic manager", ^{
 
                 compareRectangle(sdlRect1, viewRect1);
                 compareRectangle(sdlRect2, viewRect2);
+
+                // step 2: test with another scale factor
+                hapticManager.scale = scale;
+                hapticRectData = sentHapticRequest.hapticRectData;
+
+                sdlhapticRect1 = hapticRectData[1];
+                sdlRect1 = sdlhapticRect1.rect;
+
+                sdlhapticRect2 = hapticRectData[0];
+                sdlRect2 = sdlhapticRect2.rect;
+
+                compareRectangle(sdlRect1, CGRectScale(viewRect2, scale));
+                compareRectangle(sdlRect2, CGRectScale(viewRect1, scale));
             }
         });
     });
@@ -235,7 +266,7 @@ describe(@"the haptic manager", ^{
             [hapticManager updateInterfaceLayout];
         });
 
-        it(@"should have only leaf views added", ^{
+        it(@"should have only leaf views added with the predefined coordinates", ^{
             OCMVerify(sdlLifecycleManager);
 
             int expectedCount = 2;
@@ -251,6 +282,19 @@ describe(@"the haptic manager", ^{
 
                 compareRectangle(sdlRect1, viewRect1);
                 compareRectangle(sdlRect2, viewRect2);
+
+                // step 2: test with another scale factor
+                hapticManager.scale = scale;
+                hapticRectData = sentHapticRequest.hapticRectData;
+
+                sdlhapticRect1 = hapticRectData[1];
+                sdlRect1 = sdlhapticRect1.rect;
+
+                sdlhapticRect2 = hapticRectData[0];
+                sdlRect2 = sdlhapticRect2.rect;
+
+                compareRectangle(sdlRect1, CGRectScale(viewRect2, scale));
+                compareRectangle(sdlRect2, CGRectScale(viewRect1, scale));
             }
         });
     });
@@ -274,10 +318,10 @@ describe(@"the haptic manager", ^{
             [hapticManager updateInterfaceLayout];
         });
 
-        it(@"should have one view", ^{
+        it(@"should have one view with the predefined coordinates", ^{
             OCMVerify(sdlLifecycleManager);
 
-            int expectedCount = 1;
+            const int expectedCount = 1;
             expect(sentHapticRequest.hapticRectData.count).to(equal(expectedCount));
 
             if(sentHapticRequest.hapticRectData.count == expectedCount) {
@@ -286,6 +330,15 @@ describe(@"the haptic manager", ^{
                 SDLRectangle *sdlRect = sdlhapticRect.rect;
 
                 compareRectangle(sdlRect, viewRect1);
+
+                // step 2: test with another scale factor
+                hapticManager.scale = scale;
+                hapticRectData = sentHapticRequest.hapticRectData;
+
+                sdlhapticRect = hapticRectData[0];
+                sdlRect = sdlhapticRect.rect;
+
+                compareRectangle(sdlRect, CGRectScale(viewRect1, scale));
             }
         });
     });
@@ -307,7 +360,7 @@ describe(@"the haptic manager", ^{
             [[NSNotificationCenter defaultCenter] postNotificationName:SDLDidUpdateProjectionView object:nil];
         });
 
-        it(@"should have two views", ^{
+        it(@"should have two views with the predefined coordinates", ^{
             OCMVerify(sdlLifecycleManager);
 
             int expectedCount = 2;
@@ -323,16 +376,32 @@ describe(@"the haptic manager", ^{
 
                 compareRectangle(sdlRect1, viewRect2);
                 compareRectangle(sdlRect2, viewRect1);
+
+                // step 2: test with another scale factor
+                hapticManager.scale = scale;
+                hapticRectData = sentHapticRequest.hapticRectData;
+
+                sdlhapticRect1 = hapticRectData[0];
+                sdlRect1 = sdlhapticRect1.rect;
+
+                sdlhapticRect2 = hapticRectData[1];
+                sdlRect2 = sdlhapticRect2.rect;
+
+                compareRectangle(sdlRect1, CGRectScale(viewRect2, scale));
+                compareRectangle(sdlRect2, CGRectScale(viewRect1, scale));
             }
         });
     });
 
     context(@"when touched inside a view", ^{
         beforeEach(^{
-            UITextField *textField1 = [[UITextField alloc]  initWithFrame:CGRectMake(101, 101, 50, 50)];
+            viewRect1 = CGRectMake(101, 101, 50, 50);
+            viewRect2 = CGRectMake(201, 201, 50, 50);
+
+            UITextField *textField1 = [[UITextField alloc]  initWithFrame:viewRect1];
             [uiViewController.view addSubview:textField1];
 
-            UITextField *textField2 = [[UITextField alloc]  initWithFrame:CGRectMake(201, 201, 50, 50)];
+            UITextField *textField2 = [[UITextField alloc]  initWithFrame:viewRect2];
             [uiViewController.view addSubview:textField2];
 
             hapticManager = [[SDLFocusableItemLocator alloc] initWithViewController:uiViewController  connectionManager:sdlLifecycleManager scale:1.f];
@@ -341,20 +410,27 @@ describe(@"the haptic manager", ^{
         });
 
         it(@"should return a view object", ^{
-            UIView *view1 = [hapticManager viewForPoint:CGPointMake(125, 120)];
+            const CGPoint point1 = CGRectGetCenterPoint(viewRect1);
+            const CGPoint point2 = CGRectGetCenterPoint(viewRect2);
+
+            UIView *view1 = [hapticManager viewForPoint:point1];
             expect(view1).toNot(beNil());
 
-            UIView* view2 = [hapticManager viewForPoint:CGPointMake(202, 249)];
+            UIView* view2 = [hapticManager viewForPoint:point2];
             expect(view2).toNot(beNil());
         });
     });
 
     context(@"when touched in overlapping views' area", ^{
         beforeEach(^{
-            UITextField *textField1 = [[UITextField alloc]  initWithFrame:CGRectMake(101, 101, 50, 50)];
+            viewRect1 = CGRectMake(101, 201, 50, 100);
+            const CGPoint center1 = CGRectGetCenterPoint(viewRect1);
+            viewRect2 = (CGRect){center1, CGSizeMake(50, 100)};
+
+            UITextField *textField1 = [[UITextField alloc]  initWithFrame:viewRect1];
             [uiViewController.view addSubview:textField1];
 
-            UITextField *textField2 = [[UITextField alloc]  initWithFrame:CGRectMake(126, 126, 50, 50)];
+            UITextField *textField2 = [[UITextField alloc]  initWithFrame:viewRect2];
             [uiViewController.view addSubview:textField2];
 
             hapticManager = [[SDLFocusableItemLocator alloc] initWithViewController:uiViewController  connectionManager:sdlLifecycleManager scale:1.f];
@@ -370,18 +446,20 @@ describe(@"the haptic manager", ^{
 
     context(@"when touched outside view boundary", ^{
         beforeEach(^{
-            UITextField *textField1 = [[UITextField alloc]  initWithFrame:CGRectMake(101, 101, 50, 50)];
+            viewRect1 = CGRectMake(101, 101, 50, 50);
+            UITextField *textField1 = [[UITextField alloc] initWithFrame:viewRect1];
             [uiWindow insertSubview:textField1 aboveSubview:uiWindow];
 
             hapticManager = [[SDLFocusableItemLocator alloc] initWithViewController:uiViewController  connectionManager:sdlLifecycleManager scale:1.f];
             hapticManager.enableHapticDataRequests = YES;
             [hapticManager updateInterfaceLayout];
         });
+
         it(@"should return nil", ^{
-            UIView* view = [hapticManager viewForPoint:CGPointMake(0, 228)];
+            const CGPoint point1 = CGPointMake(CGRectGetMaxX(viewRect1) + 1, CGRectGetMaxY(viewRect1) + 1);
+            UIView* view = [hapticManager viewForPoint:point1];
             expect(view).to(beNil());
         });
-
     });
 });
 
