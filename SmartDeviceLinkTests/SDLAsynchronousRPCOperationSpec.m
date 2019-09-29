bf0df21f5a970fb20ad42cea4ae42f608ea50007
yLeonid <YLokhmatov@luxoft.com>
2019-09-30 00:33:35 +0300

    fix occasionally fallen test: sometimes it fells on Trevis though on a local machine it always works

--- SmartDeviceLinkTests/SDLAsynchronousRPCOperationSpec.m
+++ SmartDeviceLinkTests/SDLAsynchronousRPCOperationSpec.m
@@ -83,7 +83,7 @@ describe(@"sending responses and notifications", ^{
             [testOperationQueue addOperation:testOperation];
             [testOperationQueue cancelAllOperations];
 
-            [NSThread sleepForTimeInterval:0.5];
+            [NSThread sleepForTimeInterval:1.0];
 
             expect(testConnectionManager.receivedRequests).toEventually(beEmpty());
         });
