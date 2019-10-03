63589e504546c37da4d9860464a43c230d67e273
NicoleYarroch <nicole@livio.io>
2019-10-03 13:48:02 -0400

    Fixed some grammar

--- SmartDeviceLink/SDLStreamingVideoScaleManager.h
+++ SmartDeviceLink/SDLStreamingVideoScaleManager.h
@@ -26,7 +26,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (assign, nonatomic) float scale;
 
 /**
- The current screen size of a connected display
+ The current screen size of the connected display
  */
 @property (assign, nonatomic) CGSize screenSize;
 
@@ -45,27 +45,27 @@ NS_ASSUME_NONNULL_BEGIN
 + (instancetype)defaultConfiguration;
 
 /**
- Convenience init for creating a scale manager with the scale and connecte display screen size.
+ Convenience init for creating a scale manager with the scale and connected display screen size.
 
  @param scale The scale factor value to scale coordinates from one coordinate space to another
- @param screenSize The current screen size of a connected display
+ @param screenSize The current screen size of the connected display
  @return A SDLStreamingVideoScaleManager object
 */
 - (instancetype)initWithScale:(nullable NSNumber *)scale screenSize:(CGSize)screenSize;
 
 /**
- Scales the coordinates of the OnTouchEvent from the screen's coordinate sysytem to the view controller's coordinate system. If the scale value is less than 1.0, the touch events will not be scaled.
+ Scales the coordinates of the OnTouchEvent from the display's coordinate system to the view controller's coordinate system. If the scale value is less than 1.0, the touch events will be returned without being scaled.
 
  @param onTouchEvent A touch event with coordinates
- @return The touch event coordinates in the screen coordinate system
+ @return The touch event coordinates in the view controller's coordinate system
 */
 - (SDLOnTouchEvent *)scaleTouchEventCoordinates:(SDLOnTouchEvent *)onTouchEvent;
 
 /**
- Scales the haptic rectangle from the view controller coordinate system to the screen coordinate system. If the scale value is less than 1.0, the haptic rectangle will not be scaled.
+ Scales the haptic rectangle from the view controller's coordinate system to the display coordinate system. If the scale value is less than 1.0, the haptic rectangle will be returned without being scaled.
 
  @param hapticRect A haptic rectangle
- @return The position of the haptic rectangle in the screen coordinate system
+ @return The position of the haptic rectangle in the display's coordinate system
 */
 - (SDLHapticRect *)scaleHapticRect:(SDLHapticRect *)hapticRect;
 
