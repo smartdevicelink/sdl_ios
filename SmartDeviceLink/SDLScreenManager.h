41812e37524fe2c4246bd377341c5e4ecf5fae1f
Joel Fischer <joeljfischer@gmail.com>
2019-10-23 11:53:37 -0400

    Remove bad documentation

--- SmartDeviceLink/SDLScreenManager.h
+++ SmartDeviceLink/SDLScreenManager.h
@@ -124,7 +124,7 @@ typedef void(^SDLPreloadChoiceCompletionHandler)(NSError *__nullable error);
 #pragma mark Menu
 
 /**
- Configures the layout of the menu and sub-menus. If set after a menu already exists, the existing main menu layout will be updated, _HOWEVER_ sub-menus will not be automatically updated; you will have to send a new menu to see the new submenu layout.
+ Configures the layout of the menu and sub-menus. If set after a menu already exists, the existing main menu layout will be updated.
 
  If set menu layouts don't match available menu layouts in WindowCapability, an error log will be emitted and the layout will not be set.
 
