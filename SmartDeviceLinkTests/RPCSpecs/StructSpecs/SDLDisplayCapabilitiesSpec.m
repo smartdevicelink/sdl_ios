615df5a225c33c9be0938e2b5e14c7e095216bd7
Justin Gluck <justin.gluck@livio.io>
2019-10-23 12:20:17 -0400

    pr issues

--- SmartDeviceLinkTests/RPCSpecs/StructSpecs/SDLDisplayCapabilitiesSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/StructSpecs/SDLDisplayCapabilitiesSpec.m
@@ -52,16 +52,16 @@ describe(@"Getter/Setter Tests", ^ {
     });
     
     it(@"Should get correctly when initialized", ^ {
-        NSDictionary* dict = [@{SDLRPCParameterNameDisplayType:SDLDisplayTypeGen26DMA,
+        NSDictionary* dict = @{SDLRPCParameterNameDisplayType:SDLDisplayTypeGen26DMA,
                                        SDLRPCParameterNameDisplayName: @"test",
-                                       SDLRPCParameterNameTextFields:[@[textField] mutableCopy],
-                                       SDLRPCParameterNameImageFields:[@[imageField] mutableCopy],
-                                       SDLRPCParameterNameMediaClockFormats:[@[SDLMediaClockFormatClockText1, SDLMediaClockFormatClock3, SDLMediaClockFormatClockText3] copy],
+                                       SDLRPCParameterNameTextFields:@[textField],
+                                       SDLRPCParameterNameImageFields:@[imageField],
+                                       SDLRPCParameterNameMediaClockFormats:@[SDLMediaClockFormatClockText1, SDLMediaClockFormatClock3, SDLMediaClockFormatClockText3],
                                        SDLRPCParameterNameGraphicSupported:@YES,
-                                       SDLRPCParameterNameTemplatesAvailable:[@[@"String", @"String", @"String"] mutableCopy],
+                                       SDLRPCParameterNameTemplatesAvailable:@[@"String", @"String", @"String"],
                                        SDLRPCParameterNameScreenParams:screenParams,
                                        SDLRPCParameterNameNumberCustomPresetsAvailable:@43,
-                                      } mutableCopy];
+                                      };
         SDLDisplayCapabilities* testStruct = [[SDLDisplayCapabilities alloc] initWithDictionary:dict];
         
         expect(testStruct.displayType).to(equal(SDLDisplayTypeGen26DMA));
