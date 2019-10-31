9df686f0a6c4d551956325b2da1f42bf507fe23d
Justin Gluck <justin.gluck@livio.io>
2019-10-31 11:15:38 -0400

    documenting SDLProtocolConstant

--- SmartDeviceLink/SDLProtocolConstants.h
+++ SmartDeviceLink/SDLProtocolConstants.h
@@ -9,74 +9,140 @@
 #import <Foundation/Foundation.h>
 /**
  *  The data packet's header and payload combination.
-
- - SDLFrameTypeControl: Lowest-level type of packets. They can be sent over any of the defined services. They are used for the control of the services in which they are sent.
- - SDLFrameTypeSingle: Contains all the data for a particular packet in the payload. The majority of frames sent over the protocol utilize this frame type.
- - SDLFrameTypeFirst: The First Frame in a multiple frame payload contains information about the entire sequence of frames so that the receiving end can correctly parse all the frames and reassemble the entire payload. The payload of this frame is only eight bytes and contains information regarding the rest of the sequence.
- - SDLFrameTypeConsecutive: The Consecutive Frames in a multiple frame payload contain the actual raw data of the original payload. The parsed payload contained in each of the Consecutive Frames' payloads should be buffered until the entire sequence is complete.
  */
 typedef NS_ENUM(UInt8, SDLFrameType) {
+    ///SDLFrameType
+    ///
+    /// An SDLFrameType: Lowest-level type of packets. They can be sent over any of the defined services. They are used for the control of the services in which they are sent.
     SDLFrameTypeControl = 0x00,
+
+    ///SDLFrameType
+    ///
+    /// An SDLFrameType: Contains all the data for a particular packet in the payload. The majority of frames sent over the protocol utilize this frame type.
     SDLFrameTypeSingle = 0x01,
+
+    ///SDLFrameType
+    ///
+    /// An SDLFrameType: The First Frame in a multiple frame payload contains information about the entire sequence of frames so that the receiving end can correctly parse all the frames and reassemble the entire payload. The payload of this frame is only eight bytes and contains information regarding the rest of the sequence.
     SDLFrameTypeFirst = 0x02,
+
+    ///SDLFrameType
+    ///
+    /// The Consecutive Frames in a multiple frame payload contain the actual raw data of the original payload. The parsed payload contained in each of the Consecutive Frames' payloads should be buffered until the entire sequence is complete.
     SDLFrameTypeConsecutive = 0x03
 };
 
 /**
  *  The data packet's format and priority.
-
- - SDLServiceTypeControl: The lowest level service available.
- - SDLServiceTypeRPC: Used to send requests, responses, and notifications between an application and a head unit.
- - SDLServiceTypeAudio: The application can start the audio service to send PCM audio data to the head unit. After the StartService packet is sent and the ACK received, the payload for the Audio Service is only PCM audio data.
- - SDLServiceTypeVideo: The application can start the video service to send H.264 video data to the head unit. After the StartService packet is sent and the ACK received, the payload for the Video Service is only H.264 video data.
- - SDLServiceTypeBulkData: Similar to the RPC Service but adds a bulk data field. The payload of a message sent via the Hybrid service consists of a Binary Header, JSON Data, and Bulk Data.
  */
 typedef NS_ENUM(UInt8, SDLServiceType) {
+    /// SDLServiceType
+    ///
+    /// The lowest level service available.
     SDLServiceTypeControl = 0x00,
+
+    /// SDLServiceType
+    ///
+    /// Used to send requests, responses, and notifications between an application and a head unit.
     SDLServiceTypeRPC NS_SWIFT_NAME(rpc) = 0x07,
+
+    /// SDLServiceType
+    ///
+    /// The application can start the audio service to send PCM audio data to the head unit. After the StartService packet is sent and the ACK received, the payload for the Audio Service is only PCM audio data.
     SDLServiceTypeAudio = 0x0A,
+
+    /// SDLServiceType
+    ///
+    /// The application can start the video service to send H.264 video data to the head unit. After the StartService packet is sent and the ACK received, the payload for the Video Service is only H.264 video data.
     SDLServiceTypeVideo = 0x0B,
+
+    /// SDLServiceType
+    ///
+    /// Similar to the RPC Service but adds a bulk data field. The payload of a message sent via the Hybrid service consists of a Binary Header, JSON Data, and Bulk Data.
     SDLServiceTypeBulkData = 0x0F
 };
 
 /**
  *  The data packet's available data.
-
- - SDLFrameInfoHeartbeat: A ping packet that is sent to ensure the connection is still active and the service is still valid.
- - SDLFrameInfoStartService: Requests that a specific type of service is started.
- - SDLFrameInfoStartServiceACK: Acknowledges that the specific service has been started successfully.
- - SDLFrameInfoStartServiceNACK: Negatively acknowledges that the specific service was not started.
- - SDLFrameInfoEndService: Requests that a specific type of service is ended.
- - SDLFrameInfoEndServiceACK: Acknowledges that the specific service has been ended successfully.
- - SDLFrameInfoEndServiceNACK: Negatively acknowledges that the specific service was not ended or has not yet been started.
- - SDLFrameInfoRegisterSecondaryTransport: Notifies that a Secondary Transport has been established.
- - SDLFrameInfoRegisterSecondaryTransportACK: Acknowledges that the Secondary Transport has been recognized.
- - SDLFrameInfoRegisterSecondaryTransportNACK: Negatively acknowledges that the Secondary Transport has not been recognized.
- - SDLFrameInfoTransportEventUpdate: Indicates the status or configuration of transport(s) is/are updated.
- - SDLFrameInfoServiceDataAck: Deprecated.
- - SDLFrameInfoHeartbeatACK: Acknowledges that a Heartbeat control packet has been received.
- - SDLFrameInfoSingleFrame: Payload contains a single packet.
- - SDLFrameInfoFirstFrame: First frame in a multiple frame payload.
- - SDLFrameInfoConsecutiveLastFrame: Frame in a multiple frame payload.
  */
 typedef NS_ENUM(UInt8, SDLFrameInfo) {
+    /// SDLFrameInfo
+    ///
+    /// A ping packet that is sent to ensure the connection is still active and the service is still valid.
     SDLFrameInfoHeartbeat = 0x00,
+
+    /// SDLFrameInfo
+    ///
+    /// Requests that a specific type of service is started.
     SDLFrameInfoStartService = 0x01,
+
+    /// SDLFrameInfo
+    ///
+    /// Acknowledges that the specific service has been started successfully.
     SDLFrameInfoStartServiceACK = 0x02,
+
+    /// SDLFrameInfo
+    ///
+    /// Negatively acknowledges that the specific service was not started.
     SDLFrameInfoStartServiceNACK = 0x03,
+
+    /// SDLFrameInfo
+    ///
+    /// Requests that a specific type of service is ended.
     SDLFrameInfoEndService = 0x04,
+
+    /// SDLFrameInfo
+    ///
+    /// Acknowledges that the specific service has been ended successfully.
     SDLFrameInfoEndServiceACK = 0x05,
+
+    /// SDLFrameInfo
+    ///
+    /// Negatively acknowledges that the specific service was not ended or has not yet been started.
     SDLFrameInfoEndServiceNACK = 0x06,
+
+    /// SDLFrameInfo
+    ///
+    /// Notifies that a Secondary Transport has been established.
     SDLFrameInfoRegisterSecondaryTransport = 0x07,
+
+    /// SDLFrameInfo
+    ///
+    /// Acknowledges that the Secondary Transport has been recognized.
     SDLFrameInfoRegisterSecondaryTransportACK = 0x08,
+
+    /// SDLFrameInfo
+    ///
+    /// Negatively acknowledges that the Secondary Transport has not been recognized.
     SDLFrameInfoRegisterSecondaryTransportNACK = 0x09,
+
+    /// SDLFrameInfo
+    ///
+    /// Indicates the status or configuration of transport(s) is/are updated.
     SDLFrameInfoTransportEventUpdate = 0xFD,
+
+    /// SDLFrameInfo
+    ///
+    /// Deprecated.
     SDLFrameInfoServiceDataAck = 0xFE,
+
+    /// SDLFrameInfo
+    ///
+    /// Acknowledges that a Heartbeat control packet has been received.
     SDLFrameInfoHeartbeatACK = 0xFF,
-    // If frameType == Single (0x01)
-    SDLFrameInfoSingleFrame = 0x00,
-    // If frameType == First (0x02)
-    SDLFrameInfoFirstFrame = 0x00,
-    // If frametype == Consecutive (0x03)
-    SDLFrameInfoConsecutiveLastFrame = 0x00
+
+    /// SDLFrameInfo
+    ///
+    /// Payload contains a single packet.
+    SDLFrameInfoSingleFrame = 0x00, // If frameType == Single (0x01)
+
+    /// SDLFrameInfo
+    ///
+    /// First frame in a multiple frame payload.
+    SDLFrameInfoFirstFrame = 0x00, // If frameType == First (0x02)
+
+    /// SDLFrameInfo
+    /// 
+    /// Frame in a multiple frame payload.
+    SDLFrameInfoConsecutiveLastFrame = 0x00 // If frametype == Consecutive (0x03)
 };
