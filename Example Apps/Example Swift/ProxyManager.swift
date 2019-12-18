a49e65196a3a8e0db18ca3b21c7e7c57e8092585
NicoleYarroch <nicole@livio.io>
2019-12-18 12:13:11 -0500

    Reverted swift app globalLogLevel

--- Example Apps/Example Swift/ProxyManager.swift
+++ Example Apps/Example Swift/ProxyManager.swift
@@ -120,7 +120,7 @@ private extension ProxyManager {
         let exampleLogFileModule = SDLLogFileModule(name: "SDL Swift Example App", files: ["ProxyManager", "AlertManager", "AudioManager", "ButtonManager", "MenuManager", "PerformInteractionManager", "RPCPermissionsManager", "VehicleDataManager"])
         logConfig.modules.insert(exampleLogFileModule)
         _ = logConfig.targets.insert(SDLLogTargetFile()) // Logs to file
-        logConfig.globalLogLevel = .verbose // Filters the logs
+        logConfig.globalLogLevel = .debug // Filters the logs
         return logConfig
     }
 
