e6294c6cecd690a4652321d94e2d8a5914327cd6
Kujtim Shala <kshala@ford.com>
2020-02-21 21:33:40 +0100

    Add workaround to scale for older head units with high res.

--- SmartDeviceLink/SDLStreamingVideoLifecycleManager.m
+++ SmartDeviceLink/SDLStreamingVideoLifecycleManager.m
@@ -592,7 +592,13 @@ typedef void(^SDLVideoCapabilityResponseHandler)(SDLVideoStreamingCapability *_N
 #pragma clang diagnostic pop
     if (resolution != nil) {
         self.videoScaleManager.displayViewportResolution = CGSizeMake(resolution.resolutionWidth.floatValue,
-                                 resolution.resolutionHeight.floatValue);
+                                                                      resolution.resolutionHeight.floatValue);
+        // workaround for older high resolution displays that don't support scaling
+        NSString *make = registerResponse.vehicleType.make;
+        CGSize resolution = self.videoScaleManager.displayViewportResolution;
+        if (([make containsString:@"Ford"] || [make containsString:@"Lincoln"]) && (resolution.width > 800 || resolution.height > 800)) {
+            self.videoScaleManager.scale = 1.0f / 0.75f; // Scale by 1.333333
+        }
     }
 
     self.connectedVehicleMake = registerResponse.vehicleType.make;
