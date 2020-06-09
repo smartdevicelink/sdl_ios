f9e2b3a1f90ac2ed5ce4c8228bde305477145c3f
NicoleYarroch <nicole@livio.io>
2020-06-09 08:10:58 -0400

    Added more documentation
    Signed-off-by: NicoleYarroch <nicole@livio.io>

--- SmartDeviceLink/SDLStreamingProtocolDelegate.h
+++ SmartDeviceLink/SDLStreamingProtocolDelegate.h
@@ -14,9 +14,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @protocol SDLStreamingProtocolDelegate <NSObject>
 
-/// Called when protocol instance for audio and/or video service has been updated.
+/// Called when protocol instance for audio and/or video service has been updated. The transport is established
 ///
-/// If `newVideoProtocol` or `newAudioProtocol` is nil it indicates that underlying transport has become unavailable.
+/// If `newVideoProtocol` or `newAudioProtocol` have been set then the transport is established and a current session can be started. If `newVideoProtocol` or `newAudioProtocol` is nil it indicates that underlying transport will become unavailable and any ongoing sessions on that transport should be stopped (i.e. send a request to the module to stop the session and wait for a response).
 ///
 /// @param oldVideoProtocol protocol instance that was being used for video streaming
 /// @param newVideoProtocol protocol instance that will be used for video streaming
@@ -25,6 +25,9 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)didUpdateFromOldVideoProtocol:(nullable SDLProtocol *)oldVideoProtocol toNewVideoProtocol:(nullable SDLProtocol *)newVideoProtocol fromOldAudioProtocol:(nullable SDLProtocol *)oldAudioProtocol toNewAudioProtocol:(nullable SDLProtocol *)newAudioProtocol;
 
 /// Called when the audio and/or video must be stopped because the transport has been destroyed or errored out.
+///
+/// Since the transport does not exist, current sessions can not be properly stopped (i.e. no request can be sent module to stop the session) but the observer still needs to stop creating and sending data.
+///
 /// @param videoProtocol protocol instance that was being used for video streaming
 /// @param audioProtocol protocol instance that was being used for audio streaming
 - (void)destroyVideoProtocol:(nullable SDLProtocol *)videoProtocol audioProtocol:(nullable SDLProtocol *)audioProtocol;
