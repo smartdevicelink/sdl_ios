de646782cb4541030ab6b403b9d79c15bd1c5cc5
NicoleYarroch <nicole@livio.io>
2020-06-19 16:10:11 -0400

    Added tests
    Signed-off-by: NicoleYarroch <nicole@livio.io>

--- SmartDeviceLinkTests/SDLSubscribeButtonObserverSpec.m
+++ SmartDeviceLinkTests/SDLSubscribeButtonObserverSpec.m
@@ -6,4 +6,65 @@
 //  Copyright © 2020 smartdevicelink. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Quick/Quick.h>
+#import <Nimble/Nimble.h>
+#import <OCMock/OCMock.h>
+
+#import "SDLSubscribeButtonObserver.h"
+
+@implementation SDLSubscribeButtonObserver (Tests)
+
+- (void)testMethod {}
+
+@end
+
+QuickSpecBegin(SDLSubscribeButtonObserverSpec)
+
+describe(@"subscribe button observer", ^{
+    __block id<NSObject> testObserver = nil;
+    __block SEL testSelector = nil;
+    __block SDLSubscribeButtonUpdateHandler testUpdateBlock = nil;
+
+    beforeEach(^{
+        testObserver = [[NSObject alloc] init];
+        testSelector = @selector(testMethod);
+        testUpdateBlock = ^(SDLOnButtonPress *_Nullable buttonPress, SDLOnButtonEvent *_Nullable buttonEvent, NSError *_Nullable error) {};
+    });
+
+    it(@"Should set and get correctly", ^{
+        SDLSubscribeButtonObserver *testSubscribeButtonObserver = [[SDLSubscribeButtonObserver alloc] init];
+        testSubscribeButtonObserver.observer = testObserver;
+        testSubscribeButtonObserver.selector = testSelector;
+        testSubscribeButtonObserver.updateBlock = testUpdateBlock;
+
+        expect(testSubscribeButtonObserver.observer).to(equal(testObserver));
+        expect(NSStringFromSelector(testSubscribeButtonObserver.selector)).to(equal(NSStringFromSelector(testSelector)));
+        expect((id)testSubscribeButtonObserver.updateBlock).to(equal((id)testUpdateBlock));
+    });
+
+    it(@"Should initialize correctly with initWithObserver:selector:", ^{
+        SDLSubscribeButtonObserver *testSubscribeButtonObserver = [[SDLSubscribeButtonObserver alloc] initWithObserver:testObserver selector:testSelector];
+
+        expect(testSubscribeButtonObserver.observer).to(equal(testObserver));
+        expect(NSStringFromSelector(testSubscribeButtonObserver.selector)).to(equal(NSStringFromSelector(testSelector)));
+        expect((id)testSubscribeButtonObserver.updateBlock).to(beNil());
+    });
+
+    it(@"Should initialize correctly with initWithObserver:updateHandler:", ^{
+        SDLSubscribeButtonObserver *testSubscribeButtonObserver = [[SDLSubscribeButtonObserver alloc] initWithObserver:testObserver updateHandler:testUpdateBlock];
+
+        expect(testSubscribeButtonObserver.observer).to(equal(testObserver));
+        expect(NSStringFromSelector(testSubscribeButtonObserver.selector)).to(beNil());
+        expect((id)testSubscribeButtonObserver.updateBlock).to(equal((id)testUpdateBlock));
+    });
+
+    it(@"Should return nil if not set", ^{
+        SDLSubscribeButtonObserver *testSubscribeButtonObserver = [[SDLSubscribeButtonObserver alloc] init];
+
+        expect(testSubscribeButtonObserver.observer).to(beNil());
+        expect(NSStringFromSelector(testSubscribeButtonObserver.selector)).to(beNil());
+        expect((id)testSubscribeButtonObserver.updateBlock).to(beNil());
+    });
+});
+
+QuickSpecEnd
