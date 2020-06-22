83c6433a860500a1e7377b96fc3ddc808a7edaac
NicoleYarroch <nicole@livio.io>
2020-06-22 11:14:08 -0400

    Added helper class for testing observers
    Signed-off-by: NicoleYarroch <nicole@livio.io>

---
+++ SmartDeviceLink/TestSubscribeButtonObserver.h
@@ -0,0 +1,17 @@
+//
+//  TestSubscribeButtonObserver.h
+//  SmartDeviceLink
+//
+//  Created by Nicole on 6/22/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestSubscribeButtonObserver : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SmartDeviceLink/TestSubscribeButtonObserver.m
@@ -0,0 +1,13 @@
+//
+//  TestSubscribeButtonObserver.m
+//  SmartDeviceLink
+//
+//  Created by Nicole on 6/22/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import "TestSubscribeButtonObserver.h"
+
+@implementation TestSubscribeButtonObserver
+
+@end
