dfe064286ea5f5bd43f12b26fb8db3d9f104ebc5
NicoleYarroch <nicole@livio.io>
2020-07-21 08:31:11 -0400

    Added more docs
    Signed-off-by: NicoleYarroch <nicole@livio.io>

--- SmartDeviceLink/SDLProtocolDelegate.h
+++ SmartDeviceLink/SDLProtocolDelegate.h
@@ -20,39 +20,46 @@ NS_ASSUME_NONNULL_BEGIN
 /// @param protocol The transport's protocol
 - (void)onProtocolMessageReceived:(SDLProtocolMessage *)msg protocol:(SDLProtocol *)protocol;
 
-/// Called when the start service request succeeds.
+/// Called when the start service frame succeeds.
+/// @discussion This frame can be sent on both the primary and secondary transports
 /// @param startServiceACK A SDLProtocolMessage object
 /// @param protocol The transport's protocol
 - (void)handleProtocolStartServiceACKMessage:(SDLProtocolMessage *)startServiceACK protocol:(SDLProtocol *)protocol;
 
-/// Called when the start service request fails.
+/// Called when the start service frame fails.
+/// @discussion This frame can be sent on both the primary and secondary transports
 /// @param startServiceNAK A SDLProtocolMessage object
 /// @param protocol The transport's protocol
 - (void)handleProtocolStartServiceNAKMessage:(SDLProtocolMessage *)startServiceNAK protocol:(SDLProtocol *)protocol;
 
-/// Called when the end service request succeeds.
+/// Called when the end service frame succeeds.
+/// @discussion This frame can be sent on both the primary and secondary transports
 /// @param endServiceACK A SDLProtocolMessage object
 /// @param protocol The transport's protocol
 - (void)handleProtocolEndServiceACKMessage:(SDLProtocolMessage *)endServiceACK protocol:(SDLProtocol *)protocol;
 
-/// Called when the end service request fails.
+/// Called when the end service frame fails.
+/// @discussion This frame can be sent on both the primary and secondary transports
 /// @param endServiceNAK A SDLProtocolMessage object
 /// @param protocol The transport's protocol
 - (void)handleProtocolEndServiceNAKMessage:(SDLProtocolMessage *)endServiceNAK protocol:(SDLProtocol *)protocol;
 
 #pragma mark Secondary Transport Messages
 
-/// Called when the secondary transport registration succeeds.
+/// Called when the secondary transport registration frame succeeds.
+/// @discussion This frame is only sent on the secondary transport
 /// @param registerSecondaryTransportACK A SDLProtocolMessage object
 /// @param protocol The transport's protocol
 - (void)handleProtocolRegisterSecondaryTransportACKMessage:(SDLProtocolMessage *)registerSecondaryTransportACK protocol:(SDLProtocol *)protocol;
 
-/// Called when the secondary transport registration fails.
+/// Called when the secondary transport registration frame fails.
+/// @discussion This frame is only sent on the secondary transport
 /// @param registerSecondaryTransportNAK A SDLProtocolMessage object
 /// @param protocol The transport's protocol
 - (void)handleProtocolRegisterSecondaryTransportNAKMessage:(SDLProtocolMessage *)registerSecondaryTransportNAK protocol:(SDLProtocol *)protocol;
 
 /// Called when the status or configuration of one or more transports has updated.
+/// @discussion This frame is only sent on the primary transport
 /// @param transportEventUpdate A SDLProtocolMessage object
 /// @param protocol The transport's protocol
 - (void)handleTransportEventUpdateMessage:(SDLProtocolMessage *)transportEventUpdate protocol:(SDLProtocol *)protocol;
@@ -60,10 +67,12 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark Deprecated Messages
 
 /// A ping packet that is sent to ensure the connection is still active and the service is still valid.
+/// @discussion Deprecated - requires protocol major version 3
 /// @param session The session number
 - (void)handleHeartbeatForSession:(Byte)session;
 
-/// Called when the heartbeat message was recieved successfully.
+/// Called when the heartbeat frame was recieved successfully.
+/// @discussion Deprecated - requires protocol major version 3
 - (void)handleHeartbeatACK;
 
 #pragma mark - Transport Lifecycle
