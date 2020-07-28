1386339ee0082a36becd33d234f1f5974712274e
leonid lokhmatov, Luxoft <zaqqqqqqqq@gmail.com>
2020-07-28 14:16:48 +0300

    sdl0200 'Removing-URL-Param-Max-Length': apply review comments (test with NSURL)

--- SmartDeviceLinkTests/RPCSpecs/NotificationSpecs/SDLOnSystemRequestSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/NotificationSpecs/SDLOnSystemRequestSpec.m
@@ -2,19 +2,17 @@
 //  SDLOnSystemRequestSpec.m
 //  SmartDeviceLink
 
-
 #import <Foundation/Foundation.h>
-
-#import <Quick/Quick.h>
 #import <Nimble/Nimble.h>
+#import <Quick/Quick.h>
 
 #import "SDLFileType.h"
-#import "SDLRPCParameterNames.h"
-#import "SDLRPCFunctionNames.h"
 #import "SDLOnSystemRequest.h"
+#import "SDLRPCFunctionNames.h"
+#import "SDLRPCParameterNames.h"
 #import "SDLRequestType.h"
 
-static NSString * const SDLTestURL = @"https://www.smartdevicelink.com";
+static NSString *const SDLTestURL = @"https://www.smartdevicelink.com";
 
 QuickSpecBegin(SDLOnSystemRequestSpec)
 
@@ -57,7 +55,7 @@ describe(@"Getter/Setter Tests", ^ {
         
         expect(testNotification.requestType).to(equal(SDLRequestTypeFileResume));
         expect(testNotification.requestSubType).to(equal(@"subtype"));
-        expect(testNotification.url).to(equal([SDLTestURL mutableCopy]));
+        expect(testNotification.url).to(equal(SDLTestURL));
         expect(testNotification.timeout).to(equal(@52345));
         expect(testNotification.fileType).to(equal(SDLFileTypePNG));
         expect(testNotification.offset).to(equal(@2532678684));
@@ -76,14 +74,18 @@ describe(@"Getter/Setter Tests", ^ {
         expect(testNotification.length).to(beNil());
     });
 
-    it(@"Should set and get url correctly (zero length url)", ^ {
-        SDLOnSystemRequest* testRequest = [[SDLOnSystemRequest alloc] init];
+    it(@"Should set and get url correctly (zero length url)", ^{
+        SDLOnSystemRequest *testRequest = [[SDLOnSystemRequest alloc] init];
         NSString *testURL = @"";
         testRequest.url = testURL;
         expect(testRequest.url).to(equal(testURL));
+
+        NSURL *nsurl = [NSURL URLWithString:testURL];
+        expect(nsurl).notTo(beNil());
+        expect(nsurl.absoluteString).to(equal(testURL));
     });
 
-    it(@"Should set and get url correctly (1000 sym long url)", ^ {
+    it(@"Should set and get url correctly (1000 sym long url)", ^{
         const int size = 1000;
         SDLOnSystemRequest* testRequest = [[SDLOnSystemRequest alloc] init];
         NSMutableString *testURL = [NSMutableString stringWithCapacity:1024];
@@ -97,11 +99,15 @@ describe(@"Getter/Setter Tests", ^ {
         }
         testRequest.url = testURL;
         expect(testRequest.url).to(equal(testURL));
+
+        NSURL *nsurl = [NSURL URLWithString:testURL];
+        expect(nsurl).notTo(beNil());
+        expect(nsurl.absoluteString).to(equal(testURL));
     });
 
-    it(@"Should set and get url correctly (extra long url)", ^ {
+    it(@"Should set and get url correctly (extra long url)", ^{
         const int size = 1024 * 1024;
-        SDLOnSystemRequest* testRequest = [[SDLOnSystemRequest alloc] init];
+        SDLOnSystemRequest *testRequest = [[SDLOnSystemRequest alloc] init];
         NSMutableString *testURL = [NSMutableString stringWithCapacity:size + 20];
         [testURL setString:SDLTestURL];
         while(size > testURL.length) {
@@ -113,6 +119,10 @@ describe(@"Getter/Setter Tests", ^ {
         }
         testRequest.url = testURL;
         expect(testRequest.url).to(equal(testURL));
+
+        NSURL *nsurl = [NSURL URLWithString:testURL];
+        expect(nsurl).notTo(beNil());
+        expect(nsurl.absoluteString).to(equal(testURL));
     });
 });
 
