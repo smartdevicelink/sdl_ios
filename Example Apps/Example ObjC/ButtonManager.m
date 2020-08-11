a41b2f32b4f52211547265eb053bffe417d98311
NicoleYarroch <nicole@livio.io>
2020-08-11 10:33:34 -0400

    Fixed capitalization
    Signed-off-by: NicoleYarroch <nicole@livio.io>

--- Example Apps/Example ObjC/ButtonManager.m
+++ Example Apps/Example ObjC/ButtonManager.m
@@ -66,11 +66,11 @@ NS_ASSUME_NONNULL_BEGIN
     SDLSoftButtonState *alertImageAndTextState = [[SDLSoftButtonState alloc] initWithStateName:AlertSoftButtonImageState text:AlertSoftButtonText artwork:[SDLArtwork artworkWithImage:[[UIImage imageNamed:CarBWIconImageName] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] name:CarBWIconImageName asImageFormat:SDLArtworkImageFormatPNG]];
     SDLSoftButtonState *alertTextState = [[SDLSoftButtonState alloc] initWithStateName:AlertSoftButtonTextState text:AlertSoftButtonText image:nil];
 
-    __weak typeof(self) weakSelf = self;
+    __weak typeof(self) weakself = self;
     SDLSoftButtonObject *alertSoftButton = [[SDLSoftButtonObject alloc] initWithName:AlertSoftButton states:@[alertImageAndTextState, alertTextState] initialStateName:alertImageAndTextState.name handler:^(SDLOnButtonPress * _Nullable buttonPress, SDLOnButtonEvent * _Nullable buttonEvent) {
         if (buttonPress == nil) { return; }
 
-        [AlertManager sendAlertWithImage:CarBWIconImageName textField1:AlertMessageText textField2:nil sdlManager:weakSelf.sdlManager];
+        [AlertManager sendAlertWithImage:CarBWIconImageName textField1:AlertMessageText textField2:nil sdlManager:weakself.sdlManager];
     }];
 
     return alertSoftButton;
@@ -79,15 +79,15 @@ NS_ASSUME_NONNULL_BEGIN
 /// Returns a soft button that shows a subtle alert when tapped. If the subtle alert is not supported, then a regular alert is shown.
 /// @returns A SDLSoftButtonObject object
 - (SDLSoftButtonObject *)sdlex_softButtonSubtleAlert {
-    __weak typeof(self) weakSelf = self;
+    __weak typeof(self) weakself = self;
     return [[SDLSoftButtonObject alloc] initWithName:SubtleAlertSoftButton text:nil artwork: [SDLArtwork artworkWithImage:[[UIImage imageNamed:BatteryFullBWIconName] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] asImageFormat:SDLArtworkImageFormatPNG] handler:^(SDLOnButtonPress * _Nullable buttonPress, SDLOnButtonEvent * _Nullable buttonEvent) {
         if (buttonPress == nil) { return; }
 
-        BOOL isSubtleAlertAllowed = [weakSelf.sdlManager.permissionManager isRPCNameAllowed:SDLRPCFunctionNameSubtleAlert];
+        BOOL isSubtleAlertAllowed = [weakself.sdlManager.permissionManager isRPCNameAllowed:SDLRPCFunctionNameSubtleAlert];
         if (isSubtleAlertAllowed) {
-            [AlertManager sendSubtleAlertWithImage:BatteryEmptyBWIconName textField1:SubtleAlertHeaderText textField2:SubtleAlertSubheaderText sdlManager:weakSelf.sdlManager];
+            [AlertManager sendSubtleAlertWithImage:BatteryEmptyBWIconName textField1:SubtleAlertHeaderText textField2:SubtleAlertSubheaderText sdlManager:weakself.sdlManager];
         } else {
-            [AlertManager sendAlertWithImage:BatteryEmptyBWIconName textField1:SubtleAlertHeaderText textField2:SubtleAlertSubheaderText sdlManager:weakSelf.sdlManager];
+            [AlertManager sendAlertWithImage:BatteryEmptyBWIconName textField1:SubtleAlertHeaderText textField2:SubtleAlertSubheaderText sdlManager:weakself.sdlManager];
         }
     }];
 }
