e55963434b0dfdd3af7a1f315561ab543a70d70b
NicoleYarroch <nicole@livio.io>
2020-08-14 07:44:43 -0400

    Fixing newline
    Signed-off-by: NicoleYarroch <nicole@livio.io>

--- SmartDeviceLink/SDLNotificationConstants.h
+++ SmartDeviceLink/SDLNotificationConstants.h
@@ -183,7 +183,6 @@ extern SDLNotificationName const SDLDidReceiveDeleteCommandResponse;
 /// Name for a DeleteFile response RPC
 extern SDLNotificationName const SDLDidReceiveDeleteFileResponse;
 
-
 /// Name for a DeleteInteractionChoiceSet response RPC
 extern SDLNotificationName const SDLDidReceiveDeleteInteractionChoiceSetResponse;
 
