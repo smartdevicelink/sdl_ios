a404e306a75ff10b01953a45dd644b609e85bf5f
Joel Fischer <joeljfischer@gmail.com>
2020-09-17 15:21:01 -0400

    First attempt at the package file

--- Package.swift
+++ Package.swift
@@ -1,8 +1,12 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "SmartDeviceLink",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         .library(name: "SmartDeviceLink", targets: ["SmartDeviceLink"]),
         .library(name: "SmartDeviceLinkSwift", targets: ["SmartDeviceLinkSwift"])
@@ -10,7 +14,12 @@ let package = Package(
     targets: [
         .target(
             name: "SmartDeviceLink",
-            path: "SmartDeviceLink"
+            path: "SmartDeviceLink",
+            publicHeadersPath: "public",
+            resources: [
+                .process("SDLAssets.xcassets"),
+                .process("SDLLockScreen.storyboard")
+            ]
         ),
         .target(
             name: "SmartDeviceLinkSwift",
