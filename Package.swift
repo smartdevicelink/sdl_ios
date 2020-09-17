c2c3918ce20123060652383385e7ed6a5ed32456
Joel Fischer <joeljfischer@gmail.com>
2020-09-17 15:30:53 -0400

    Extra comma

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
 //                .process("SDLAssets.xcassets"),
 //                .process("SDLLockScreen.storyboard")
 //            ]
-            publicHeadersPath: "public",
+            publicHeadersPath: "public"
         ),
         .target(
             name: "SmartDeviceLinkSwift",
