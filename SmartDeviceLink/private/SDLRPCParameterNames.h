beb8e21d592656341303094227c7df75cc558bd5
leonid lokhmatov, Luxoft <zaqqqqqqqq@gmail.com>
2020-11-14 01:36:07 +0200

    SDL-0269 'New vehicle data ClimateData': minor update - add deprecated messages and warning supress, etc

--- SmartDeviceLink/private/SDLRPCParameterNames.h
+++ SmartDeviceLink/private/SDLRPCParameterNames.h
@@ -242,7 +242,7 @@ extern SDLRPCParameterName const SDLRPCParameterNameEscSystem;
 extern SDLRPCParameterName const SDLRPCParameterNameETA;
 extern SDLRPCParameterName const SDLRPCParameterNameEvent;
 extern SDLRPCParameterName const SDLRPCParameterNameExpires;
-extern SDLRPCParameterName const SDLRPCParameterNameExternalTemperature __deprecated_msg("Use SDLRPCParameterNameClimateData instead on 7.1+ RPC version connections");
+extern SDLRPCParameterName const SDLRPCParameterNameExternalTemperature __deprecated;
 extern SDLRPCParameterName const SDLRPCParameterNameFanSpeed;
 extern SDLRPCParameterName const SDLRPCParameterNameFanSpeedAvailable;
 extern SDLRPCParameterName const SDLRPCParameterNameFCCFacilityId;
