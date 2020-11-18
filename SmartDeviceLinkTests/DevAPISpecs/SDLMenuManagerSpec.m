4bc3a7b374ebf2735680a70612ee87e0681273da
Frank Elias <francois.elias@livio.io>
2020-11-18 09:14:04 -0500

    MenuManager unit test

--- SmartDeviceLinkTests/DevAPISpecs/SDLMenuManagerSpec.m
+++ SmartDeviceLinkTests/DevAPISpecs/SDLMenuManagerSpec.m
@@ -46,6 +46,7 @@ describe(@"menu manager", ^{
     __block SDLSystemCapabilityManager *mockSystemCapabilityManager = nil;
     __block SDLArtwork *testArtwork = nil;
     __block SDLArtwork *testArtwork2 = nil;
+    __block SDLArtwork *testArtwork3 = nil;
 
     __block SDLMenuCell *textOnlyCell = nil;
     __block SDLMenuCell *textOnlyCell2 = nil;
@@ -58,6 +59,8 @@ describe(@"menu manager", ^{
     beforeEach(^{
         testArtwork = [[SDLArtwork alloc] initWithData:[@"Test data" dataUsingEncoding:NSUTF8StringEncoding] name:@"some artwork name" fileExtension:@"png" persistent:NO];
         testArtwork2 = [[SDLArtwork alloc] initWithData:[@"Test data 2" dataUsingEncoding:NSUTF8StringEncoding] name:@"some artwork name 2" fileExtension:@"png" persistent:NO];
+        testArtwork3 = [[SDLArtwork alloc] initWithData:[@"Test data 3" dataUsingEncoding:NSUTF8StringEncoding] name:@"some artwork name" fileExtension:@"png" persistent:NO];
+        testArtwork3.overwrite = true;
 
         textOnlyCell = [[SDLMenuCell alloc] initWithTitle:@"Test 1" icon:nil voiceCommands:nil handler:^(SDLTriggerSource  _Nonnull triggerSource) {}];
         textAndImageCell = [[SDLMenuCell alloc] initWithTitle:@"Test 2" icon:testArtwork voiceCommands:nil handler:^(SDLTriggerSource  _Nonnull triggerSource) {}];
@@ -211,6 +214,8 @@ describe(@"menu manager", ^{
             context(@"when the image is already on the head unit", ^{
                 beforeEach(^{
                     OCMStub([mockFileManager hasUploadedFile:[OCMArg isNotNil]]).andReturn(YES);
+                    OCMExpect([mockFileManager hasUploadedFile:testArtwork3]);
+                    OCMExpect([mockFileManager uploadArtwork:testArtwork3 completionHandler:nil]);
                 });
 
                 it(@"should properly update an image cell", ^{
@@ -228,6 +233,25 @@ describe(@"menu manager", ^{
                     expect(submenu).to(haveCount(1));
                     expect(sentCommand.cmdIcon.value).to(equal(testArtwork.name));
                     expect(sentSubmenu.menuIcon.value).to(equal(testArtwork2.name));
+                });
+
+                fit(@"should properly override an image cell", ^{
+                    testManager.menuCells = @[textAndImageCell, submenuImageCell];
+
+                    NSPredicate *addCommandPredicate = [NSPredicate predicateWithFormat:@"self isMemberOfClass: %@", [SDLAddCommand class]];
+                    NSArray *add = [[mockConnectionManager.receivedRequests copy] filteredArrayUsingPredicate:addCommandPredicate];
+                    SDLAddCommand *sentCommand = add.firstObject;
+
+                    NSPredicate *addSubmenuPredicate = [NSPredicate predicateWithFormat:@"self isMemberOfClass: %@", [SDLAddSubMenu class]];
+                    NSArray *submenu = [[mockConnectionManager.receivedRequests copy] filteredArrayUsingPredicate:addSubmenuPredicate];
+                    SDLAddSubMenu *sentSubmenu = submenu.firstObject;
+//                    sentSubmenu.menuIcon = testArtwork3.imageRPC;
+                    sentCommand.cmdIcon = testArtwork3.imageRPC;
+                    expect(add).to(haveCount(1));
+                    expect(submenu).to(haveCount(1));
+                    expect(sentCommand.cmdIcon.value).to(equal(testArtwork.name));
+                    expect(sentSubmenu.menuIcon.value).to(equal(testArtwork2.name));
+                    OCMVerifyAll(mockFileManager);
                 });
             });
 
