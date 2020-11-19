4342a6c9a87f381e6220ea6a6a3996d4e4447a37
NicoleYarroch <nicole@livio.io>
2020-11-19 07:55:38 -0500

    Added test specs
    Signed-off-by: NicoleYarroch <nicole@livio.io>

---
+++ SmartDeviceLinkTests/SDLAlertManagerSpec.m
@@ -0,0 +1,9 @@
+//
+//  SDLAlertManagerSpec.m
+//  SmartDeviceLinkTests
+//
+//  Created by Nicole on 11/18/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ SmartDeviceLinkTests/SDLPresentAlertOperationSpec.m
@@ -0,0 +1,9 @@
+//
+//  SDLPresentAlertOperationSpec.m
+//  SmartDeviceLinkTests
+//
+//  Created by Nicole on 11/18/20.
+//  Copyright © 2020 smartdevicelink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
