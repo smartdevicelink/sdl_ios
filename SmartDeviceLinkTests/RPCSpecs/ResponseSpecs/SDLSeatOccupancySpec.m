a9f15208b819bad10059d153c847c7676da91414
YLokhmatov@luxoft.com <zaqqqqqqqq@gmail.com>
2021-01-15 12:21:40 +0200

    [0262] 'new vehicle data SeatOccupancy': Apply suggestions from code review
    Co-authored-by: Joel Fischer <joeljfischer@gmail.com>

--- SmartDeviceLinkTests/RPCSpecs/ResponseSpecs/SDLSeatOccupancySpec.m
+++ SmartDeviceLinkTests/RPCSpecs/ResponseSpecs/SDLSeatOccupancySpec.m
@@ -20,43 +20,41 @@ NSArray *seatStatusArr = @[seatStatus];
 
 describe(@"getter/setter tests", ^{
     context(@"init", ^{
-        SDLSeatOccupancy* testStruct = [[SDLSeatOccupancy alloc] init];
-        testStruct.seatsBelted = seatStatusArr;
-        testStruct.seatsOccupied = seatStatusArr;
-
         it(@"expect all properties to be set properly", ^{
+            SDLSeatOccupancy* testStruct = [[SDLSeatOccupancy alloc] init];
+            testStruct.seatsBelted = seatStatusArr;
+            testStruct.seatsOccupied = seatStatusArr;
+
             expect(testStruct.seatsBelted).to(equal(seatStatusArr));
             expect(testStruct.seatsOccupied).to(equal(seatStatusArr));
         });
     });
     
     context(@"initWithDictionary:", ^{
-        NSDictionary *dict = @{
-            SDLRPCParameterNameSeatsBelted:seatStatusArr,
-            SDLRPCParameterNameSeatsOccupied:seatStatusArr,
-        };
-
-        SDLSeatOccupancy* testStruct = [[SDLSeatOccupancy alloc] initWithDictionary:dict];
-
         it(@"expect all properties to be set properly", ^{
+            NSDictionary *dict = @{
+                SDLRPCParameterNameSeatsBelted:seatStatusArr,
+                SDLRPCParameterNameSeatsOccupied:seatStatusArr,
+            };
+
+            SDLSeatOccupancy* testStruct = [[SDLSeatOccupancy alloc] initWithDictionary:dict];
+
             expect(testStruct.seatsBelted).to(equal(seatStatusArr));
             expect(testStruct.seatsOccupied).to(equal(seatStatusArr));
         });
     });
     
     context(@"init", ^{
-        SDLSeatOccupancy* testStruct = [[SDLSeatOccupancy alloc] init];
-
         it(@"expect all properties to be nil", ^{
+            SDLSeatOccupancy* testStruct = [[SDLSeatOccupancy alloc] init];
             expect(testStruct.seatsBelted).to(beNil());
             expect(testStruct.seatsOccupied).to(beNil());
         });
     });
 
     context(@"initWithSeatsOccupied:seatsBelted:", ^{
-        SDLSeatOccupancy* testStruct = [[SDLSeatOccupancy alloc] initWithSeatsOccupied:seatStatusArr seatsBelted:seatStatusArr];
-
         it(@"expect all properties to be set properly", ^{
+            SDLSeatOccupancy* testStruct = [[SDLSeatOccupancy alloc] initWithSeatsOccupied:seatStatusArr seatsBelted:seatStatusArr];
             expect(testStruct.seatsBelted).to(equal(seatStatusArr));
             expect(testStruct.seatsOccupied).to(equal(seatStatusArr));
         });
--- SmartDeviceLinkTests/RPCSpecs/ResponseSpecs/SDLSeatStatusSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/ResponseSpecs/SDLSeatStatusSpec.m
@@ -20,43 +20,43 @@ NSNumber<SDLBool> *conditionActive = @YES;
 
 describe(@"getter/setter tests", ^{
     context(@"init", ^{
-        SDLSeatStatus* testStruct = [[SDLSeatStatus alloc] init];
-        testStruct.conditionActive = conditionActive;
-        testStruct.seatLocation = seatLocation;
-
         it(@"expect all properties to be set properly", ^{
+            SDLSeatStatus* testStruct = [[SDLSeatStatus alloc] init];
+            testStruct.conditionActive = conditionActive;
+            testStruct.seatLocation = seatLocation;
+
             expect(testStruct.conditionActive).to(equal(conditionActive));
             expect(testStruct.seatLocation).to(equal(seatLocation));
         });
     });
 
     context(@"initWithDictionary:", ^{
-        NSDictionary *dict = @{
-            SDLRPCParameterNameConditionActive:conditionActive,
-            SDLRPCParameterNameSeatLocation:seatLocation,
-        };
-
-        SDLSeatStatus* testStruct = [[SDLSeatStatus alloc] initWithDictionary:dict];
-
         it(@"expect all properties to be set properly", ^{
+            NSDictionary *dict = @{
+                SDLRPCParameterNameConditionActive:conditionActive,
+                SDLRPCParameterNameSeatLocation:seatLocation,
+            };
+
+            SDLSeatStatus* testStruct = [[SDLSeatStatus alloc] initWithDictionary:dict];
+
             expect(testStruct.conditionActive).to(equal(conditionActive));
             expect(testStruct.seatLocation).to(equal(seatLocation));
         });
     });
 
     context(@"init", ^{
-        SDLSeatStatus* testStruct = [[SDLSeatStatus alloc] init];
-
         it(@"expect all properties to be nil", ^{
+            SDLSeatStatus* testStruct = [[SDLSeatStatus alloc] init];
+
             expect(testStruct.conditionActive).to(beNil());
             expect(testStruct.seatLocation).to(beNil());
         });
     });
 
     context(@"initWithSeatsOccupied:seatsBelted:", ^{
-        SDLSeatStatus* testStruct = [[SDLSeatStatus alloc] initWithSeatLocation:seatLocation conditionActive:conditionActive];
-
         it(@"expect all properties to be set properly", ^{
+                SDLSeatStatus* testStruct = [[SDLSeatStatus alloc] initWithSeatLocation:seatLocation conditionActive:conditionActive];
+
             expect(testStruct.conditionActive).to(equal(conditionActive));
             expect(testStruct.seatLocation).to(equal(seatLocation));
         });
