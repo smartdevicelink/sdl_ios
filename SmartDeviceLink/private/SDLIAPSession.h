5de3bbb5cf64045c1b7168e7d968fef789db986b
Michael Kinney <michaelakinney@comcast.net>
2021-01-21 19:18:15 -0500

    refactor sdliapsession

--- SmartDeviceLink/private/SDLIAPSession.h
+++ SmartDeviceLink/private/SDLIAPSession.h
@@ -8,38 +8,41 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@protocol SDLIAPSessionDelegate <NSObject>
 
+- (void)streamsDidOpen;
+- (void)streamsDidEnd;
+- (void)streamHasSpaceToWrite;
+- (void)streamDidError;
+- (void)streamHasBytesAvailable:(NSInputStream *)inputStream;
+
+@end
 @interface SDLIAPSession : NSObject <NSStreamDelegate>
 
 /**
- *  The accessory with which to open a session.
+ *  Convenience initializer for setting an accessory and protocol string.
+ *
+ *  @param accessory    The accessory with which to open a session
+ *  @param protocol     The unique protocol string used to create the session with the accessory
+ *  @return             A SDLIAPSession object
  */
-@property (nullable, strong, nonatomic, readonly) EAAccessory *accessory;
+- (instancetype)initWithAccessory:(nullable EAAccessory *)accessory forProtocol:(NSString *)protocol iAPSessionDelegate:(id<SDLIAPSessionDelegate>)iAPSessionDelegate;
+
+// document
+- (void) closeSession;
+
+// document
+- (void)write:(NSMutableData *) data length: (NSUInteger) length  withCompletionHandler:(void (^)(NSInteger bytesWritten))completionHandler;
 
 /**
- *  The session created between the app and the accessory.
+ *  The accessory with which to open a session.  Should be refactored to remove need to  make this public
  */
-@property (nullable, strong, nonatomic, readonly) EASession *eaSession;
+@property (nullable, strong, nonatomic, readonly) EAAccessory *accessory;
 
 /**
  *  The unique protocol string used to create the session with the accessory.
  */
 @property (nullable, strong, nonatomic, readonly) NSString *protocolString;
-
-/**
- *  Returns whether or not both the input and output streams for the session are closed.
- */
-@property (assign, readonly, getter=isStopped) BOOL stopped;
-
-/**
- * The input stream for the session is open when a `NSStreamEventOpenCompleted` event is received for the input stream. The input stream is closed when the stream status is `NSStreamStatusClosed`.
- */
-@property (nonatomic, assign) BOOL isInputStreamOpen;
-
-/**
- * The output stream for the session is open when a `NSStreamEventOpenCompleted` event is received for the output stream. The output stream has been closed when the stream status is `NSStreamStatusClosed`.
- */
-@property (nonatomic, assign) BOOL isOutputStreamOpen;
 
 /**
  *  The unique ID assigned to the session between the app and accessory. If no session exists the value will be 0.
@@ -51,51 +54,14 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property (assign, nonatomic, readonly, getter=isSessionInProgress) BOOL sessionInProgress;
 
-/**
- *  Convenience initializer for setting an accessory and protocol string.
- *
- *  @param accessory    The accessory with which to open a session
- *  @param protocol     The unique protocol string used to create the session with the accessory
- *  @return             A SDLIAPSession object
- */
-- (instancetype)initWithAccessory:(nullable EAAccessory *)accessory forProtocol:(NSString *)protocol;
-
-/**
- *  Starts a session.
- */
-- (void)startSession;
-
-/// Stops the current session.
-/// @param disconnectCompletionHandler Handler called when the session has been closed
-- (void)destroySessionWithCompletionHandler:(void (^)(void))disconnectCompletionHandler;
-
-/**
- *  Creates a session with the accessory.
- *
- *  @return Whether or not the session was created successfully
- */
-- (BOOL)createSession;
-
-/**
- *  Starts a session input or output stream.
- *
- *  @param stream The stream to be started.
- */
-- (void)startStream:(NSStream *)stream;
-
-/**
- *  Stops a session input or output stream.
- *
- *  @param stream The stream to be stopped.
- */
-- (void)stopStream:(NSStream *)stream;
-
-/**
- *  Cleans up a closed session
- */
-- (void)cleanupClosedSession;
+// document
+@property(readonly) BOOL hasSpaceAvailable;
+@property(readonly) BOOL bothStreamsOpen;
+@property(readonly) BOOL isConnected;
 
 
 @end
 
 NS_ASSUME_NONNULL_END
+
+
