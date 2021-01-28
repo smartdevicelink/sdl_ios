19e4d9fd1da48f55946e57529ce03f8fa880ed2d
Michael Kinney <michaelakinney@comcast.net>
2021-01-28 14:58:53 -0500

    update SDLIAPSession unit tests

--- SmartDeviceLinkTests/TransportSpecs/iAP/SDLIAPSessionSpec.m
+++ SmartDeviceLinkTests/TransportSpecs/iAP/SDLIAPSessionSpec.m
@@ -10,38 +10,35 @@
 #import <Nimble/Nimble.h>
 #import <OCMock/OCMock.h>
 
-#import "EAAccessory+OCMock.h"
+#import "EAAccessory+OCMock.m"
 #import "SDLIAPSession.h"
 #import "SDLIAPConstants.h"
-
 
 QuickSpecBegin(SDLIAPSessionSpec)
 
 describe(@"SDLIAPSession", ^{
     __block EAAccessory *mockAccessory = nil;
+    __block id<SDLIAPSessionDelegate> mockDelegate = nil;
 
     describe(@"Initialization", ^{
         __block SDLIAPSession *testSession = nil;
 
         beforeEach(^{
             mockAccessory = [EAAccessory.class sdlCoreMock];
-            testSession = [[SDLIAPSession alloc] initWithAccessory:mockAccessory forProtocol:ControlProtocolString];
+            mockDelegate = OCMProtocolMock(@protocol(SDLIAPSessionDelegate));
+            testSession = [[SDLIAPSession alloc] initWithAccessory:mockAccessory forProtocol:ControlProtocolString iAPSessionDelegate:mockDelegate];
         });
 
         it(@"should init correctly", ^{
             expect(testSession.accessory).to(equal(mockAccessory));
             expect(testSession.protocolString).to(equal(ControlProtocolString));
-            expect(testSession.isInputStreamOpen).to(beFalse());
-            expect(testSession.isOutputStreamOpen).to(beFalse());
-        });
-
-        it(@"should get correctly", ^{
-            expect(testSession.isStopped).to(beTrue());
-            expect(testSession.connectionID).to(equal(0));
+            expect(testSession.bothStreamsOpen).to(beFalse());
+            expect(testSession.hasSpaceAvailable).to(beFalse());
             expect(testSession.isSessionInProgress).to(beFalse());
         });
     });
 });
 
 QuickSpecEnd
+
 
