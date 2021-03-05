f545a9d77a789fabb7e36f2d8c1482bb02770b24
Frank Elias <francois.elias@livio.io>
2021-03-05 15:58:44 -0500

    Comments review

--- SmartDeviceLink/private/SDLMenuManager.m
+++ SmartDeviceLink/private/SDLMenuManager.m
@@ -724,8 +724,8 @@ UInt32 const MenuCellIdMin = 1;
     params.menuName = cell.uniqueTitle;
     params.parentID = cell.parentCellId != UINT32_MAX ? @(cell.parentCellId) : nil;
     params.position = @(position);
-    params.secondaryText = (cell.secondaryText.length == 0 || ![self.windowCapability hasTextFieldOfName:SDLTextFieldNameMenuCommandSecondaryText]) ? nil : cell.secondaryText;
-    params.tertiaryText = (cell.tertiaryText.length == 0 || ![self.windowCapability hasTextFieldOfName:SDLTextFieldNameMenuCommandTertiaryText]) ? nil : cell.tertiaryText;
+    params.secondaryText = (cell.secondaryText.length > 0 && [self.windowCapability hasTextFieldOfName:SDLTextFieldNameMenuCommandSecondaryText]) ? cell.secondaryText : nil;
+    params.tertiaryText = (cell.tertiaryText.length > 0 && [self.windowCapability hasTextFieldOfName:SDLTextFieldNameMenuCommandTertiaryText]) ? cell.tertiaryText : nil;
 
     command.menuParams = params;
     command.vrCommands = (cell.voiceCommands.count == 0) ? nil : cell.voiceCommands;
@@ -747,8 +747,8 @@ UInt32 const MenuCellIdMin = 1;
         submenuLayout = self.menuConfiguration.defaultSubmenuLayout;
     }
     
-    NSString *secondaryText = (cell.secondaryText.length == 0 || ![self.windowCapability hasTextFieldOfName:SDLTextFieldNameMenuSubMenuSecondaryText]) ? nil : cell.secondaryText;
-    NSString *tertiaryText = (cell.tertiaryText.length == 0 || ![self.windowCapability hasTextFieldOfName:SDLTextFieldNameMenuSubMenuTertiaryText]) ? nil : cell.tertiaryText;
+    NSString *secondaryText = (cell.secondaryText.length > 0 && [self.windowCapability hasTextFieldOfName:SDLTextFieldNameMenuSubMenuSecondaryText]) ? cell.secondaryText : nil;
+    NSString *tertiaryText = (cell.tertiaryText.length > 0 && [self.windowCapability hasTextFieldOfName:SDLTextFieldNameMenuSubMenuTertiaryText]) ? cell.tertiaryText : nil;
     return [[SDLAddSubMenu alloc] initWithMenuID:cell.cellId menuName:cell.uniqueTitle position:@(position) menuIcon:icon menuLayout:submenuLayout parentID:nil secondaryText:secondaryText tertiaryText:tertiaryText secondaryImage:secondaryImage];
 }
 
