5672dd3bf1bd46bf98807ab6b5e7c5c404153ade
Frank Elias <francois.elias@livio.io>
2021-04-08 10:19:07 -0400

    Spec files update

--- SmartDeviceLinkTests/DevAPISpecs/SDLVoiceCommandSpec.m
+++ SmartDeviceLinkTests/DevAPISpecs/SDLVoiceCommandSpec.m
@@ -7,18 +7,28 @@ QuickSpecBegin(SDLVoiceCommandSpec)
 
 describe(@"a voice command", ^{
     __block SDLVoiceCommand *testCommand = nil;
+    __block SDLVoiceCommand *testCommand2 = nil;
 
     describe(@"initializing", ^{
         __block NSArray<NSString *> *someVoiceCommands = nil;
+        __block NSArray<NSString *> *someVoiceCommands2 = nil;
 
         beforeEach(^{
             someVoiceCommands = @[@"some command"];
+            someVoiceCommands2 = @[@"Test 1", @"Test 1", @"Test 1"];
         });
 
         it(@"should initialize properly", ^{
             testCommand = [[SDLVoiceCommand alloc] initWithVoiceCommands:someVoiceCommands handler:^{}];
 
             expect(testCommand.voiceCommands).to(equal(someVoiceCommands));
+        });
+
+        it(@"should initialize properly with reduced repeated strings", ^{
+            testCommand2 = [[SDLVoiceCommand alloc] initWithVoiceCommands:someVoiceCommands2 handler:^{}];
+
+            expect(testCommand2.voiceCommands).toNot(equal(someVoiceCommands2));
+            expect(testCommand2.voiceCommands.count).to(equal(1));
         });
     });
 });
