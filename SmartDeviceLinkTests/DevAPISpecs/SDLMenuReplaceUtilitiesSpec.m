e37c26fcfaab20aee2abb7a753244fd4a5f92487
Joel Fischer <joeljfischer@gmail.com>
2021-08-09 08:53:32 -0400

    Stubbing out additional tests

--- SmartDeviceLinkTests/DevAPISpecs/SDLMenuReplaceUtilitiesSpec.m
+++ SmartDeviceLinkTests/DevAPISpecs/SDLMenuReplaceUtilitiesSpec.m
@@ -24,29 +24,73 @@ describe(@"finding all artworks from cells", ^{
     __block NSArray<SDLMenuCell *> *testMenuCells = nil;
     __block SDLFileManager *mockFileManager = nil;
     __block SDLWindowCapability *testWindowCapability = nil;
+    __block NSArray<SDLTextField *> *allSupportedTextFields = nil;
+    __block NSArray<SDLImageField *> *allSupportedImageFields = nil;
 
     beforeEach(^{
         mockFileManager = OCMClassMock([SDLFileManager class]);
     });
+
+    context(@"when all the files are uploaded", ^{
+        beforeEach(^{
+            OCMStub([mockFileManager fileNeedsUpload:[OCMArg any]]);
+        });
+
+        context(@"when the window capability doesn't support the primary image", ^{
+            beforeEach(^{
+
+            });
+        });
+
+        context(@"when the window capability supports primary but not secondary image", ^{
+
+        });
+
+        context(@"when the window capability supports both images", ^{
+
+        });
+    });
+
+    context(@"when no files are uploaded", ^{
+
+    });
+});
+
+describe(@"retrieving a commandId", ^{
+
+});
+
+describe(@"retrieving a position", ^{
+
 });
 
 describe(@"generating RPCs", ^{
+    context(@"delete commands", ^{
 
+    });
+
+    context(@"main menu commands", ^{
+
+    });
+
+    context(@"sub menu commands", ^{
+
+    });
 });
 
 // updating menu cells
-describe(@"updating menu cells", ^{
+describe(@"updating menu cell lists", ^{
     __block NSArray<SDLMenuCell *> *testNewMenuCells = nil;
     __block UInt32 testCommandId = 0;
 
     // removeMenuCellFromList:withCmdId:
-    describe(@"removeMenuCellFromList:withCmdId:", ^{
+    describe(@"removing commands from a list", ^{
         context(@"the list only has one level", ^{
 
         });
     });
 
-    describe(@"addMenuRequestWithCommandId:position:fromNewMenuList:toMainMenuList:", ^{
+    describe(@"add commands to the list", ^{
         __block NSMutableArray<SDLMenuCell *> *testMenuCells = nil;
         __block UInt16 testPosition = 0;
     });
