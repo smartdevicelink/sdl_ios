738bea504926cd91c7778075dc76fd2619b9283c
Frank Elias <francois.elias@livio.io>
2021-08-25 14:46:43 -0400

    spec file update

--- SmartDeviceLinkTests/RPCSpecs/PayloadSpecs/SDLSecurityQueryPayloadSpec.m
+++ SmartDeviceLinkTests/RPCSpecs/PayloadSpecs/SDLSecurityQueryPayloadSpec.m
@@ -1,5 +1,5 @@
 //
-//  SDLsss.m
+//  SDLSecurityQueryPayloadSpec.m
 //  SmartDeviceLinkTests
 //
 //  Created by Frank Elias on 8/12/21.
@@ -15,19 +15,18 @@
 #import "SDLRPCParameterNames.h"
 #import "SDLRPCFunctionNames.h"
 #import "SDLSecurityQueryPayload.h"
+#import "SDLSecurityQueryErrorCode.h"
 
 QuickSpecBegin(SDLSecurityQueryPayloadSpec)
 
 __block SDLSecurityQueryPayload* testPayload;
-__block NSDictionary* dict = @{SDLRPCParameterNameResponse:
-                                   @{SDLRPCParameterNameParameters:@{},
-                                     SDLRPCParameterNameOperationName:SDLRPCFunctionNameDeleteCommand}};
+__block NSDictionary* dict = @{@"id": @"3", @"text":@"SDL does not support encryption"};
 
 NSData* (^testData)(void) = ^NSData* {
     NSData* jsonData = [NSJSONSerialization dataWithJSONObject:dict options:0 error:0];
     NSData* binaryData = [NSData dataWithBytes:"PrimitiveString" length:strlen("PrimitiveString")];
 
-    UInt8 header[12] = {0x01, 0x00, 0x00, 0x06, 0x00, 0x00, 0x14, 0x43, 0x00, 0x00, 0x00, 0x00};
+    Byte header[12] = {0x20, 0x00, 0x00, 0x02, 0x00, 0x00, 0x14, 0x43, 0x00, 0x00, 0x00, 0x00};
     *(UInt32 *)&header[8] = CFSwapInt32HostToBig((unsigned int)jsonData.length);
 
     NSMutableData *data = [NSMutableData dataWithCapacity:12 + jsonData.length];
@@ -41,17 +40,17 @@ NSData* (^testData)(void) = ^NSData* {
 beforeSuite(^ {
     testPayload = [[SDLSecurityQueryPayload alloc] init];
 
-    testPayload.queryType = 0x01;
-    testPayload.queryID = 0x06;
+    testPayload.queryType = 0x20;
+    testPayload.queryID = 0x02;
     testPayload.sequenceNumber = 0x1443;
     testPayload.jsonData = [NSJSONSerialization dataWithJSONObject:dict options:0 error:0];
     testPayload.binaryData = [NSData dataWithBytes:"PrimitiveString" length:strlen("PrimitiveString")];
 });
 
 describe(@"Getter/Setter Tests", ^ {
-    it(@"Should set and get correctly", ^ {
-        expect(@(testPayload.queryType)).to(equal(@0x01));
-        expect(@(testPayload.queryID)).to(equal(@0x06));
+    it(@"should set and get correctly", ^ {
+        expect(@(testPayload.queryType)).to(equal(SDLSecurityQueryTypeNotification));
+        expect(@(testPayload.queryID)).to(equal(SDLSecurityQueryIdSendInternalError));
         expect(@(testPayload.sequenceNumber)).to(equal(@0x1443));
         expect([NSJSONSerialization JSONObjectWithData:testPayload.jsonData options:0 error:0]).to(equal(dict));
         expect([NSString stringWithUTF8String:[testPayload binaryData].bytes]).to(equal(@"PrimitiveString"));
@@ -59,19 +58,21 @@ describe(@"Getter/Setter Tests", ^ {
 });
 
 describe(@"Data Tests", ^ {
-    it (@"Should convert to byte data correctly", ^ {
+    it(@"should convert to byte data correctly", ^ {
         expect(testPayload.data).to(equal(testData()));
     });
 });
 
 describe(@"RPCPayloadWithData Test", ^ {
-    it (@"Should convert from byte data correctly", ^ {
+    it(@"should convert from byte data correctly", ^ {
         SDLSecurityQueryPayload* constructedPayload = [SDLSecurityQueryPayload securityPayloadWithData:testData()];
 
-        expect(@(constructedPayload.queryType)).to(equal(@0x01));
-        expect(@(constructedPayload.queryID)).to(equal(@0x06));
+        expect(@(constructedPayload.queryType)).to(equal(SDLSecurityQueryTypeNotification));
+        expect(@(constructedPayload.queryID)).to(equal(SDLSecurityQueryIdSendInternalError));
         expect(@(constructedPayload.sequenceNumber)).to(equal(@0x1443));
-        expect([NSJSONSerialization JSONObjectWithData:constructedPayload.jsonData options:0 error:0]).to(equal(dict));
+        NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:constructedPayload.jsonData options:0 error:0];
+        expect(jsonDict).to(equal(dict));
+        expect(jsonDict[@"text"]).to(equal(SDLSecurityQueryErrorCodeNotSupported));
         expect([NSString stringWithUTF8String:[constructedPayload binaryData].bytes]).to(equal(@"PrimitiveString"));
     });
 });
