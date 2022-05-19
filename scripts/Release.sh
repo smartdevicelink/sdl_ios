9468962f69764b9460c533be6532bce6d3b2831a
George Miller <George@livio.io>
2022-05-19 09:47:27 -0400

    Setting up for phase 3

--- scripts/Release.sh
+++ scripts/Release.sh
@@ -8,25 +8,32 @@ echo "Start"
 
 # Phase 2: Script changes build numbers automatically in pbxproj and podspecs
 
+# probably need to pull latest from before doing any of this
 
 # 1 bump version in projectFile
-# sed stream edit.  It's fast, and can make changes.
-bash Phase2.sh
-
 # 2 update version in podspec
 # SmartDeviceLink-iOS.podspec
+bash Phase2.sh
+
+
 
 # 3 update RPC versions
+# file?
 
 # 3 update protocol versions
+# file?
 
 # 4 update to newest bson
+# file?
 
 # 4 update package.swift
+# ?
 
 # 4 update CocoaPods dependancy
+# ?
 
 # 5 update changelog
+# ?
 
 # 6 Install Jazzy
 echo "6 Install Jazzy"
@@ -38,20 +45,18 @@ echo "6 generate documentation"
 
 # 7 ensure RPC_SPEC has released to master
 
-# 7 update the submodule to poitn to new release
+# 7 update the submodule to point to new release
 
+
+# git commands
 # 8 commit release to develop
-
 # 8 merge release to master
-
 # 8 tag it
-
 # 8 merge master back to develop
 
 
 
 # Phase 1: Script runs pod trunk pushes, builds xcframework, builds documentation
-
 
 # 1 Create new release for tag
 # ?
@@ -59,7 +64,15 @@ echo "6 generate documentation"
 # 1 add highlights of changes
 # ?
 
+
+# 2 push new release to primary cocoapod
+# 3 Push the new release to the secondary cocoapod using command line:
+# 4 add binary xcframework archive for manual installation
+# i
+# ii
+# iii
 bash phase1.sh
+
 
 # iv Compress the .xcframework and add the it to the release.
 echo "4.iv Compress the .xcframework and add the it to the release."
---
+++ scripts/phase3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# George Miller
+# 05-18-2022
+
+# I need to figure out some git commands
