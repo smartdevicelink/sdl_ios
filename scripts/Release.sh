b2124eee3d765ef402e0630c24886c0e6151b767
George Miller <George@livio.io>
2022-05-19 16:50:57 -0400

    Mostly notes
    Some changes from the review, but mostly notes about future work in this file.

--- scripts/Release.sh
+++ scripts/Release.sh
@@ -8,9 +8,10 @@
 
 # script start
 echo "Zug Zug"
+# echo "Starting SDL release script..."
 
 # do some directory shenanigans to get us to the correct working directory.
-echo $PWD
+# echo $PWD #debug
 if [[ $PWD == *"scripts" ]]; then
     cd ..
 fi
@@ -20,42 +21,46 @@ echo
 echo "Step 1: Update version in Project File"
 
 # 1.1) get the current version and build from the podspec file
-ProjFile=./SmartDeviceLink-iOS.xcodeproj/project.pbxproj
-NewFile=./SmartDeviceLink-iOS.xcodeproj/new.pbxproj
-Current_version_number=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjFile)
-Current_build_number=$(sed -n '/CURRENT_PROJECT_VERSION/{s/CURRENT_PROJECT_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjFile)
-echo "Current Version "$Current_version_number
-echo "Current Build "$Current_build_number
+project_file=./SmartDeviceLink-iOS.xcodeproj/project.pbxproj
+new_file=./SmartDeviceLink-iOS.xcodeproj/new.pbxproj
+current_version_number=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $project_file)
+current_build_number=$(sed -n '/CURRENT_PROJECT_VERSION/{s/CURRENT_PROJECT_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $project_file)
+echo "Current Version "$current_version_number
+echo "Current Build "$current_build_number
 
 # 1.2) prompt user for new version
 echo "Enter the new version number (semantic versioning x.x.x format) or blank to skip: "
-read NewVersionNumber
-echo "New Build?"
-read NewBuildNumber
+read new_version_number
 
 # 1.3) if not blank, or not the same
-if [ -z $NewVersionNumber ]; then
+if [ -z $new_version_number ]; then
     echo "No version number entered. Skipping..."
 else
-    if [ $Current_version_number != $NewVersionNumber ]; then
+    if [ $current_version_number != $NewVersinew_version_numberonNumber ]; then
         echo "Changed Version Number"
         # 1.4) swap new version in to file
-        sed '/MARKETING_VERSION/{s/'$Current_version_number'/'$NewVersionNumber'/;}' $ProjFile > $NewFile
-        mv -f $NewFile $ProjFile
+        sed '/MARKETING_VERSION/{s/'$current_version_number'/'$new_version_number'/;}' $project_file > $new_file
+        mv -f $new_file $project_file
     else
-        echo No file change needed for version
+        echo "No project file change needed for version"
     fi
 fi
-if [ -z $NewBuildNumber ]; then
-    echo No file change needed for build
+
+# in the event that the build number somehow is not what it shoudl be, we have code to fix it.
+#echo "New Build?"
+#read new_build_number
+# per review, the new build number will always be 1
+new_build_number=1
+if [ -z $new_build_number ]; then
+    #echo "No project file change needed for build"
 else
-    if [ $Current_build_number != $NewBuildNumber ]; then
+    if [ $current_build_number != $new_build_number ]; then
         echo make the change
         # 1.4) swap new version in to file
-        sed '/CURRENT_PROJECT_VERSION/{s/'$Current_build_number'/'$NewBuildNumber'/;}' $ProjFile > $NewFile
-        mv -f $NewFile $ProjFile
+        sed '/CURRENT_PROJECT_VERSION/{s/'$current_build_number'/'$new_build_number'/;}' $project_file > $new_file
+        mv -f $new_file $project_file
     else
-        echo No file change needed for build
+        #echo "No project file change needed for build"
     fi
 fi
 
@@ -72,79 +77,92 @@ CurrentVersion=$(sed -n '/s.version/{s/s.version//;s/=//;s/[\"]//g;s/^[[:space:]
 echo "Current Version="$CurrentVersion
 
 # 2.2) prompt user for new version
-echo "New Version?"
-read NewVersion
+#echo "New Version?"
+#read new_version
+# per review, the new version should be the same as above, but we should check with the user.
+echo "Enter the new version number for podfile (semantic versioning x.x.x format) or blank to set to match project file ("$new_version_number"): "
+read new_version
 
-# 2.3) if not blank, or not the same
-if [ -z $NewVersion ]; then
-    echo No file change needed
+# 2.3) If blank, use new version number from above.  If new version is different from current, change it.
+if [ -z $new_version ]; then
+    new_version=$new_version_number
 else
-    if [ $CurrentVersion != $NewVersion ]; then
-        echo make the change
-        # 2.4) swap new version in to file
-        sed '/s.version/{s/'$CurrentVersion'/'$NewVersion'/;}' $PodSpecFile > $PodSpecNewFile
-        mv -f $PodSpecNewFile $PodSpecFile
-    else
-        echo No file change needed for version
-    fi
+fi
+if [ $CurrentVersion != $new_version ]; then
+    echo make the change
+    # 2.4) swap new version in to file
+    sed '/s.version/{s/'$CurrentVersion'/'$new_version'/;}' $PodSpecFile > $PodSpecNewFile
+    mv -f $PodSpecNewFile $PodSpecFile
+else
+    #echo "No pod file change needed for version"
 fi
 
 
 # 3 update RPC versions
-# file?
+# SDLGlobals.h
+# TODO
 
 # 3 update protocol versions
-# file?
+# SDLGlobals.h
+# TODO
 
 # 4 update to newest bson
-# file?
+# git submodule ... commands
+# TODO
 
-# 4 update package.swift
-# ?
-
-# 4 update CocoaPods dependancy
-# ?
+# Update Package.swift and CocoaPods dependency files to point to latest if necessary.
 
 # 5 update changelog
-# ?
+# TODO - prompt user to make changes to this file:
+# Changelog.md
 
 # 6 Install Jazzy
+# we may want to check if Jazzy is already installed, and only run this if not.  TBD
 echo "6 Install Jazzy"
 sudo gem install jazzy
 
 # 6 generate documentation
 echo "6 generate documentation"
-#bash generate-documentation.sh
+# TODO - prompt user to ask if they want to do this.
+# bash generate-documentation.sh
+# TODO - before bringing code over, evaluate what code does and make sure it will work here.  That code already does the cd ..
+
 
 
 # 7 ensure RPC_SPEC has released to master
+# TODO - prompt user to do this.
+# maybe provide links to assist?
 
 # 7 update the submodule to point to new release
+# TODO - prompt user to do this and wait for confirmation
 
 
-# git commands
-# 8 commit release to develop
-# git checkout <branch>
-# git add -A
-# git commit -m <message here>
-# git push --set-upstream origin <branchname>
+# TODO - prompt the user for this.
+# and every step of this process, the user needs complete control.
+# that means we tell them what we want to do, ask them if they want us to do it, report the results, and gracefully handle exits.
 
-# 8 merge release to master
-# git merge <branch> <master>
+    # git commands
+    # 8 commit release to develop
+    # git checkout <branch>
+    # git add -A
+    # git commit -m <message here>
+    # git push --set-upstream origin <branchname>
 
-# 8 tag it
-# git tag <something>
+    # 8 merge release to master
+    # git merge <branch> <master>
 
-# 8 merge master back to develop
-# git merge <branch> <master>
+    # 8 tag it
+    # git tag <something>
+
+    # 8 merge master back to develop
+    # git merge <branch> <master>
 
 
+    # 1 Create new release for tag
+    # ?
 
-# 1 Create new release for tag
-# ?
-
-# 1 add highlights of changes
-# ?
+    # 1 add highlights of changes
+    # ?
 
 
 # 2 push new release to primary cocoapod
@@ -172,6 +190,7 @@ xcodebuild -create-xcframework -framework './SmartDeviceLink-Device.xcarchive/Pr
 # iv Compress the .xcframework and add the it to the release.
 echo "4.iv Compress the .xcframework and add the it to the release."
 # SmartDeviceLink.xcframework
+# TODO - Zip the .xcframework and name it based on the naming scheme in the releases. Then delete the old intermediate files.
 
 # 5 add docset to release (docs/docsets/)
 # ?
@@ -184,3 +203,4 @@ echo "4.iv Compress the .xcframework and add the it to the release."
 
 # script end
 echo "Work Complete"
+# echo "Release complete. Time to party 🍾"
