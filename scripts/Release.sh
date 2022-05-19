ca03740ea062c0cd0441b44faf1908efc65e74ff
George Miller <George@livio.io>
2022-05-19 10:01:53 -0400

    Release script with combined phases

--- scripts/Release.sh
+++ scripts/Release.sh
@@ -3,18 +3,91 @@
 # George Miller
 # 05-17-2022
 # If you don't have permission to run, try: chmod u+x Release.sh
+# numbering follows this document: https://github.com/smartdevicelink/sdl_ios/wiki/Release-Steps
+# The numbering does restart halfway because the document does.
 
-echo "Start"
+# script start
+echo "Zug Zug"
 
-# Phase 2: Script changes build numbers automatically in pbxproj and podspecs
-
-# probably need to pull latest from before doing any of this
+# do some directory shenanigans to get us to the correct working directory.
+echo $PWD
+if [[ $PWD == *"scripts" ]]; then
+    cd ..
+fi
 
 # 1 bump version in projectFile
+echo
+echo "Step 1: Update version in Project File"
+
+# 1.1) get the current version and build from the podspec file
+ProjFile=./SmartDeviceLink-iOS.xcodeproj/project.pbxproj
+NewFile=./SmartDeviceLink-iOS.xcodeproj/new.pbxproj
+Current_version_number=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjFile)
+Current_build_number=$(sed -n '/CURRENT_PROJECT_VERSION/{s/CURRENT_PROJECT_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $ProjFile)
+echo "Current Version "$Current_version_number
+echo "Current Build "$Current_build_number
+
+# 1.2) prompt user for new version
+echo "New Version?"
+read NewVersionNumber
+echo "New Build?"
+read NewBuildNumber
+
+# 1.3) if not blank, or not the same
+if [ -z $NewVersionNumber ]; then
+    echo No file change needed for version
+else
+    if [ $Current_version_number != $NewVersionNumber ]; then
+        echo make the change
+        # 1.4) swap new version in to file
+        sed '/MARKETING_VERSION/{s/'$Current_version_number'/'$NewVersionNumber'/;}' $ProjFile > $NewFile
+        mv -f $NewFile $ProjFile
+    else
+        echo No file change needed for version
+    fi
+fi
+if [ -z $NewBuildNumber ]; then
+    echo No file change needed for build
+else
+    if [ $Current_build_number != $NewBuildNumber ]; then
+        echo make the change
+        # 1.4) swap new version in to file
+        sed '/CURRENT_PROJECT_VERSION/{s/'$Current_build_number'/'$NewBuildNumber'/;}' $ProjFile > $NewFile
+        mv -f $NewFile $ProjFile
+    else
+        echo No file change needed for build
+    fi
+fi
+
+
 # 2 update version in podspec
 # SmartDeviceLink-iOS.podspec
-bash Phase2.sh
+echo
+echo "Step 2: Update version in podspec File"
 
+# 2.1) get the current version from the podspec file
+PodSpecFile=SmartDeviceLink-iOS.podspec
+PodSpecNewFile=NewFile.podspec
+CurrentVersion=$(sed -n '/s.version/{s/s.version//;s/=//;s/[\"]//g;s/^[[:space:]]*//g;p;q;}' $PodSpecFile)
+echo "Current Version="$CurrentVersion
+
+# 2.2) prompt user for new version
+echo "New Version?"
+read NewVersion
+
+# 2.3) if not blank, or not the same
+if [ -z $NewVersion ]; then
+    echo No file change needed
+else
+    if [ $CurrentVersion != $NewVersion ]; then
+        echo make the change
+        # 2.4) swap new version in to file
+        sed '/s.version/{s/'$CurrentVersion'/'$NewVersion'/;}' $PodSpecFile > $PodSpecNewFile
+        mv -f $PodSpecNewFile $PodSpecFile
+    else
+        echo No file change needed for version
+    fi
+fi
 
 
 # 3 update RPC versions
@@ -43,6 +116,7 @@ sudo gem install jazzy
 echo "6 generate documentation"
 #bash generate-documentation.sh
 
+
 # 7 ensure RPC_SPEC has released to master
 
 # 7 update the submodule to point to new release
@@ -50,13 +124,21 @@ echo "6 generate documentation"
 
 # git commands
 # 8 commit release to develop
+# git checkout <branch>
+# git add -A
+# git commit -m <message here>
+# git push --set-upstream origin <branchname>
+
 # 8 merge release to master
+# git merge <branch> <master>
+
 # 8 tag it
+# git tag <something>
+
 # 8 merge master back to develop
+# git merge <branch> <master>
 
 
-
-# Phase 1: Script runs pod trunk pushes, builds xcframework, builds documentation
 
 # 1 Create new release for tag
 # ?
@@ -66,13 +148,26 @@ echo "6 generate documentation"
 
 
 # 2 push new release to primary cocoapod
-# 3 Push the new release to the secondary cocoapod using command line:
-# 4 add binary xcframework archive for manual installation
-# i
-# ii
-# iii
-bash phase1.sh
+echo "2 push new release to primary Cocoapod"
+#pod trunk push SmartDeviceLink.podspec --allow-warnings
 
+# 3 Push the new release to the secondary cocoapod using command line:
+echo "3 Push the new release to the secondary cocoapod using command line:"
+#pod trunk push SmartDeviceLink-iOS.podspec --allow-warnings.
+
+# 4 add binary xcframework archive for manual installation
+echo "4 add binary xcframework archive for manual installation"
+# i
+echo "4.i"
+xcodebuild archive -project 'SmartDeviceLink-iOS.xcodeproj/' -scheme 'SmartDeviceLink' -configuration Release -destination 'generic/platform=iOS' -archivePath './SmartDeviceLink-Device.xcarchive' SKIP_INSTALL=NO
+
+# ii
+echo "4.ii"
+xcodebuild archive -project 'SmartDeviceLink-iOS.xcodeproj/' -scheme 'SmartDeviceLink' -configuration Release -destination 'generic/platform=iOS Simulator' -archivePath './SmartDeviceLink-Simulator.xcarchive' SKIP_INSTALL=NO
+
+# iii
+echo "4.iii"
+xcodebuild -create-xcframework -framework './SmartDeviceLink-Device.xcarchive/Products/Library/Frameworks/SmartDeviceLink.framework/' -framework './SmartDeviceLink-Simulator.xcarchive/Products/Library/Frameworks/SmartDeviceLink.framework/' -output './SmartDeviceLink.xcframework'
 
 # iv Compress the .xcframework and add the it to the release.
 echo "4.iv Compress the .xcframework and add the it to the release."
@@ -87,4 +182,5 @@ echo "4.iv Compress the .xcframework and add the it to the release."
 # 6 rename framework similar to old releases
 # ?
 
-echo "Complete"
+# script end
+echo "Work Complete"
