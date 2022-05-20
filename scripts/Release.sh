4f028f8c5e4fac964e8ae5eeb8d8f9bf15743dfe
George Miller <George@livio.io>
2022-05-20 14:02:26 -0400

    Periodic
    I haven't done a commit in a while and I have touched a lot of things.

--- scripts/Release.sh
+++ scripts/Release.sh
@@ -7,14 +7,20 @@
 # The numbering does restart halfway because the document does.
 
 # script start
-echo "Zug Zug"
-# echo "Starting SDL release script..."
+echo 
+#echo "Zug Zug"
+echo "Starting SDL release script..."
 
-# do some directory shenanigans to get us to the correct working directory.
-# echo $PWD #debug
+# If we are running from the scripts directory, we want to pop back to the project root to do everything.
 if [[ $PWD == *"scripts" ]]; then
     cd ..
 fi
+# If, for soem reason, we are not now in the correct working directory, exit
+if [[ $PWD != *"sdl_ios" ]]; then
+    echo "Please run this from the sdl_ios project root or the sdl_ios/scripts directory"
+    exit 0
+fi
+
 
 # 1 bump version in projectFile
 echo
@@ -46,7 +52,7 @@ else
     fi
 fi
 
-# in the event that the build number somehow is not what it shoudl be, we have code to fix it.
+# in the event that the build number somehow is not what it should be, we have code to fix it.
 #echo "New Build?"
 #read new_build_number
 # per review, the new build number will always be 1
@@ -96,71 +102,95 @@ fi
 
 
 # 3 update RPC versions
-# SDLGlobals.h
-# TODO
+# /SmartDeviceLink/private/SDLGlobals.h
+# TODO - extract version, prompt user to fix it if necessary.
+echo "Step 3: Please update RPC version in /SmartDeviceLink/private/SDLGlobals.h"
 
 # 3 update protocol versions
-# SDLGlobals.h
-# TODO
+# /SmartDeviceLink/private/SDLGlobals.h
+# TODO - extract version, prompt user to fix it if necessary.
+echo "Step 3: Please update protocol versions in /SmartDeviceLink/private/SDLGlobals.h"
 
 # 4 update to newest bson
 # git submodule ... commands
-# TODO
-
-# Update Package.swift and CocoaPods dependency files to point to latest if necessary.
+# 4.1 Update Package.swift and CocoaPods dependency files to point to latest if necessary.
+# TODO - what needs to be done for this?
 
 # 5 update changelog
-# TODO - prompt user to make changes to this file:
+# IDEA - I should record the timestamp of the changelog, and then check after the user returns to see that they did touch the file.
+# IDEA - we could also insert a template into the changelog that includes the version the users have selected above.
+echo 
+echo "Please go update Changelog.md, then return here and press enter."
+read user_input
 # Changelog.md
-
-# 6 Install Jazzy
-# TODO - we may want to check if Jazzy is already installed, and only run this if not.
-echo "6 Install Jazzy"
-#sudo gem install jazzy
 
 # 6 generate documentation
 echo "6 generate documentation"
-# TODO - prompt user to ask if they want to do this.
-# bash generate-documentation.sh
-# TODO - before bringing code over, evaluate what code does and make sure it will work here.  That code already does the cd ..
+echo "Would you like to automatically generate documentation with Jazzy (Y/n)?"
+read user_input
+if [[ ! $user_input == [Nn] ]]; then    
+    # 6 Install Jazzy
+    # Check if Jazzy is already installed, and if not then install jazzy
+    echo "Check if Jazzy is already installed, and install if necessary (may require password)"
+    if [ -z "$(mdfind -name 'Jazzy')" ]; then
+        echo "6 Install Jazzy"
+        sudo gem install jazzy
+    fi
 
-
+    # this runs Jazzy to generate the documentation
+    echo "Run Jazzy to generate documentation"
+    jazzy --clean --objc --framework-root SmartDeviceLink --sdk iphonesimulator --umbrella-header SmartDeviceLink/public/SmartDeviceLink.h --theme theme --output docs
+#then
+#    echo "not Jazzy..."
+fi
 
 # 7 ensure RPC_SPEC has released to master
-# TODO - prompt user to do this.
-# maybe provide links to assist?
+echo "Step 7: ensure RPC_SPEC has released to master"
+# TODO - cleanup wording
+# IDEA - maybe provide links to assist?
+echo "Please ensure RPC_SPEC has released to master and return here"
+read user_input
 
 # 7 update the submodule to point to new release
-# TODO - prompt user to do this and wait for confirmation
+#echo "update the submodule to point to new release"
+# TODO - cleanup wording
+# IDEA - maybe provide links to assist?
+echo "Please update the submodule to point to new release and return here"
+read user_input
 
 
-# TODO - prompt the user for this.
+echo "Please perform the following steps"
 # and every step of this process, the user needs complete control.
 # that means we tell them what we want to do, ask them if they want us to do it, report the results, and gracefully handle exits.
 
-    # git commands
-    # 8 commit release to develop
-    # git checkout <branch>
-    # git add -A
-    # git commit -m <message here>
-    # git push --set-upstream origin <branchname>
+# git commands
+#todo - I don't like any of how this section is handled.
+# 8 commit release to develop
+echo "step 8: commit release to develop"
+# git checkout <branch>
+# git add -A
+# git commit -m <message here>
+# git push --set-upstream origin <branchname>
 
-    # 8 merge release to master
-    # git merge <branch> <master>
+# 8 merge release to master
+echo "8.1 merge release to master"
+# git merge <branch> <master>
 
-    # 8 tag it
-    # git tag <something>
+# 8 tag it
+echo "8.2 tag the release"
+# git tag <something>
 
-    # 8 merge master back to develop
-    # git merge <branch> <master>
+# 8 merge master back to develop
+echo "8.3 merge master back to develop"
+# git merge <branch> <master>
 
+# 1 Create new release for tag
+echo "1.0 Create new release for tag"
+# ?
 
-    # 1 Create new release for tag
-    # ?
-
-    # 1 add highlights of changes
-    # ?
-
+# 1 add highlights of changes
+echo "1.1 add highlights of changes"
+# ?
 
 # 2 push new release to primary cocoapod
 echo "2 push new release to primary Cocoapod"
@@ -169,6 +199,7 @@ echo "2 push new release to primary Cocoapod"
 # 3 Push the new release to the secondary cocoapod using command line:
 echo "3 Push the new release to the secondary cocoapod using command line:"
 #pod trunk push SmartDeviceLink-iOS.podspec --allow-warnings.
+read user_input
 
 # 4 add binary xcframework archive for manual installation
 echo "Creating .xcframework for manual installation, to be added to the release."
@@ -188,6 +219,7 @@ xcodebuild -create-xcframework -framework './SmartDeviceLink-Device.xcarchive/Pr
 echo "4.iv Compress the .xcframework and add the it to the release."
 # SmartDeviceLink.xcframework
 # TODO - Zip the .xcframework and name it based on the naming scheme in the releases. Then delete the old intermediate files.
+
 
 # 5 add docset to release (docs/docsets/)
 # ?
