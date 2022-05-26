1c7ee6727bd85ed4f639e2683a7c3e546b270691
George Miller <George@livio.io>
2022-05-26 14:39:38 -0400

    changes per review.

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,6 @@
 
 # script start
 echo 
-#echo "Zug Zug"
 echo "Starting SDL release script..."
 
 # If we are running from the scripts directory, we want to pop back to the project root to do everything.
@@ -21,6 +20,8 @@ if [[ $PWD != *"sdl_ios" ]]; then
     exit 0
 fi
 
+# TODO - For Phase3, this is probably the correct place to checkout develop
+# git checkout develop
 
 # 1 bump version in projectFile
 echo
@@ -31,14 +32,14 @@ project_file=./SmartDeviceLink-iOS.xcodeproj/project.pbxproj
 new_file=./SmartDeviceLink-iOS.xcodeproj/new.pbxproj
 current_version_number=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $project_file)
 current_build_number=$(sed -n '/CURRENT_PROJECT_VERSION/{s/CURRENT_PROJECT_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $project_file)
-echo "Current Version "$current_version_number
-echo "Current Build "$current_build_number
+echo "Current Version: "$current_version_number
+echo "Current Build: "$current_build_number
 
 # 1.2) prompt user for new version
 echo "Enter the new version number (semantic versioning x.x.x format) or blank to skip: "
 read new_version_number
 
-# 1.3) if not blank, or not the same
+# 1.3) if not blank, or not the same, skip, otherwise change the version number
 if [ -z $new_version_number ]; then
     echo "No version number entered. Skipping..."
 else
@@ -52,67 +53,36 @@ else
     fi
 fi
 
-# in the event that the build number somehow is not what it should be, we have code to fix it.
-#echo "New Build?"
-#read new_build_number
-# per review, the new build number will always be 1
-new_build_number=1
-if [ -z $new_build_number ]; then
-    echo "No project file change needed for build number"
-else
-    if [ $current_build_number != $new_build_number ]; then
-        echo make the change
-        # 1.4) swap new version in to file
-        sed '/CURRENT_PROJECT_VERSION/{s/'$current_build_number'/'$new_build_number'/;}' $project_file > $new_file
-        mv -f $new_file $project_file
-    else
-        echo "No project file change needed for build number"
-    fi
-fi
-
-
 # 2 update version in podspec
 # SmartDeviceLink-iOS.podspec
 echo
 echo "Step 2: Update version in podspec File"
 
 # 2.1) get the current version from the podspec file
-PodSpecFile=SmartDeviceLink-iOS.podspec
-PodSpecNewFile=NewFile.podspec
-CurrentVersion=$(sed -n '/s.version/{s/s.version//;s/=//;s/[\"]//g;s/^[[:space:]]*//g;p;q;}' $PodSpecFile)
-echo "Current Version="$CurrentVersion
+pod_spec_file=SmartDeviceLink-iOS.podspec
+pod_spec_new_file=NewFile.podspec
+current_version=$(sed -n '/s.version/{s/s.version//;s/=//;s/[\"]//g;s/^[[:space:]]*//g;p;q;}' $pod_spec_file)
+echo "Current Version: "$current_version
 
-# 2.2) prompt user for new version
-#echo "New Version?"
-#read new_version
-# per review, the new version should be the same as above, but we should check with the user.
-echo "Enter the new version number for podfile (semantic versioning x.x.x format) or blank to set to match project file ("$new_version_number"): "
-read new_version
-
-# 2.3) If blank, use new version number from above.  If new version is different from current, change it.
-if [ -z $new_version ]; then
-    new_version=$new_version_number
-fi
-if [ $CurrentVersion != $new_version ]; then
-    echo "changing version in $PodSpecFile to $new_version"
-    # 2.4) swap new version in to file
-    sed '/s.version/{s/'$CurrentVersion'/'$new_version'/;}' $PodSpecFile > $PodSpecNewFile
-    mv -f $PodSpecNewFile $PodSpecFile
+# 2.2) Use new version number from above.  If new version is different from current, change it.
+if [ $current_version != $new_version_number ]; then
+    echo "changing version in $pod_spec_file to $new_version_number"
+    # 2.3) swap new version in to file
+    sed '/s.version/{s/'$current_version'/'$new_version_number'/;}' $pod_spec_file > $pod_spec_new_file
+    mv -f $pod_spec_new_file $pod_spec_file
 fi
 
-
-
-# 3 update RPC versions
+# 3 update RPC and protocol versions
 # /SmartDeviceLink/private/SDLGlobals.m
-# TODO - extract version, prompt user to fix it if necessary.
+# extract versions and prompt user to fix it if necessary.
 echo 
 echo "Step 3: Please update RPC version in /SmartDeviceLink/private/SDLGlobals.h"
-
-# 3 update protocol versions
-# /SmartDeviceLink/private/SDLGlobals.m
-# TODO - extract version, prompt user to fix it if necessary.
-echo 
-echo "Step 3: Please update protocol versions in /SmartDeviceLink/private/SDLGlobals.h"
+file="SmartDeviceLink/private/SDLGlobals.m"
+current_rpc_version=$(sed -n '/SDLMaxProxyProtocolVersion/{s/^.*@//;s/[\;]//;s/[\"]//g;p;q;}' $file)
+current_protocol_version=$(sed -n '/SDLMaxProxyRPCVersion/{s/^.*@//;s/[\;]//;s/[\"]//g;p;q;}' $file)
+echo "Current RPC Version: "$current_rpc_version
+echo "Current Protocol Version: "$current_rpc_version
+echo "Step 3: If these are not correct, please update protocol versions in /SmartDeviceLink/private/SDLGlobals.m"
 
 
 # 4 Update to the newest BSON submodule. Update Package.swift and CocoaPods dependency files to point to latest if necessary.
@@ -147,153 +117,132 @@ read user_input
 
 # 6 generate documentation
 echo 
-echo "6 generate documentation"
 echo "Would you like to automatically generate documentation with Jazzy (Y/n)?"
 read user_input
 if [[ ! $user_input == [Nn] ]]; then    
     # 6 Install Jazzy
     # Check if Jazzy is already installed, and if not then install jazzy
-    echo "Check if Jazzy is already installed, and install if necessary (may require password)"
     if [ -z "$(mdfind -name 'Jazzy')" ]; then
-        echo "6 Install Jazzy"
+        echo "Jazzy is not installed, attempting to install (may require your password)..."
         sudo gem install jazzy
     fi
 
     # this runs Jazzy to generate the documentation
-    echo "Run Jazzy to generate documentation"
+    echo "Running Jazzy to generate documentation..."
     jazzy --clean --objc --framework-root SmartDeviceLink --sdk iphonesimulator --umbrella-header SmartDeviceLink/public/SmartDeviceLink.h --theme theme --output docs
-#else
-#    echo "not Jazzy..."
 fi
 
 # 7 Ensure that the RPC_SPEC has released to the master branch and update the submodule to point to the new release tag (or to the HEAD of master, if no release of the RPC_SPEC is occurring).
-# TODO - cleanup wording
-# IDEA - maybe provide links to assist?
+echo 
+echo "The rpc spec site will be opened for you"
+echo "https://github.com/smartdevicelink/rpc_spec"
+echo "Please check if there is a new release of the RPC_SPEC to master."
+echo "If there is, please update the rpc_spec submodule to point to the new master, then return and press enter."
+read user_input # give user a chance to read before we throw a page at them.
+open "https://github.com/smartdevicelink/rpc_spec"
+read user_input # wait for user to return
+
 echo
-echo "Step 7: ensure RPC_SPEC has released to master"
-echo "Ensure that the RPC_SPEC has released to the master branch and update the submodule to point to the new release tag (or to the HEAD of master, if no release of the RPC_SPEC is occurring)"
-echo "Then return here"
-read user_input
-
-
-
-echo
-echo "Please perform the following steps"
+echo "Please perform the following steps to push the release to master:"
 # and every step of this process, the user needs complete control.
 # that means we tell them what we want to do, ask them if they want us to do it, report the results, and gracefully handle exits.
 
 # git commands
-#todo - I don't like any of how this section is handled.
 # 8 commit release to develop
-echo "step 8: commit release to develop"
-# git checkout <branch>
-# git add -A
-# git commit -m <message here>
-# git push --set-upstream origin <branchname>
+echo "Would you like to commit these changes to the develop branch? Y/n"
+read user_input
+#if [[ ! $user_input == [Nn] ]]; then
+#todo - phase 3 - run the commands automatically
+echo "Please Commit and push the changes to develop"
 
 # 8 merge release to master
-echo "8.1 merge release to master"
-# git merge <branch> <master>
-
-# 8 tag it
-echo "8.2 tag the release"
-# git tag <something>
-
-# 8 merge master back to develop
-echo "8.3 merge master back to develop"
-# git merge <branch> <master>
+echo "Would you like to merge this release to master? (This will not push to master) Y/n"
+read user_input
+#if [[ ! $user_input == [Nn] ]]; then
+#todo - phase 3 - run the commands automatically
+echo "Please push to master, then press enter"
 read user_input
 
+# 8 tag it
+echo "Would you like to tag this release? (This will not push the tag) Y/n"
+read user_input
+#if [[ ! $user_input == [Nn] ]]; then
+#todo - phase 3 - run the commands automatically
+
+# 8 merge master back to develop
+echo "Would you like to merge master back into develop? (This will not push the branch) Y/n"
+# git merge develop <master>
+read user_input
+#if [[ ! $user_input == [Nn] ]]; then
+#todo - phase 3 - run the commands automatically
 
 echo
 echo "Please perform the following steps"
 # 1 Create new release for tag
-echo "Step 1: Create new release for tag"
-# ?
+echo "Would you like to go to the Github releases page to create a release? Y/n"
+read user_input
+if [[ ! $user_input == [Nn] ]]; then  
+    open "https://github.com/smartdevicelink/sdl_ios/releases"
 
-# 1 add highlights of changes
-echo "1.1 add highlights of changes"
-# ?
+    # TODO - phase 3 - this can be automated with gh
+    # https://cli.github.com/manual/gh_release_create
+fi
+
+
 
 # 2 push new release to primary cocoapod
-echo "Step 2: push new release to primary Cocoapod"
-echo "pod trunk push SmartDeviceLink.podspec --allow-warnings"
-#pod trunk push SmartDeviceLink.podspec --allow-warnings
-
 # 3 Push the new release to the secondary cocoapod using command line:
-echo "Step 3: Push the new release to the secondary cocoapod using command line:"
-echo "pod trunk push SmartDeviceLink-iOS.podspec --allow-warnings."
-#pod trunk push SmartDeviceLink-iOS.podspec --allow-warnings.
+echo "Would you like to push the release to CocoaPods? Y/N"
 read user_input
+if [[ ! $user_input == [Nn] ]]; then  
+    pod trunk push SmartDeviceLink.podspec --allow-warnings
+    pod trunk push SmartDeviceLink-iOS.podspec --allow-warnings.
+else
+    # commands for manual
+    echo "pod trunk push SmartDeviceLink.podspec --allow-warnings"
+    echo "pod trunk push SmartDeviceLink-iOS.podspec --allow-warnings."
+fi
+
 
 
 # 4 Add a binary xcframework archive for manual installation with the following commands
 echo
-echo "Step 4: Add a binary xcframework archive for manual installation"
-# i
-echo "4.i - Build Device archive"
-xcodebuild archive -project 'SmartDeviceLink-iOS.xcodeproj/' -scheme 'SmartDeviceLink' -configuration Release -destination 'generic/platform=iOS' -archivePath './SmartDeviceLink-Device.xcarchive' SKIP_INSTALL=NO
+echo "Would you like to create a binary xcframework for manual installation? Y/N"
+read user_input
+if [[ ! $user_input == [Nn] ]]; then  
+    xcodebuild archive -project 'SmartDeviceLink-iOS.xcodeproj/' -scheme 'SmartDeviceLink' -configuration Release -destination 'generic/platform=iOS' -archivePath './SmartDeviceLink-Device.xcarchive' SKIP_INSTALL=NO
+    xcodebuild archive -project 'SmartDeviceLink-iOS.xcodeproj/' -scheme 'SmartDeviceLink' -configuration Release -destination 'generic/platform=iOS Simulator' -archivePath './SmartDeviceLink-Simulator.xcarchive' SKIP_INSTALL=NO
+    xcodebuild -create-xcframework -framework './SmartDeviceLink-Device.xcarchive/Products/Library/Frameworks/SmartDeviceLink.framework/' -framework './SmartDeviceLink-Simulator.xcarchive/Products/Library/Frameworks/SmartDeviceLink.framework/' -output './SmartDeviceLink.xcframework'
 
-# ii
-echo "4.ii - Build Simulator archive"
-xcodebuild archive -project 'SmartDeviceLink-iOS.xcodeproj/' -scheme 'SmartDeviceLink' -configuration Release -destination 'generic/platform=iOS Simulator' -archivePath './SmartDeviceLink-Simulator.xcarchive' SKIP_INSTALL=NO
-
-# iii
-echo "4.iii - Build framework"
-xcodebuild -create-xcframework -framework './SmartDeviceLink-Device.xcarchive/Products/Library/Frameworks/SmartDeviceLink.framework/' -framework './SmartDeviceLink-Simulator.xcarchive/Products/Library/Frameworks/SmartDeviceLink.framework/' -output './SmartDeviceLink.xcframework'
-
-# iv Compress the .xcframework and add the it to the release.
-echo
-echo "4.iv Compress the .xcframework and add the it to the release."
-file="SmartDeviceLink.xcframework"
-zip_file_name="SmartDeviceLink-$new_version_number.xcframework.zip" #SmartDeviceLink-7.3.1.xcframework.zip
-if [ ! -f "$zip_file_name" ]; then
-    rm $zip_file_name #kill the old zip if present.  Useful for re-running the script
-fi
-#verify file exists before acting on it.  It's just good practice.  Maybe review and apply through this script.
-if [ ! -f "$file" ]; then
-    zip $zip_file_name $file
-fi
-#remove old files.  We check if the zip exists first to make sure we don't break anything
-if [ ! -f "$zip_file_name" ]; then
-    rm $file
+    file="SmartDeviceLink.xcframework"
+    zip_file_name="SmartDeviceLink-$new_version_number.xcframework.zip" #SmartDeviceLink-7.3.1.xcframework.zip
+    if [ ! -f "$zip_file_name" ]; then
+        #kill the old zip if present.  Useful for re-running the script
+        rm $zip_file_name 
+    fi
+    #verify file exists before acting on it.
+    if [ ! -f "$file" ]; then
+        zip $zip_file_name $file
+    fi
+    #Check to see if the zip exists, and then remove old files.
+    if [ ! -f "$zip_file_name" ]; then
+        rm $file
+    fi
 fi
 
-echo
-echo "Please add the framework to the release."
-echo "$framework_zip_file_name" # tell the user the name of the file we prepared
+echo "The xcframework zip file was created at $framework_zip_file_name. Please add it to the Github Release, then press enter..."
 read user_input
 #TODO - phase 3 - automate adding to release
-# git add <filename>
 
-# 5 add docset to release (docs/docsets/)
-echo 
-echo "step 5: Add the docset to the release (docs/docsets/)."
-echo "Please add the docset to the release."
-# probably need to zip this, and name it
-# name should be SmartDeviceLink-$new_version_number-docset.tgz  #BTW, what is tgz, and why not just .zip?
+# Rename the docset and pack it
+# SmartDeviceLink-$new_version_number-docset.tgz
 docset_directory="docs/docsets/"
 docset_tar_file_name="SmartDeviceLink-$new_version_number-docset.tgz"
 tar -czf $docset_tar_file_name $docset_directory
-echo "$docset_tar_file_name" # tell the user the name of the file we prepared
-read user_input
-
+echo "Please add the docset at $docset_tar_file_name to the Github release, then press enter..."
 # TODO - phase 3 - automate adding to release
-# git add <filename>
 
 
-# This was kind of handled in step 5, but I want to leave it here til phase 3, in case we decide to do it.
-#echo
-#echo "step 6: Rename the docset and framework similar to older releases"
-#read user_input
-# 6 rename docset similar to old releases
-# name should be SmartDeviceLink-$new_version_number-docset.tgz
 
-
-# script end
 echo
-#echo "Work Complete" #WoW
-#echo "Release complete. Time to party 🍾" #LivioCulture
-echo "Release complete"
-
-
+echo "Release complete."
