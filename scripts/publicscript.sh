42a808235938c3a85a8a144513d7fa9e90e4efd2
George Miller <George@livio.io>
2022-06-14 11:09:55 -0400

    cleaned up some unused bits

--- scripts/publicscript.sh
+++ scripts/publicscript.sh
@@ -19,16 +19,22 @@ path_pre="SmartDeviceLink/"
 #public_dir=$path_pre"/public"
 #private_dir=$path_pre"/private"
 
-# Cd into the correct project directory
-cd $project_directory
+
+# 1 Make sure we are in the correct directory
+# If we are running from the scripts directory, we want to pop back to the project root to do everything.
+if [[ $PWD == *"scripts" ]]; then
+    cd ..
+fi
+# If, for some reason, we are not now in the correct working directory, exit
+if [[ $PWD != *"sdl_ios" ]]; then
+    echo "Please run this from the sdl_ios project root or the sdl_ios/scripts directory"
+    exit 0
+fi
 
 # Pick out the lines that have "Public" in them, and filters everything down to get the fileref
 public_fileref_list=$(sed -n '/Public/{s/^.*fileRef//;s/^[[:space:]]*//;s/\/.*//;s/=//;p;}' $project_file)
 
-# Loop through list of fileref and use sed on the project file to find the file path
-public_filepaths=()
-#other_filepaths=()
-
+# TODO - this loop is very slow compared to sed or cat.  Is there a faster way to do this?
 for public_fileref in $public_fileref_list
 do
     # Pick out the lines with the file reference
@@ -37,16 +43,7 @@ do
     publicref_path=$(sed -n '/path/{s/^.*path//;s/=//;s/\"//g;s/^[[:space:]]*//g;p;}' <<< "$publicref_lines")
     # Eliminate everything after the ";"
     publicref_path=$(sed -n 's/\;.*//p;' <<< "$publicref_path")
-    # All of these have the attribute "public"
-    public_filepaths+=$publicref_path
     
-    # Identify lines that do not have "public" in the path
-    #if [[ "$publicref_path" != *"public"* ]]; then
-    #    echo $publicref_path
-    #    other_filepaths+=$publicref_path
-    #fi
-    #echo $publicref_path
-
     # Test to see if the file is at that path
     # Some of the paths already have the pre on them
     test_path=$publicref_path
