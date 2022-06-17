45289cfaf1738829d56ba92f18237d4286cfe830
George Miller <George@livio.io>
2022-06-17 15:56:50 -0400

    major changes
    I discovered the sed commands were not working as intended, and that the project file does not explicitly specify the private attribute.  So I had to rethink the entire approach.

--- scripts/publicscript.sh
+++ scripts/publicscript.sh
@@ -18,81 +18,59 @@
 # to test, you can go into xcode and just change the target membership of a file, then run the script and see that the file was identified and moved.
 # use Fork to undo changes as needed.
 
+#TODO - rename this script
 
-project_directory="../sdl_ios/"
 project_file="SmartDeviceLink-iOS.xcodeproj/project.pbxproj"
-path_pre="SmartDeviceLink/"
-public_dir=$path_pre"/public"
-#private_dir=$path_pre"/private"
+target_path="SmartDeviceLink/"
 
 
-# 1 Make sure we are in the correct directory
+# Make sure we are in the correct directory.
 # If we are running from the scripts directory, we want to pop back to the project root to do everything.
 if [[ $PWD == *"scripts" ]]; then
     cd ..
 fi
-# If, for some reason, we are not now in the correct working directory, exit
+# If, for some reason, we are not now in the correct working directory, exit.
 if [[ $PWD != *"sdl_ios" ]]; then
     echo "Please run this from the sdl_ios project root or the sdl_ios/scripts directory"
     exit 0
 fi
 
-# Pick out the lines that have "Public" in them, and filters everything down to get the fileref
-public_fileref_list=$(sed -n '/Public/{s/^.*fileRef//;s/^[[:space:]]*//;s/\/.*//;s/=//;p;}' $project_file)
+# Find the lines with "path".
+path_lines=$(sed -n '/path/{s/[[:space:]]*//;s/\/\*.*\*\///g;s/{.*path//;p;}' $project_file)
+# Filter to get only the lines with the header_filepath and fileref.
+header_files=$(sed -n '/\.h/{s/[[:space:]]*//g;s/\"//g;s/\;.*//g;s/==/=/;p;}' <<< "$path_lines")
 
-# TODO - this loop is very slow compared to sed or cat.  Is there a faster way to do this?
-for public_fileref in $public_fileref_list
+# For each line.
+for line in $header_files
 do
+    # Pick out the fileref and the header_filepath.
+    fileref=$(sed -n 's/=.*//;p;' <<< $line)
+    header_filepath=$(sed -n 's/.*=//;p;' <<< $line)
     
-    # Pick out the lines with the file reference
-    publicref_lines="$(sed -n "/$public_fileref/{p;}" $project_file)"
-    # Trim everything before "path", then trim "=", " ", and quotes
-    publicref_path=$(sed -n '/path/{s/^.*path//;s/=//;s/\"//g;s/^[[:space:]]*//g;p;}' <<< "$publicref_lines")
-    # Trim after the ";"
-    publicref_path=$(sed -n 's/\;.*//p;' <<< "$publicref_path")
+    # Use the fileref to get the attributes.
+    attributes=$(sed -n '/fileRef[[:space:]]*=[[:space:]]*'$fileref'/{s/.*fileRef[[:space:]]*=[[:space:]]*'$fileref'//;s/\/\*.*\*\///g;p;};' $project_file)
     
-    # Most of the paths in the project file do not have the full path from the project root.
-    # So we prepend the missing part if needed
-    test_path=$publicref_path
-    if [[ "$publicref_path" != *$path_pre* ]]; then
-        
-        # add the public folder if necessary
-        if [[ "$publicref_path" != *public* ]]; then
-            #echo $publicref_path
-            test_path="public/"$test_path
-        fi
-        test_path=$path_pre$test_path
+    # Determine public or private.
+    if [[ $attributes == *"Public"* ]]; then
+        header_type="public"
+    else
+        header_type="private"
     fi
     
-    # this is excessively verbose
-    #echo
-    #echo "file "$publicref_path
-    #echo "should be located at "$test_path
-
-    # Test to see if the file is in the public folder (does the file exist at the specified path)
-    if [ ! -f $test_path ]; then 
-        echo "ALERT"
-        echo $test_path
-        echo 
-
-        # If we did not find the file, lets see where it actually is.
-        # TODO - this is searching the project root (sdl_ios).  In needs to be limited to the target directory (smartdevicelink)
-        file_found_location=$(find . -name "$publicref_path" -maxdepth 2)
-        if [ ! -z "$file_found_location" ]; then
-            echo "File found: "$file_found_location
-
-            # Move the file to the correct destination
-            echo "Copying file to correct destination"
-            # cp -f $file_found_location $public_dir
-            # mv -f $file_found_location $test_path #DEBUG - copy is safer for testing.
-
-        else
-            echo "File could not be found in $PWD"
+    # Find the real location of the file.
+    file_found_location=$(find "$target_path" -name "$(basename "$header_filepath")" -maxdepth 2)
+    
+    # If file is found.
+    if [ ! -z "$file_found_location" ]; then
+        
+        # Test to see if the file is where it should be.
+        if [[ ! $file_found_location == *"/$header_type/"* ]]; then
+            
+            # The file is not where it should be: Alert the user.
+            echo "ALERT"
+            echo $header_filepath $header_type
+            echo $file_found_location
+            echo
         fi
     fi
 done
-
-
-
-
-
