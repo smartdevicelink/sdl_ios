02d65401616b7d3e4d3c656eda1c379664895627
George Miller <George@livio.io>
2022-07-20 11:21:59 -0400

    Lots of cleanup

--- scripts/release.sh
+++ scripts/release.sh
@@ -23,8 +23,6 @@ prompt_user() {
     fi
 }
 
-# TODO - phase 4 - github cli "gh" needs to be installed before we can use those commands. We could automate this or at least check if gh is installed.
-
 # Script start
 echo 
 echo "Starting SDL release script..."
@@ -48,22 +46,21 @@ else
     # to run things we need to use arch -x86_64
 fi
 # TODO - based on architecture flags, the commands for things like Jazzy change
-# Rosetta is homebrew crap, so lets avoid it.  Same problem as the GH commands.
-
 
 # Setup branch variables
 develop_branch="develop"
 main_branch="master"
 
-
 # Checkout develop - so we can update versions.
 # We need to checkout the branch before we start modifying files.
-# TODO - make this optional.  Then set a flag that determines if you skip other operations because you don't have this checked out.
 
 current_branch=$(git branch --show-current)
 if [ $current_branch == $develop_branch ]; then
+    echo
+    echo "$develop_branch is already checked out."
 else
     echo
+    echo "Checkout of $develop_branch is required for some steps"
     prompt_user "Would you like to automatically checkout $develop_branch"
     if [[ $? == 1 ]]; then
 
@@ -94,27 +91,13 @@ else
             echo "Automatic checkout has failed. Abort."
             exit 0
         fi
-    else
-        echo "checkout of $develop_branch is required for some steps. Please do so manually now."
-        echo "You may ignore this message if you already have $develop_branch checked out..."
-        read user_input
     fi
 fi
 
-# This is a redundant check to make sure the correct branch is checked out.
-current_branch=$(git branch --show-current)
-if [ ! $current_branch == $develop_branch ]; then
-    echo "You do not have $develop_branch currently checked out.  Any changes you make now will be lost when you check out $develop_branch."
-fi
-
-# Bump version in projectFile
 echo
 echo "Updating the version"
 
 # Get the current version and build from the podspec file
-# TODO - we failed to change the podspec file during release.  I think it got lost during a checkout.  Figure this out!
-# No seriously, I don't think Joel had things checked otu properly when we made changes.  I think that's why the changes got lost.
-# to test this, I need to run through a mock run, make the podspec changes, and see that they are good and not lost
 project_file="./SmartDeviceLink-iOS.xcodeproj/project.pbxproj"
 new_file="./SmartDeviceLink-iOS.xcodeproj/new.pbxproj"
 current_version_number=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' $project_file)
@@ -188,12 +171,11 @@ if [ ! -z "$submodule_info" ]; then
 fi
 
 # Update changelog
-# TODO - insert a template into the changelog that includes the version the users have selected above.
-#echo "A template for this release has been inserted into the changelog. Please update it."
+# TODO - we could insert a template into the changelog that includes the version the users have selected above.
 echo 
 echo "Please update CHANGELOG.md, then return here and press enter..."
 read user_input
-# TODO - check modified info before and after so we can detect if the user failed to update the file.
+# TODO - check modified info before and after so we can detect if the user did update the file.
 
 # Generate documentation
 prompt_user "Would you like to automatically generate documentation with Jazzy"
@@ -216,85 +198,75 @@ echo "If there is, please update the rpc_spec submodule to point to the newest c
 read user_input
 
 # Git commands
-
-# This is a redundant check to make sure the correct branch is checked out.
+# Check to make sure the correct branch is checked out.
 current_branch=$(git branch --show-current)
 if [ $current_branch == $develop_branch ]; then
     echo
     echo "$develop_branch has already been checked out for you."
-else
-    echo "You do not have $develop_branch currently checked out.  Any changes you make now will be lost when you check out $develop_branch."
-fi
 
-prompt_user "Would you like to walk through the git commands for this release"
-if [[ $? == 1 ]]; then
-    
-    prompt_user "Would you like to commit and push these changes to the develop branch"
+    prompt_user "Would you like to walk through the git commands for this release"
     if [[ $? == 1 ]]; then
-        # Add, commit, and push changes to develop
-        git add -A
-        git commit -m "Update for release $new_version_number"
-        git push --set-upstream origin $develop_branch
-    else
-        echo "Aborting script!"
-        exit 0
-    fi
-
-
-    # Merge release to master (update master from)
-    prompt_user "Would you like to merge this release to master? (This will not push to master)"
-    if [[ $? == 1 ]]; then
-        # Checkout master
-        git checkout $main_branch
-
-        # Merge develop with master.
-        # This udpates the checked out master with the contents of develop
-        # This is the where we put the new changes into the release.
-        git merge $develop_branch $main_branch
-
-        echo "Please check that everything is correct. "
-       
         
-
-        echo "If these changes are correct, please commit them manually and then push them to master..."
-        # Now that everything is ready, 
-        # TODO - this is where the changes should be committed and pushed
-        #git commit -m "commit message here "
-        #git push --set-upstream origin $main_branch
-        
-        # Tag it
-        prompt_user "Would you like to tag this release with $new_version_number? (This will not push the tag)"
+        prompt_user "Would you like to commit and push these changes to the develop branch"
         if [[ $? == 1 ]]; then
-            git tag $new_version_number
-            # IDEA - else condition that allows the user to enter a different tag
-            # TODO - do you need to push a tag?
-            # git push --set-upstream origin $main_branch
+            # Add, commit, and push changes to develop
+            git add -A
+            git commit -m "Update for release $new_version_number"
+            git push --set-upstream origin $develop_branch
+        else
+            echo "Aborting script!"
+            exit 0
         fi
+
+
+        # Merge release to master (update master from)
+        prompt_user "Would you like to merge this release to master? (This will not push to master)"
+        if [[ $? == 1 ]]; then
+            # Checkout master
+            git checkout $main_branch
+
+            # Merge develop with master.
+            # This updates the checked out master with the contents of develop
+            git merge $develop_branch $main_branch
+
+            echo "Please check that everything is correct. "
         
-        
-    else
-        echo "Aborting script!"
-        exit 0
+            # Tag it
+            prompt_user "Would you like to tag this release with $new_version_number? (This will not push the tag)"
+            if [[ $? == 1 ]]; then
+                git tag $new_version_number
+                # git push --set-upstream origin $main_branch
+            fi
+            
+
+            echo "If these changes are correct, please commit them manually and then push them to master..."
+            read user_input
+            # TODO - here are the commands if we decide to automate this.
+            # git commit -m "commit message here "
+            # git push --set-upstream origin $main_branch
+
+        else
+            echo "Aborting script!"
+            exit 0
+        fi
+
+        # Merge master back to develop
+        prompt_user "Would you like to merge master back into develop (This will not push the branch)"
+        if [[ $? == 1 ]]; then
+            git checkout $develop_branch
+            git merge $main_branch $develop_branch
+        else
+            echo "Aborting script!"
+            exit 0
+        fi
     fi
 
-    # Merge master back to develop
-    # TODO - this did not work during the release.  Why? DONE - it was backwards!!!!
-    prompt_user "Would you like to merge master back into develop (This will not push the branch)"
-    if [[ $? == 1 ]]; then
-        git checkout $develop_branch
-        git merge $main_branch $develop_branch
-    else
-        echo "Aborting script!"
-        exit 0
-    fi
+else
+    echo "You do not have $develop_branch currently checked out."
 fi
-
-# TODO - at this point the release is technically done.  IE the master branch is correct.
-# but we need the release notes published, and we need to publish the framework.
-
 
 # TODO - can we provide templates for the release based on the changelog?
-# TODO - can we open directories to facilitate drag'n'drop
+# TODO - can we open directories for drag and drop to the release?
 # Create new release for tag
 prompt_user "Would you like to open to the Github releases page to create a release"
 if [[ $? == 1 ]]; then
@@ -333,7 +305,6 @@ if [[ $? == 1 ]]; then
     echo 
     echo "Please add the docset at $docset_tar_file_name to the Github release, then press enter..."
     read user_input
-    # TODO - phase 4 - adding the docset to the release should also be automatic
 fi
 echo
 echo "Release complete."
