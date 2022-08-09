c1e8af61510397a12c4a5885c74cdf7faf41cba5
George Miller <George@livio.io>
2022-08-09 16:30:39 -0400

    Rough start to unit testing

---
+++ SmartDeviceLinkTests/ProtocolSpecs/SDLProtocolReceivedMessageProcessorSpec.m
@@ -0,0 +1,24 @@
+//
+//  SDLProtocolReceivedMessageProcessorSpec.m
+//  SmartDeviceLinkTests
+//
+//  Created by George Miller on 8/9/22.
+//  Copyright © 2022 smartdevicelink. All rights reserved.
+//
+
+#import <Quick/Quick.h>
+#import <Nimble/Nimble.h>
+
+#import <Foundation/Foundation.h>
+
+
+
+QuickSpecBegin(SDLProtocolReceivedMessageProcessorSpec)
+
+describe(@"Awesome Test", ^{
+    it(@"Should Be Awesome", ^{
+        expect(1 + 1).to(equal(2));
+    });
+});
+
+QuickSpecEnd
