91546da8fea00f2b0e434c016a7940ac886570a7
George Miller <George@livio.io>
2022-08-09 09:41:14 -0400

    a rough start

---
+++ SmartDeviceLinkSwift/SDLManagerSwift.swift
@@ -0,0 +1,21 @@
+//
+//  SDLManagerSwift.swift
+//  SmartDeviceLinkSwift
+//
+//  Created by George Miller on 8/8/22.
+//  Copyright © 2022 smartdevicelink. All rights reserved.
+//
+
+import Foundation
+
+import SmartDeviceLink
+
+//todo - better name maybe?
+public class SDLManagerSwift {
+    
+    // wrapper function for sdlmanager.sendRPC?
+    // or maybe for SDLLifeCycleManager.sendRPC?
+    public class func sendRPC(){
+        
+    }
+}
